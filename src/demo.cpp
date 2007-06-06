b81082977017c2da428f7f0f1be2b7b867afeecc
Miguel de Icaza <miguel@gnome.org>
2007-06-06 21:11:20 +0000

    2007-06-06  Miguel de Icaza  <miguel@novell.com>
    	* demo.cpp (main): Allow loading a XAML file from the command
    	line, for your testing pleasure.
    
    svn path=/trunk/moon/; revision=78786

--- src/demo.cpp
+++ src/demo.cpp
@@ -63,77 +63,82 @@ main (int argc, char *argv [])
 
 	w = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_signal_connect (GTK_OBJECT (w), "delete_event", G_CALLBACK (delete_event), NULL);
-
-	printf ("ac=%d\n", argc);
-	// Create our objects
-	r_trans = new RotateTransform ();
-	v_trans = new RotateTransform ();
-	s_trans = new ScaleTransform ();
-	t_trans = new TranslateTransform ();
-
 	Surface *t = surface_new (600, 600);
 	gtk_container_add (GTK_CONTAINER(w), t->drawing_area);
+		
 
-	r = rectangle_new ();
-	framework_element_set_width (r, 50.0);
-	framework_element_set_height (r, 50.0);
-	shape_set_stroke_thickness (r, 5.0);
-	r->SetValue (Canvas::LeftProperty, Value (50.0));
-	r->SetValue (Canvas::TopProperty, Value (50.0));
+	if (argc == 2){
+		gtk_window_set_title (GTK_WINDOW (w), argv [1]);
 
-	rectangle_set_radius_x (r, 10);
-	rectangle_set_radius_y (r, 20);
-	item_set_render_transform (r, s_trans);
-	Color *c = new Color (1.0, 0.0, 0.5, 0.5);
-	SolidColorBrush *scb = new SolidColorBrush ();
-	solid_color_brush_set_color (scb, c);
-	shape_set_stroke (r, scb);
-	Color *c2 = new Color (0.5, 0.5, 0.0, 0.25);
-	SolidColorBrush *scb2 = new SolidColorBrush ();
-	solid_color_brush_set_color (scb2, c2);
-	shape_set_fill (r, scb2);
+		UIElement *e = xaml_create_from_file (argv [1]);
+		if (e == NULL){
+			printf ("Was not able to load the file\n");
+		}
 
-	Rectangle *r2 = rectangle_new ();
-	framework_element_set_width (r2, 50.0);
-	framework_element_set_height (r2, 50.0);
-	shape_set_stroke_dash_array (r2, &dash, 1);
-	r2->SetValue (Canvas::LeftProperty, Value (50.0));
-	r2->SetValue (Canvas::TopProperty, Value (50.0));
-	item_set_render_transform (r2, r_trans);
-	shape_set_stroke (r2, scb);
-	panel_child_add (t, r2);
-
+		panel_child_add (t, e);
+		
+	} else {
+		// Create our objects
+		r_trans = new RotateTransform ();
+		v_trans = new RotateTransform ();
+		s_trans = new ScaleTransform ();
+		t_trans = new TranslateTransform ();
+		
+		r = rectangle_new ();
+		framework_element_set_width (r, 50.0);
+		framework_element_set_height (r, 50.0);
+		shape_set_stroke_thickness (r, 5.0);
+		r->SetValue (Canvas::LeftProperty, Value (50.0));
+		r->SetValue (Canvas::TopProperty, Value (50.0));
+		
+		rectangle_set_radius_x (r, 10);
+		rectangle_set_radius_y (r, 20);
+		item_set_render_transform (r, s_trans);
+		Color *c = new Color (1.0, 0.0, 0.5, 0.5);
+		SolidColorBrush *scb = new SolidColorBrush ();
+		solid_color_brush_set_color (scb, c);
+		shape_set_stroke (r, scb);
+		Color *c2 = new Color (0.5, 0.5, 0.0, 0.25);
+		SolidColorBrush *scb2 = new SolidColorBrush ();
+		solid_color_brush_set_color (scb2, c2);
+		shape_set_fill (r, scb2);
+		
+		Rectangle *r2 = rectangle_new ();
+		framework_element_set_width (r2, 50.0);
+		framework_element_set_height (r2, 50.0);
+		shape_set_stroke_dash_array (r2, &dash, 1);
+		r2->SetValue (Canvas::LeftProperty, Value (50.0));
+		r2->SetValue (Canvas::TopProperty, Value (50.0));
+		item_set_render_transform (r2, r_trans);
+		shape_set_stroke (r2, scb);
+		panel_child_add (t, r2);
+		
 #ifdef XAML_DEMO
-	panel_child_add (t, xaml_create_from_str ("<Line Stroke='Blue' X1='10' Y1='10' X2='10' Y2='300' />"));
+		panel_child_add (t, xaml_create_from_str ("<Line Stroke='Blue' X1='10' Y1='10' X2='10' Y2='300' />"));
 #endif
-
+		
 #ifdef VIDEO_DEMO
-	v = video_new ("file:///tmp/BoxerSmacksdownInhoffe.wmv");
-	item_set_render_transform (v, v_trans);
-	item_set_transform_origin (v, Point (1, 1));
-	printf ("Got %d\n", v);
-	panel_child_add (t, v);
+		v = video_new ("file:///tmp/BoxerSmacksdownInhoffe.wmv");
+		item_set_render_transform (v, v_trans);
+		item_set_transform_origin (v, Point (1, 1));
+		printf ("Got %d\n", v);
+		panel_child_add (t, v);
 #endif
-
-	panel_child_add (t, r);
-
+		
+		panel_child_add (t, r);
+		
 #ifdef VIDEO_DEMO
-	//UIElement *v2 = video_new ("file:///tmp/Countdown-Colbert-BestNailings.wmv", 100, 100);
-	//UIElement *v2 = video_new ("file:///tmp/red.wmv", 100, 100);
-	UIElement *v2 = video_new ("file:///tmp/BoxerSmacksdownInhoffe.wmv");
-	v2->SetValue (Canvas::LeftProperty, Value (100.0));
-	v2->SetValue (Canvas::TopProperty, Value (100.0));
-	item_set_render_transform (v2, s_trans);
-	panel_child_add (t, v2);
+		//UIElement *v2 = video_new ("file:///tmp/Countdown-Colbert-BestNailings.wmv", 100, 100);
+		//UIElement *v2 = video_new ("file:///tmp/red.wmv", 100, 100);
+		UIElement *v2 = video_new ("file:///tmp/BoxerSmacksdownInhoffe.wmv");
+		v2->SetValue (Canvas::LeftProperty, Value (100.0));
+		v2->SetValue (Canvas::TopProperty, Value (100.0));
+		item_set_render_transform (v2, s_trans);
+		panel_child_add (t, v2);
+		gtk_timeout_add (60, animate, NULL);
 #endif
-
-	printf ("set usize\n");
+	}		
 	gtk_widget_set_usize (w, 600, 400);
-	printf ("show\n");
 	gtk_widget_show_all (w);
-	gtk_timeout_add (60, animate, NULL);
-	printf ("timeout set\n");
-	//g_idle_add (animate, w);
-	printf ("entering main\n");
 	gtk_main ();
 }
