1ca6d495f1ea1f582df3220bfff4b0444b02b7a5
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-06-15 15:14:10 +0000

    * Makefile.am, index.html, tests.js: Split test in two files, index.html and 	tests.js, tests now is turned on real unit tets.
    2007-06-15  Everaldo Canuto  <ecanuto@novell.com>
    
    
    svn path=/trunk/moon/; revision=79706

---
+++ plugin/test/tests.js
@@ -0,0 +1,91 @@
+/*
+ * tests.js: JavaScript unit tests for MoonLight browser plugin.
+ *
+ * Author:
+ *   Everaldo Canuto (everaldo@novell.com)
+ *
+ * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ * 
+ */
+
+var already = false;
+var output;
+var control1;
+var control2;
+
+function print (text) {
+	output.value += text + "\n";
+}
+
+function AssertAreEqual (text, tvalue, expected) {
+	var result = (tvalue == expected) ? "." : "F";
+	print (result + " " + text + ": " + tvalue);
+}
+
+function AssertNotEqual (text, tvalue, expected) {
+	var result = (tvalue == expected) ? "F" : ".";
+	print (result + " " + text + ": " + tvalue);
+}
+
+function SetupTests () {
+	output = document.form.outputtext;
+	control1 = document.getElementById("moonControl1");
+	control2 = document.getElementById("moonControl2");
+
+	output.value = "";
+	BeforeLoadTests ();
+}
+
+function BeforeLoadTests () {
+	print ("=> Page started\n");
+
+	AssertNotEqual ("control1.settings.version", control1.settings.version, "");
+	AssertAreEqual ("control1.isLoaded", control1.isLoaded, false);
+}
+
+function AfterLoadTests () {
+	// I dont know why but IE calls onload two times.
+	if (already) return;
+	already = true;
+
+	print ("\n=> Page loaded");
+
+	print (""); ControlTest ();
+	print (""); ControlSettingsTest ();
+	print (""); ControlContentTest ();
+}
+
+function ControlTest () {
+	// control.isLoaded
+	AssertAreEqual ("control1.isLoaded", control1.isLoaded, true);
+
+	// control.initParams
+	AssertAreEqual ("control1.initParams", control1.initParams, "");
+	AssertAreEqual ("control2.initParams", control2.initParams, "paramValue1, paramValue2");
+
+	// control.source - Cant be changed after initialization.
+	AssertAreEqual ("control1.source", control1.source, "circle.xaml");
+	control1.source = "emote.xaml";
+	AssertAreEqual ("control1.source", control1.source, "emote.xaml");
+}
+
+function ControlSettingsTest () {
+	// control.settings.background
+	AssertAreEqual ("control1.settings.background", control1.settings.background, "#c0c0c0");
+	AssertAreEqual ("control2.settings.background", control2.settings.background, "#ffebcd");
+
+	// control.settings.version
+	AssertNotEqual ("control1.settings.version", settings.version, "");
+}
+
+function ControlContentTest () {
+	// control1.content.actualWidth
+	AssertAreEqual ("control1.content.actualWidth", control1.content.actualWidth, 0);
+	AssertAreEqual ("control2.content.actualWidth", control2.content.actualWidth, 0);
+
+	// control.content.actualHeight
+	AssertAreEqual ("control1.content.actualHeight", control1.content.actualHeight, 0);
+	AssertAreEqual ("control2.content.actualHeight", control2.content.actualHeight, 0);
+}
