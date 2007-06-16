50720cb3bf85d4d5346db4d695f988fe7393248b
Jackson Harper <jackson@novell.com>
2007-06-16 01:30:42 +0000

    New test for custom elements.
    svn path=/trunk/moon/; revision=79821

---
+++ test/test-custom-element.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace MonoDemo {
+
+	public class CustomCanvas : Canvas {
+
+		public CustomCanvas ()
+		{
+			Background = new SolidColorBrush (Colors.Red);
+		}
+        }
+}
+
