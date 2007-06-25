4d828aaf2e42809a4079039541ad2c1a57aef6c4
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-06-25 17:59:37 +0000

    * Makefile: Add clean target and add -t to laucher options. 	* image/*: Remove background images, we dont need it.
    2007-06-25  Everaldo Canuto  <ecanuto@novell.com>
    
    
    svn path=/trunk/moon/; revision=80706

--- desklet/novell.clock/clock.cs
+++ desklet/novell.clock/clock.cs
@@ -10,52 +10,42 @@ using System.Windows.Shapes;
 
 namespace Golem.Desklet
 {
-  public class Clock : Canvas 
-  {
-    RotateTransform secondsHand;
-    RotateTransform minuteHand;
-    RotateTransform hourHand;
-    Image background;
-    Timer t;
-    Uri moon1;
-    Uri moon2;
-    
-    void UpdateTime (object status)
-    {
-      Console.WriteLine ("UpdateTime");
-      DateTime now = DateTime.Now;
+	public class Clock : Canvas 
+	{
+		RotateTransform secondsHand;
+		RotateTransform minuteHand;
+		RotateTransform hourHand;
+		Timer timer;
 
-      if (now.Second < 30)
-	background.Source = moon1;
-      else
-	background.Source = moon2;
+		void UpdateTime (object status)
+		{
+			Console.WriteLine ("UpdateTime");
+			DateTime now = DateTime.Now;
 
-      secondsHand.Angle = now.Second * 6;
-      minuteHand.Angle = now.Minute * 6;
-      hourHand.Angle = now.Hour * 30;
-    }
+			secondsHand.Angle = now.Second * 6;
+			minuteHand.Angle = now.Minute * 6;
+			hourHand.Angle = now.Hour * 30;
+		}
 
-    public void PageLoaded (object o, EventArgs e)
-    {
-      Console.WriteLine ("page loaded");
-      secondsHand = FindName ("secondsHand") as RotateTransform;
-      minuteHand = FindName ("minuteHand") as RotateTransform;
-      hourHand = FindName ("hourHand") as RotateTransform;
-      background = FindName ("backImage") as Image;
+		public void PageLoaded (object o, EventArgs e)
+		{
+			Console.WriteLine ("page loaded");
+			secondsHand = FindName ("secondsHand") as RotateTransform;
+			minuteHand = FindName ("minuteHand") as RotateTransform;
+			hourHand = FindName ("hourHand") as RotateTransform;
 
-      string imagesDir = IO.Path.Combine (Environment.CurrentDirectory, "images");
-      moon1 = new Uri (IO.Path.Combine (imagesDir, "moon1.jpg"));
-      moon2 = new Uri (IO.Path.Combine (imagesDir, "moon2.jpg"));
-      
-      if (secondsHand == null || minuteHand == null || hourHand == null || background == null) {
-	// TODO: Should communicate the error to the desklet host
-	Console.WriteLine ("Missing elements in the desklet xaml file");
-	return;
-      }
+			// We can use 
+			//string imagesDir = IO.Path.Combine (Environment.CurrentDirectory, "images");
 
-      Console.WriteLine ("Starting timer");
-      AutoResetEvent autoEvent = new AutoResetEvent(false);
-      t = new Timer (new TimerCallback (UpdateTime), autoEvent, 5, 1000);
-    }
-  }
+			if (secondsHand == null || minuteHand == null || hourHand == null) {
+				// TODO: Should communicate the error to the desklet host
+				Console.WriteLine ("Missing elements in the desklet xaml file");
+				return;
+			}
+
+			Console.WriteLine ("Starting timer");
+			AutoResetEvent autoEvent = new AutoResetEvent(false);
+			timer = new Timer (new TimerCallback (UpdateTime), autoEvent, 5, 1000);
+		}
+	}
 }
