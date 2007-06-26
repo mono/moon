b68a5be5ee11d805d46433b3cd65c60c889e6a4c
Marek Habersack <grendel@twistedcode.net>
2007-06-26 22:47:11 +0000

    2007-06-27  Marek Habersack  <mhabersack@novell.com>
    	* Metar.cs: added Weather decoding
    
    svn path=/trunk/moon/; revision=80828

--- desklet/weather/Metar.cs
+++ desklet/weather/Metar.cs
@@ -27,13 +27,14 @@ namespace Desklets.Weather
 		public static Regex TempAndDew = new Regex ("^(M?[0-9]{2})/(M?[0-9]{2})?$", RegexOptions.Compiled);
 		public static Regex PressureHg = new Regex ("A([0-9]{4})", RegexOptions.Compiled);
 		public static Regex PressureMb = new Regex ("Q([0-9]{4})", RegexOptions.Compiled);
+		public static Regex Weather = new Regex ("^(VC)?(-|\\+)?(MI|PR|BC|DR|BL|SH|TS|FZ)?((DZ|RA|SN|SG|IC|PL|GR|GS|UP)+)?(BR|FG|FU|VA|DU|SA|HZ|PY)?(PO|SQ|FC|SS)?$", RegexOptions.Compiled);
 	}
 
 	public enum WindSpeedUnits {
 		Unknown,
 		Knots,
-		MilesPerSecond,
-		KilometersPerSecond
+		MetersPerSecond,
+		KilometersPerHour
 	};
 
 	public enum VisibilityDirection {
@@ -70,6 +71,7 @@ namespace Desklets.Weather
 
 	public enum CloudsKind {
 		Invalid,
+		None,
 		Cumulus,
 		Cumulonumbus,
 		ToweringCumulus,
@@ -102,6 +104,14 @@ namespace Desklets.Weather
 
 		public double HeightMeters {
 			get { return height * 0.3048; }
+		}
+
+		public Clouds ()
+		{
+			this.coverage = CloudsCoverage.Clear;
+			this.kind = CloudsKind.None;
+			this.height = 0;
+			this.accuracy = CloudsAccuracy.Exactly;
 		}
 		
 		public Clouds (string coverage, string height, string kind)
@@ -232,7 +242,7 @@ namespace Desklets.Weather
 		WindSpeedUnits unit;
 		int gusts = -1;
 		int direction = -1;
-		int speed = -1;
+		double speed = -1;
 
 		public WindSpeedUnits Unit {
 			get { return unit; }
@@ -246,8 +256,20 @@ namespace Desklets.Weather
 			get { return direction; }
 		}
 
-		public int Speed {
+		public double Speed {
 			get { return speed; }
+		}
+
+		public double Knots {
+			get { return ToKnots (); }
+		}
+
+		public double MetersPerSecond {
+			get { return ToMetersPerSecond (); }
+		}
+
+		public double KilometersPerHour {
+			get { return ToKilometersPerHour (); }
 		}
 		
 		public Wind (string direction, string speed, string gusts, string unit)
@@ -263,7 +285,7 @@ namespace Desklets.Weather
 			}
 
 			try {
-				this.speed = Convert.ToInt32 (speed);
+				this.speed = Convert.ToDouble (speed);
 			} catch {
 				this.speed = -1;
 			}
@@ -282,11 +304,11 @@ namespace Desklets.Weather
 					break;
 
 				case "MPS":
-					this.unit = WindSpeedUnits.MilesPerSecond;
+					this.unit = WindSpeedUnits.MetersPerSecond;
 					break;
 
 				case "KPH":
-					this.unit = WindSpeedUnits.KilometersPerSecond;
+					this.unit = WindSpeedUnits.KilometersPerHour;
 					break;
 
 				default:
@@ -294,7 +316,308 @@ namespace Desklets.Weather
 					break;
 			}
 		}
+
+		double ToKilometersPerHour ()
+		{
+			switch (unit) {
+				case WindSpeedUnits.Knots:
+					return speed * 0.5399568;
+
+				case WindSpeedUnits.MetersPerSecond:
+					return speed * 3.6;
+
+				case WindSpeedUnits.KilometersPerHour:
+					return speed;
+			}
+
+			return 0.0;
+		}
 		
+		double ToMetersPerSecond ()
+		{
+			switch (unit) {
+				case WindSpeedUnits.Knots:
+					return speed * 0.51444444;
+
+				case WindSpeedUnits.MetersPerSecond:
+					return speed;
+
+				case WindSpeedUnits.KilometersPerHour:
+					return speed * 0.27777778;
+			}
+
+			return 0.0;
+		}
+		
+		double ToKnots () 
+		{
+			switch (unit) {
+				case WindSpeedUnits.Knots:
+					return speed;
+
+				case WindSpeedUnits.MetersPerSecond:
+					return speed * 1.9438445;
+
+				case WindSpeedUnits.KilometersPerHour:
+					return speed * 0.5399568;
+			}
+
+			return 0.0;
+		}
+	}
+
+	public class Temperature
+	{
+		double celcius = 0.0;
+		double kelvin = 0.0;
+		double fahrenheit = 0.0;
+
+		public double Celcius {
+			get { return celcius; }
+		}
+
+		public double Kelvin {
+			get { return kelvin; }
+		}
+
+		public double Fahrenheit {
+			get { return fahrenheit; }
+		}
+		
+		public Temperature (string celcius)
+		{
+			try {
+				this.celcius = Convert.ToDouble (celcius);
+			} catch {
+				this.celcius = 0.0;
+			}
+
+			this.fahrenheit = (this.celcius * 1.8) + 32.0;
+			this.kelvin = this.celcius + 273.15;
+		}
+	}
+
+	public enum WeatherIntensity {
+		Invalid = 0x00,
+		Light = 0x01,
+		Moderate = 0x02,
+		Heavy = 0x04,
+		Vincinity = 0x08
+	};
+
+	public enum WeatherDescriptor {
+		Invalid,
+		Shallow,
+		Partial,
+		Patches,
+		LowDrifting,
+		Blowing,
+		Shower,
+		Thunderstorm,
+		Freezing
+	};
+
+	public enum WeatherPrecipitation {
+		Invalid,
+		Drizzle,
+		Rain,
+		Snow,
+		SnowGrains,
+		IceCrystals,
+		IcePellets,
+		Hail,
+		SmallHail,
+		Unknown
+	};
+
+	public enum WeatherObscuration {
+		Invalid,
+		Mist,
+		Fog,
+		Smoke,
+		VolcanicAsh,
+		Dust,
+		Sand,
+		Haze,
+		Spray
+	};
+
+	public enum WeatherMisc {
+		Invalid,
+		DustWhirls,
+		Squalls,
+		Tornado,
+		DustStorm
+	};
+	
+	public class Weather
+	{
+		WeatherIntensity intensity = WeatherIntensity.Invalid;
+		WeatherDescriptor descriptor = WeatherDescriptor.Invalid;
+		WeatherPrecipitation precipitation = WeatherPrecipitation.Invalid;
+		WeatherObscuration obscuration = WeatherObscuration.Invalid;
+		WeatherMisc misc = WeatherMisc.Invalid;
+		
+		public Weather (string proximity, string intensity, string descriptor,
+				string precipitation, string obscuration, string misc)
+		{
+			if (proximity == "VC")
+				this.intensity = WeatherIntensity.Vincinity;
+			
+			switch (intensity) {
+				case "-":
+					this.intensity |= WeatherIntensity.Light;
+					break;
+					
+				case "":
+					this.intensity |= WeatherIntensity.Moderate;
+					break;
+					
+				case "+":
+					this.intensity |= WeatherIntensity.Heavy;
+					break;
+			}
+			
+			switch (descriptor) {
+				case "MI":
+					this.descriptor = WeatherDescriptor.Shallow;
+					break;
+
+				case "PR":
+					this.descriptor = WeatherDescriptor.Partial;
+					break;
+
+				case "BC":
+					this.descriptor = WeatherDescriptor.Patches;
+					break;
+
+				case "DR":
+					this.descriptor = WeatherDescriptor.LowDrifting;
+					break;
+
+				case "BL":
+					this.descriptor = WeatherDescriptor.Blowing;
+					break;
+
+				case "SH":
+					this.descriptor = WeatherDescriptor.Shower;
+					break;
+
+				case "TH":
+					this.descriptor = WeatherDescriptor.Thunderstorm;
+					break;
+
+				case "FZ":
+					this.descriptor = WeatherDescriptor.Freezing;
+					break;
+
+				default:
+					this.descriptor = WeatherDescriptor.Invalid;
+					break;
+			};
+
+			switch (precipitation) {
+				case "DZ":
+					this.precipitation = WeatherPrecipitation.Drizzle;
+					break;
+
+				case "RA":
+					this.precipitation = WeatherPrecipitation.Rain;
+					break;
+
+				case "SN":
+					this.precipitation = WeatherPrecipitation.Snow;
+					break;
+
+				case "SG":
+					this.precipitation = WeatherPrecipitation.SnowGrains;
+					break;
+					
+				case "IC":
+					this.precipitation = WeatherPrecipitation.IceCrystals;
+					break;
+
+				case "PE":
+					this.precipitation = WeatherPrecipitation.IcePellets;
+					break;
+
+				case "GR":
+					this.precipitation = WeatherPrecipitation.Hail;
+					break;
+
+				case "GS":
+					this.precipitation = WeatherPrecipitation.SmallHail;
+					break;
+
+				case "UP":
+					this.precipitation = WeatherPrecipitation.Unknown;
+					break;
+
+				default:
+					this.precipitation = WeatherPrecipitation.Invalid;
+					break;
+			};
+
+			switch (obscuration) {
+				case "BR":
+					this.obscuration = WeatherObscuration.Mist;
+					break;
+
+				case "FG":
+					this.obscuration = WeatherObscuration.Fog;
+					break;
+
+				case "FU":
+					this.obscuration = WeatherObscuration.Smoke;
+					break;
+
+				case "VA":
+					this.obscuration = WeatherObscuration.VolcanicAsh;
+					break;
+
+				case "DU":
+					this.obscuration = WeatherObscuration.Dust;
+					break;
+
+				case "SA":
+					this.obscuration = WeatherObscuration.Sand;
+					break;
+
+				case "HZ":
+					this.obscuration = WeatherObscuration.Haze;
+					break;
+
+				case "PY":
+					this.obscuration = WeatherObscuration.Spray;
+					break;
+
+				default:
+					this.obscuration = WeatherObscuration.Invalid;
+					break;
+			};
+
+			switch (misc) {
+				case "PO":
+					this.misc = WeatherMisc.DustWhirls;
+					break;
+
+				case "SQ":
+					this.misc = WeatherMisc.Squalls;
+					break;
+
+				case "FC":
+					this.misc = WeatherMisc.Tornado;
+					break;
+
+				case "SS":
+					this.misc = WeatherMisc.DustStorm;
+					break;
+
+				default:
+					this.misc = WeatherMisc.Invalid;
+					break;
+			};
+		}
 	}
 	
 	public class Metar
@@ -305,11 +628,12 @@ namespace Desklets.Weather
 		Wind wind;
 		Visibility visibility;
 		Clouds clouds;
-		double temperature; // Celcius
-		double dewPoint; // Celcius
+		Temperature temperature;
+		Temperature dewPoint;
 		double pressureHg;
 		double pressureMb;
-
+		Weather weather;
+		
 		public string StationCode {
 			get { return stationCode; }
 		}
@@ -321,13 +645,41 @@ namespace Desklets.Weather
 		public Wind Wind {
 			get { return wind; }
 		}
+
+		public Visibility Visibility {
+			get { return visibility; }
+		}
+
+		public Clouds Clouds {
+			get { return clouds; }
+		}
+
+		public Temperature Temperature {
+			get { return temperature; }
+		}
+
+		public Temperature DewPoint {
+			get { return dewPoint; }
+		}
+
+		public double PressureHg {
+			get { return pressureHg; }
+		}
+
+		public double PressureMb {
+			get { return pressureMb; }
+		}
+
+		public Weather Weather {
+			get { return weather; }
+		}
 		
 		public Metar (string metar)
 		{
 			this.metar = metar;
 			Parse ();
 		}
-
+		
 		void Parse (string metar)
 		{
 			this.metar = metar;
@@ -347,6 +699,11 @@ namespace Desklets.Weather
 
 		void DoPart (string part)
 		{
+			if (part == "CAVOK") {
+				this.clouds = new Clouds ();
+				return;
+			}
+			
 			Match match;
 			match = Regexps.StationId.Match (part);
 			if (match.Success) {
@@ -400,7 +757,7 @@ namespace Desklets.Weather
 				visibility = new Visibility (groups [0], groups [1]);
 				return;
 			}
-
+			
 			match = Regexps.Clouds.Match (part);
 			if (match.Success) {
 				groups = GetGroups (match);
@@ -412,18 +769,9 @@ namespace Desklets.Weather
 			match = Regexps.TempAndDew.Match (part);
 			if (match.Success) {
 				groups = GetGroups (match);
-
-				try {
-					temperature = Convert.ToDouble (groups [0].Replace ("M", "-"));
-				} catch {
-					temperature = Double.MinValue;
-				}
-
-				try {
-					dewPoint = Convert.ToDouble (groups [1].Replace ("M", "-"));
-				} catch {
-					dewPoint = Double.MinValue;
-				}
+				
+				temperature = new Temperature (groups [0].Replace ("M", "-"));
+				dewPoint = new Temperature (groups [1].Replace ("M", "-"));
 				return;
 			}
 
@@ -451,6 +799,15 @@ namespace Desklets.Weather
 				return;
 			}
 
+			match = Regexps.Weather.Match (part);
+			if (match.Success) {
+				groups = GetGroups (match);
+
+				weather = new Weather (groups [0], groups [1], groups [2],
+						       groups [3], groups [4], groups [5]);
+				return;
+			}
+			
 			// We ignore all the other parts
 		}
 		
