d889592ee2b067314f749a15c7c58eab6418a32b
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-06-26 01:49:59 +0000

    * clock.xaml: Clean clock design. 	* clock.cs: Remove some unused code.
    2007-06-25  Everaldo Canuto  <ecanuto@novell.com>
    
    
    svn path=/trunk/moon/; revision=80756

--- desklet/clock/clock.cs
+++ desklet/clock/clock.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
-namespace Golem.Desklet
+namespace Desklets
 {
 	public class Clock : Canvas 
 	{
@@ -19,31 +19,25 @@ namespace Golem.Desklet
 
 		void UpdateTime (object status)
 		{
-			Console.WriteLine ("UpdateTime");
 			DateTime now = DateTime.Now;
 
 			secondsHand.Angle = now.Second * 6;
-			minuteHand.Angle = now.Minute * 6;
-			hourHand.Angle = now.Hour * 30;
+			minuteHand.Angle  = now.Minute * 6;
+			hourHand.Angle    = now.Hour   * 30;
 		}
 
 		public void PageLoaded (object o, EventArgs e)
 		{
-			Console.WriteLine ("page loaded");
 			secondsHand = FindName ("secondsHand") as RotateTransform;
-			minuteHand = FindName ("minuteHand") as RotateTransform;
-			hourHand = FindName ("hourHand") as RotateTransform;
+			minuteHand  = FindName ("minuteHand")  as RotateTransform;
+			hourHand    = FindName ("hourHand")    as RotateTransform;
 
 			// We can use 
 			//string imagesDir = IO.Path.Combine (Environment.CurrentDirectory, "images");
 
-			if (secondsHand == null || minuteHand == null || hourHand == null) {
-				// TODO: Should communicate the error to the desklet host
-				Console.WriteLine ("Missing elements in the desklet xaml file");
+			if (secondsHand == null || minuteHand == null || hourHand == null)
 				return;
-			}
 
-			Console.WriteLine ("Starting timer");
 			AutoResetEvent autoEvent = new AutoResetEvent(false);
 			timer = new Timer (new TimerCallback (UpdateTime), autoEvent, 5, 1000);
 		}
