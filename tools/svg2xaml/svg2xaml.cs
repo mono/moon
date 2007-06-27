e899444dd03f20b24a2cb816d1fe480b29154ae2
Andreia Gaita <avidigal@novell.com>
2007-06-27 17:52:26 +0000

    2007-06-27  Andreia Gaita  <avidigal@novell.com>
    * sv2xaml.cs, svg2xaml.mdp: command-line tool to
    convert svg to xaml
    * svg2xaml-gui.mdp: new name of the gui app and 
    project 
    * svg2xaml.mds: add a nice solution for both apps,
    while I'm at it.
    
    svn path=/trunk/moon/; revision=80896

---
+++ tools/svg2xaml/svg2xaml.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+
+namespace svg2xaml
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{		
+			if (args.Length == 0)
+			{
+				Console.WriteLine ("Usage: mono svg2xaml.exe input.svg [output.xaml].");
+				Console.WriteLine ("If you don't specify an output file, it will be created");
+				Console.WriteLine ("as 'input.svg.xaml' in the location of the svg file.");
+				return;
+			}
+			
+			string input = args[0];
+			if (!File.Exists (input))
+			{
+				Console.WriteLine ("Error: file {0} does not exist.");
+				Console.WriteLine ("This might work better if you try and convert an svg that actually exists.");
+				return;
+			}
+			
+			string output = input + ".xaml";
+			if (args.Length > 1)
+				output = args[1];
+			
+			Console.WriteLine ("Converting file {0} into {1}...", input, output);
+			
+			XmlDocument xsltdoc = new XmlDocument();
+			try
+			{
+				xsltdoc.Load ("svg2xaml.xslt");
+			}
+			catch (FileNotFoundException e)
+			{
+				Console.WriteLine ("Ooops, can't find the transformation stylesheet. That won't work :p");
+				return;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine (ex.Message);
+				return;
+			}
+			
+			try
+			{
+				XslTransform t = new XslTransform();
+				t.Load (xsltdoc);
+				t.Transform (input, output);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine ("Houston, we have a problem!");
+				Exception a = ex;
+				Console.WriteLine (a.StackTrace);
+				while (a != null)
+				{
+					Console.WriteLine (a.Message);
+					a = ex.InnerException;
+				}
+			}
+			
+			Console.WriteLine ("Conversion done, file {0} created. Have a nice day :)", output);
+		}
+	}
+}
