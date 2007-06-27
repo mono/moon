ab6737de4bf218c5fee0a2355efce2cb1b9e69be
Andreia Gaita <avidigal@novell.com>
2007-06-27 17:11:11 +0000

    2007-06-27  Andreia Gaita  <avidigal@novell.com>
    * svg2xaml.mdp, Makefile, Main.cs, MainWindow.cs
      gtk-gui/generated.cs, gtk-gui/gui.stetic,
      gtk-gui/MainWindow.cs: 
      
      Little svg2xaml application to load and convert a 
      svg file to a xaml file. Note: the mdp doesn't use 
      the makefile.
    
    svn path=/trunk/moon/; revision=80888

---
+++ tools/svg2xaml/Main.cs
@@ -0,0 +1,26 @@
+
+// project created on 27/06/2007 at 00:15
+using System;
+using Gtk;
+using Gtk.Moonlight;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Input;
+using System.IO;
+using System.Windows.Media.Animation;
+
+namespace svg2xaml
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
---
+++ tools/svg2xaml/MainWindow.cs
@@ -0,0 +1,95 @@
+
+using System;
+using Gtk;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+using Gtk.Moonlight;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+
+
+public partial class MainWindow: Gtk.Window
+{	
+	StringBuilder svg;
+	StringBuilder xaml;
+	string svgFilename;
+	static GtkSilver silver;
+	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		svg = new StringBuilder();
+		xaml = new StringBuilder();
+		silver = new GtkSilver (400, 400);
+		hbox1.PackStart (silver, true, true, 0);
+
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected virtual void OnChooseFile (object sender, System.EventArgs e)
+	{
+		Gtk.FileChooserDialog fc = new FileChooserDialog (
+		"Choose svg file", this, FileChooserAction.Open,
+		"Cancel", ResponseType.Cancel, 
+		"Open", ResponseType.Accept);
+		
+		if (fc.Run() == (int)ResponseType.Accept)
+		{
+			svg.Remove (0, svg.Length);
+			svgFilename = fc.Filename;
+			FileStream file = File.OpenRead(svgFilename);
+			using (StreamReader sr = new StreamReader (file))
+				svg.Append(sr.ReadToEnd ());
+			file.Close();
+			RefreshSvg();
+		}
+		fc.Destroy();
+	}
+
+	protected virtual void OnConvert (object sender, System.EventArgs e)
+	{
+		XmlDocument xsltdoc = new XmlDocument();
+		xsltdoc.Load ("svg2xaml.xslt");
+		XslTransform t = new XslTransform();
+		t.Load (xsltdoc);
+		t.Transform (svgFilename, svgFilename + ".xaml");
+		FileStream file = File.OpenRead(svgFilename + ".xaml");
+		using (StreamReader sr = new StreamReader (file))
+			xaml.Append(sr.ReadToEnd ());
+		file.Close();
+		RefreshXaml();
+	}
+	
+	void RefreshSvg ()
+	{
+		Gdk.Pixbuf pixbuf = Rsvg.Pixbuf.FromFile (svgFilename);
+		image1.Pixbuf = pixbuf;
+	}
+	
+	void RefreshXaml ()
+	{
+		DependencyObject d = XamlReader.Load (xaml.ToString());
+		if (d == null){
+			Console.Error.WriteLine ("No dependency object returned from XamlReader");
+			return;
+		}
+		
+		if (!(d is Canvas)){
+			Console.Error.WriteLine ("No Canvas as root");
+			return;
+		}
+		Canvas canvas = (Canvas) d;
+		silver.Attach (canvas);
+		
+		
+	}
+}
