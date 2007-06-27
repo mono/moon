621ac100622947822407b1c10570104b0b6e9d95
Everaldo Canuto <everaldo@mono-cvs.ximian.com>
2007-06-27 16:59:29 +0000

    svn path=/trunk/moon/; revision=80887

---
+++ desklet/glassyslides/slideshow.cs
@@ -0,0 +1,67 @@
+/*
+ * monitor.cs: Glassy network monitor desklet.
+ *
+ * Author:
+ *   Everaldo Canuto (ecanuto@novell.com)
+ *
+ * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ * 
+ */
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Desklets
+{
+	public class SlideShow : Canvas 
+	{
+		int image_index = 1;
+
+		Storyboard change;
+		Storyboard fadein;
+		Storyboard fadeout;
+		ImageBrush image;
+
+		public void ChangePicture (object o, EventArgs e)
+		{
+			image_index = (image_index < 8) ? image_index + 1 : 1;
+			
+			string uri = "data/image0" + image_index.ToString() + ".jpg";
+			image.SetValue (ImageBrush.ImageSourceProperty, uri);
+
+			//fadein.Begin ();
+			change.Begin ();
+		}
+
+		public void FadeInPicture (object o, EventArgs e)
+		{
+			fadeout.Begin ();
+		}
+
+		public void FadeOutPicture (object o, EventArgs e)
+		{
+			change.Begin ();
+		}
+
+		public void PageLoaded (object o, EventArgs e)
+		{
+			change  = FindName ("change")  as Storyboard;
+			fadein  = FindName ("fadein")  as Storyboard;
+			fadeout = FindName ("fadeout") as Storyboard;
+			image   = FindName ("image")   as ImageBrush;
+
+			change.Completed  += new EventHandler (ChangePicture);
+			fadein.Completed  += new EventHandler (FadeInPicture);
+			fadeout.Completed += new EventHandler (FadeOutPicture);
+
+			change.Begin ();
+		}
+	}
+}
