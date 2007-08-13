c2474e4a554fc0cd9101fd16f7ea79d815d685da
Chris Toshok <toshok@novell.com>
2007-08-13 22:51:51 +0000

    take only 1 cl arg
    svn path=/trunk/moon/; revision=84019

--- test/harness/moon-harness.cpp
+++ test/harness/moon-harness.cpp
@@ -2,8 +2,8 @@
 #include <libmoon.h>
 #include <cairo.h>
 
-#define TEST_WIDTH 640
-#define TEST_HEIGHT 480
+#define TEST_WIDTH 400
+#define TEST_HEIGHT 400
 
 void
 runTest (const char *xaml_file, const char *output_prefix)
@@ -11,16 +11,16 @@ runTest (const char *xaml_file, const char *output_prefix)
 	Surface *s = new Surface (TEST_WIDTH, TEST_HEIGHT);
 	Type::Kind type;
 
-	cairo_surface_t* surf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32,
-							    TEST_WIDTH, TEST_HEIGHT);
-
 	DependencyObject *dob = xaml_create_from_file (xaml_file, true, &type);
 
 	s->Attach ((UIElement*)dob);
 
+	cairo_surface_t* surf = cairo_image_surface_create (CAIRO_FORMAT_ARGB32,
+							    s->GetWidth (), s->GetHeight ());
+
 	cairo_t *cr = cairo_create (surf);
 
-	s->Paint (cr, 0, 0, TEST_WIDTH, TEST_HEIGHT);
+	s->Paint (cr, 0, 0, s->GetWidth(), s->GetHeight());
 
 	cairo_surface_write_to_png (surf, output_prefix);
 
@@ -38,6 +38,13 @@ main (int argc, char **argv)
 	gdk_threads_init ();
 	runtime_init ();
 
+	char *test = argv[1];
 
-	runTest (argv[1], argv[2]);
+	char *png = g_strconcat (g_path_get_basename (test),
+				 ".png", NULL);
+
+	
+	runTest (test, png);
+
+	g_free (png);
 }
