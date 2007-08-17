be6c98f8b38010007722738f3bdd1f2f46eac707
Chris Toshok <toshok@novell.com>
2007-08-17 18:48:11 +0000

    add downloader support from demo.cpp, so we can dtrt wrt images
    svn path=/trunk/moon/; revision=84316

--- test/harness/xaml2png.cpp
+++ test/harness/xaml2png.cpp
@@ -1,9 +1,102 @@
 #include <gtk/gtk.h>
 #include <libmoon.h>
 #include <cairo.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
 #define TEST_WIDTH 400
 #define TEST_HEIGHT 400
+
+char *testdir;
+
+static gpointer downloader_create_state (Downloader* dl);
+static void downloader_destroy_state (gpointer data);
+static void downloader_open (const char *verb, const char *uri, gpointer state);
+static void downloader_send (gpointer state);
+static void downloader_abort (gpointer state);
+static void downloader_abort (gpointer state);
+
+class FileDownloadState {
+ public:
+	FileDownloadState (Downloader *dl) : uri(NULL), downloader(dl) { }
+
+	virtual ~FileDownloadState () { Close (); }
+	size_t size;
+	char *uri;
+
+	void Abort () { Close (); }
+	char* GetResponseText (char *fname, char* PartName) { return NULL; } // XXX
+	void Open (const char *verb, const char *URI)
+	{
+		uri = g_strdup (URI);
+
+		int fd = open (uri, O_RDONLY);
+		if (fd == -1) {
+			if (!g_path_is_absolute (uri)) {
+				g_free (uri);
+				uri = g_strconcat (testdir, G_DIR_SEPARATOR_S, URI, NULL);
+				fd = open (uri, O_RDONLY);
+			}
+		  
+			if (fd == -1) {
+				const char *msg = g_strerror (errno);
+				printf ("downloader failed to open %s: %s\n", URI, msg);
+				downloader_notify_error (downloader, msg);
+				return;
+			}
+		}
+
+		struct stat sb;
+		fstat (fd, &sb);
+		close (fd);
+		size = sb.st_size;
+		downloader_notify_size (downloader, size);
+	}
+
+	void Send () {
+		if (uri != NULL)
+			downloader_notify_finished (downloader, uri);
+	}
+
+	void Close ()
+	{
+		g_free (uri);
+	}
+ private:
+	Downloader *downloader;
+};
+
+static gpointer
+downloader_create_state (Downloader *dl)
+{
+	return new FileDownloadState (dl);
+}
+
+static void
+downloader_destroy_state (gpointer data)
+{
+	delete ((FileDownloadState*)data);
+}
+
+static void
+downloader_open (const char *verb, const char *uri, gpointer state)
+{
+	((FileDownloadState*)state)->Open (verb, uri);
+}
+
+static void
+downloader_send (gpointer state)
+{
+	((FileDownloadState*)state)->Send ();
+}
+
+static void
+downloader_abort (gpointer state)
+{
+	((FileDownloadState*)state)->Abort ();
+}
+
 
 void
 runTest (const char *xaml_file, const char *output_prefix)
@@ -39,9 +132,18 @@ main (int argc, char **argv)
 	g_thread_init (NULL);
 	gdk_threads_init ();
 	runtime_init ();
+
+	downloader_set_functions (downloader_create_state,
+				  downloader_destroy_state,
+				  downloader_open,
+				  downloader_send,
+				  downloader_abort);
 	
 	char *test = argv[1];
 	char *png = argv[2];
 
+	testdir = g_path_get_dirname (test);
+	if (!strcmp ("", testdir))
+		testdir = g_get_current_dir();
 	runTest (test, png);
 }
