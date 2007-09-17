6ca17ca9d7fabd503200b6d020bf4648b757e751
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-09-17 14:53:28 +0000

    Load the mono runtime by default.
    svn path=/trunk/moon/; revision=85885

--- plugin/moonlight.h
+++ plugin/moonlight.h
@@ -42,7 +42,7 @@
 #define PLUGIN_DESCRIPTION  "Novell Moonlight is Mono's Free/Open Source implementation of SilverLight"
 #define MIME_TYPES_HANDLED  "application/x-silverlight:xaml:Novell MoonLight"
 
-#define INCLUDE_MONO_RUNTIME 0
+#define INCLUDE_MONO_RUNTIME 1
 
 #define MAX_STREAM_SIZE 65536
 
