01a42705b407082f8694a37a2d1ef405eed1db78
Jb Evain <jbevain@gmail.com>
2007-10-04 23:20:24 +0000

    revert for now, it breaks the Stopped status for playlists
    svn path=/trunk/moon/; revision=86932

--- src/media.cpp
+++ src/media.cpp
@@ -109,8 +109,6 @@ MediaSource::GetMediaPlayer ()
 bool
 MediaSource::Open ()
 {
-	media_element_set_current_state (element, "Opening");
-
 	if (!OpenInternal ()) {
 		media_element_set_can_seek (element, false);
 		media_element_set_can_pause (element, false);
