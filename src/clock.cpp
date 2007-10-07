1f7f576cf222e51cd4ef196dc7563fa93b9df5b3
Chris Toshok <toshok@novell.com>
2007-10-07 18:23:43 +0000

    svn path=/trunk/moon/; revision=87055

--- src/clock.cpp
+++ src/clock.cpp
@@ -468,7 +468,7 @@ Clock::Tick ()
 #if CLOCK_DEBUG
 		printf ("+ clock %p (%s) duration is %lld, and new_time == %lld\n", this, timeline->GetName(), duration_timespan, new_time);
 #endif
-		if (new_time >= duration_timespan) {
+		if (!is_reversed && new_time >= duration_timespan) {
 #if CLOCK_DEBUG
 			printf ("+ clock %p (%s) hit its duration\n", this, timeline->GetName());
 #endif
@@ -507,7 +507,7 @@ Clock::Tick ()
 				}
 			}
 		}
-		else if (new_time <= 0) {
+		else if (is_reversed && new_time <= 0) {
 			is_reversed = false;
 			new_time = -new_time;
 
@@ -759,7 +759,7 @@ ClockGroup::Tick ()
 		   otherwise just call its Tick
 		   method */
 		Clock *c = (Clock*)l->data;
-		if (c->GetClockState() != Clock::Stopped) {
+		if (c->GetClockState() != Clock::Stopped && c->GetNewClockState() != Clock::Stopped) {
 			c->Tick ();
 		}
 		else if ((c->GetClockState() == Clock::Stopped) && (!c->GetHasStarted() && c->GetBeginTime () <= current_time)) {
