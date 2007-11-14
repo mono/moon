fc37d70e08aea62bc266f8f73f2547f4c6f159f2
Sebastien Pouliot <sebastien@ximian.com>
2007-11-14 14:33:08 +0000

    openfile.cpp: Added support for Filter and FilterIndex.
    svn path=/trunk/moon/; revision=89611

--- src/openfile.cpp
+++ src/openfile.cpp
@@ -1,8 +1,9 @@
 /*
  * openfile.cpp: File open interfaces
  *
- * Author:
+ * Authors:
  *   Miguel de Icaza (miguel@novell.com)
+ *   Sebastien Pouliot  <sebastien@ximian.com>
  *
  * Copyright 2007 Novell, Inc. (http://www.novell.com)
  *
@@ -12,8 +13,6 @@
 #include <config.h>
 #include <stdlib.h>
 #include <string.h>
-#include <malloc.h>
-#include <math.h>
 
 #include <gtk/gtk.h>
 #include "openfile.h"
@@ -26,13 +25,51 @@ open_file_dialog_show (const char *title, bool multsel, const char *filter, int 
 					    GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
 					    GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);
 
-	gtk_file_chooser_set_select_multiple (GTK_FILE_CHOOSER(widget), multsel ? TRUE : FALSE);
+	GtkFileChooser *chooser = GTK_FILE_CHOOSER(widget);
+	if (filter && (strlen (filter) > 1)) {
+		char **filters = g_strsplit (filter, "|", 0);
+
+		// to be valid (managed code) we know we have an even number of items
+		// (if not we're still safe by dropping the last one)
+		int pos = 0;
+		int n = g_strv_length (filters) >> 1;
+		for (int i=0; i < n; i++) {
+			char *name = g_strstrip (filters[pos++]);
+			if (strlen (name) < 1)
+				continue;
+
+			char *pattern = g_strstrip (filters[pos++]);
+			if (strlen (pattern) < 1)
+				continue;
+
+			GtkFileFilter *ff = gtk_file_filter_new ();
+			gtk_file_filter_set_name (ff, g_strdup (name));
+			// there can be multiple patterns in a single string
+			if (g_strrstr (pattern, ";")) {
+				int n = 0;
+				char **patterns = g_strsplit (pattern, ";", 0);
+				while (char *p = patterns[n++])
+					gtk_file_filter_add_pattern (ff, g_strdup (p));
+				g_strfreev (patterns);
+			} else {
+				// or a single one
+				gtk_file_filter_add_pattern (ff, g_strdup (pattern));
+			}
+			gtk_file_chooser_add_filter (chooser, ff);
+			// idx (FilterIndex) is 1 (not 0) based
+			if (i == (idx - 1))
+				gtk_file_chooser_set_filter (chooser, ff);
+		}
+		g_strfreev (filters);
+	}
+
+	gtk_file_chooser_set_select_multiple (chooser, multsel ? TRUE : FALSE);
 
 	gint code = gtk_dialog_run (GTK_DIALOG (widget));
 	char **ret = NULL;
 
 	if (code == GTK_RESPONSE_ACCEPT){
-		GSList *k, *l = gtk_file_chooser_get_filenames (GTK_FILE_CHOOSER (widget));
+		GSList *k, *l = gtk_file_chooser_get_filenames (chooser);
 		int i, count = g_slist_length (l);
 
 		ret = g_new (char *, count + 1);
