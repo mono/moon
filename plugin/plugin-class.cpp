624d66867b886511bc24b425ddb6cdaf3069fbe9
Jb Evain <jbevain@gmail.com>
2007-11-20 15:02:03 +0000

    revert that, it's not precise enough
    svn path=/trunk/moon/; revision=89979

--- plugin/plugin-class.cpp
+++ plugin/plugin-class.cpp
@@ -1633,8 +1633,6 @@ MoonlightContentObject::Invoke (int id, NPIdentifier name,
 	
 	switch (id) {
 	case MoonId_FindName: {
-		NULL_TO_NPVARIANT (*result);
-
 		if (!argCount)
 			return true;
 		
@@ -1654,15 +1652,11 @@ MoonlightContentObject::Invoke (int id, NPIdentifier name,
 	}
 
 	case MoonId_CreateObject:
-		NULL_TO_NPVARIANT (*result);
-
 		// not implemented yet
 		DEBUG_WARN_NOTIMPLEMENTED ("content.createObject");
 		return true;
 
 	case MoonId_CreateFromXaml: {
-		NULL_TO_NPVARIANT (*result);
-
 		if (argCount < 1)
 			return true;
 
@@ -1678,9 +1672,8 @@ MoonlightContentObject::Invoke (int id, NPIdentifier name,
 		if (dep != NULL) {
 			depobj = EventObjectCreateWrapper (instance, dep);
 			dep->unref ();
-		} else
-			return true;
-
+		}
+		
 		OBJECT_TO_NPVARIANT (depobj, *result);
 		return true;
 	}
