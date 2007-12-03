mono/moon

commit 6e219f9d8b77ac1addd2b809e454f6d456869ecb

i2007-12-03  Larry Ewing  <lewing@novell.com>
	* brush.cpp (RadialGradientBrush::SetupBrush): add debug spew
	if we can't invert the matrix but still draw someting.

svn path=/trunk/moon/; revision=90549

src/brush.cpp
@@ -576,11 +576,15 @@ RadialGradientBrush::SetupBrush (cairo_t *cr, UIElement *uielement, double width
 		// TODO - optimization, check for empty/identity matrix too ?
 		cairo_matrix_multiply (&matrix, &matrix, &tm);
 	}
-	cairo_matrix_invert (&matrix);
+	cairo_status_t status = cairo_matrix_invert (&matrix);
+	if (status != CAIRO_STATUS_SUCCESS) {
+		printf ("Moonlight: Error inverting matrix falling back\n");
+		cairo_matrix_init_identity (&matrix);
+	}
 	cairo_pattern_set_matrix (pattern, &matrix);
-	
 	GradientBrush::SetupGradient (pattern, uielement);
 	
+	cairo_set_matrix (cr, &matrix);
 	cairo_set_source (cr, pattern);
 	cairo_pattern_destroy (pattern);
 }

commit 57562ab8d0237c729e8bca29d4b9b538f36eaa85

brush.cpp: Fix breakage introduced in r90549 (the matrix is already set on the pattern).
svn path=/trunk/moon/; revision=90576

src/brush.cpp
@@ -583,8 +583,7 @@ RadialGradientBrush::SetupBrush (cairo_t *cr, UIElement *uielement, double width
 	}
 	cairo_pattern_set_matrix (pattern, &matrix);
 	GradientBrush::SetupGradient (pattern, uielement);
-	
-	cairo_set_matrix (cr, &matrix);
+
 	cairo_set_source (cr, pattern);
 	cairo_pattern_destroy (pattern);
 }