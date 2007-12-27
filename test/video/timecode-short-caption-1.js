44cc6d1390fe457bf13ffdd4cf79e71599ac405f
Rolf Bjarne Kvinge <RKvinge@novell.com>
2007-12-27 12:55:46 +0000

    Remove unnecessary files.
    svn path=/trunk/moon/; revision=91933

--- test/video/timecode-short-caption-1.js
+++
@@ -1,8 +0,0 @@
-
-tests.push ("timecode-short-caption-1");
-
-results ["timecode-short-caption-1"] = [
-"MediaOpened",
-"1 MarkerReached: ms = 1004, type = caption, text = one sec",
-"MediaEnded"
-];
