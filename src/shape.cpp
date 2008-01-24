450056bcc55c42688589bf18e6ea546b86ead50a
Sebastien Pouliot <sebastien@ximian.com>
2008-01-24 21:35:36 +0000

    2008-01-24  Sebastien Pouliot  <sebastien@ximian.com>
    	* shape.cpp: Avoid calls to cairo_in_[stroke|fill] if no stroke 
    	(or fill) are present for the shape.
    
    
    svn path=/trunk/moon/; revision=93843

--- src/shape.cpp
+++ src/shape.cpp
@@ -389,8 +389,6 @@ Shape::InsideObject (cairo_t *cr, double x, double y)
 	if (!InsideClip (cr, x, y))
 		return false;
 
-	bool ret = false;
-
 	cairo_save (cr);
 	// don't do the operation but do consider filling
 	DoDraw (cr, false);
@@ -399,8 +397,8 @@ Shape::InsideObject (cairo_t *cr, double x, double y)
 
 	uielement_transform_point (this, &nx ,&ny);
 
-	if (cairo_in_stroke (cr, nx, ny) || (CanFill () && cairo_in_fill (cr, nx, ny)))
-		ret = TRUE;
+	// don't check in_stroke without a stroke or in_fill without a fill (even if it can be filled)
+	bool ret = ((stroke && cairo_in_stroke (cr, nx, ny)) || (fill && CanFill () && cairo_in_fill (cr, nx, ny)));
 	
 	cairo_new_path (cr);
 
