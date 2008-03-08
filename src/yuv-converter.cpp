9f022f06006ac075789b36fd33a300339437b9b6
Geoff Norton <grompf@sublimeintervention.com>
2008-03-08 23:16:16 +0000

    * yuv-converter.cpp:  Remove some code duplication and refactor 	to support register starved implementations.
    2008-03-08  Geoff Norton  <gnorton@novell.com>
    
    
    svn path=/trunk/moon/; revision=97787

--- src/yuv-converter.cpp
+++ src/yuv-converter.cpp
@@ -24,10 +24,6 @@ G_END_DECLS
 
 #if HAVE_MMX
 /*
- * R = Y + 1.403V
- * G = Y - 0.344U - 0.714V
- * B = Y + 1.770U
- *
  * R V coefficient = 1.403*64 == 89.792 =~ 90 == 0x5a
  * G U cofficient = -0.344*64 == -22.016 =~ -22 == 0xffe9
  * G V cofficient = -0.714*64 == -45.696 =~ -46 == 0xffd1
@@ -43,6 +39,82 @@ static const uint64_t mmx_table [6] __attribute__ ((aligned (16))) = {
 									0x0080008000800080ULL, /* 128 */
 									0xFFFFFFFFFFFFFFFFULL, /* Alpha channel */
 };
+
+/* We process 2 lines per so we fold this into a define:
+ * Register usage:
+ * 	%mm2: Rcoeff * V
+ * 	%mm3: GUcoeff * U + GVcoeff * V
+ * 	%mm1: Bcoeff * U 
+ * This will read 8 pels from y_plane and do the following math on them:
+ * 
+ * R = Y + 1.403V
+ * G = Y - 0.344U - 0.714V
+ * B = Y + 1.770U
+ * 
+ * It then packs BGRA32 and outputs to dest
+ */
+#define YUV2RGB_MMX(y_plane, dest) do { \
+			__asm__ __volatile__ ( \
+				"movq (%0), %%mm0;"		/* mm0 [Y0 Y1 Y2 Y3 Y4 Y5 Y6 Y7] */	\
+				"movq %%mm0, %%mm4;"		/* mm4 == mm0 */			\
+													\
+				"psllw $8, %%mm0;"		/* mm0 [00 Y0 00 Y2 00 Y4 00 Y6] */	\
+				"psrlw $8, %%mm0;"		/* mm0 [Y0 00 Y2 00 Y4 00 Y6 00] */	\
+				"psrlw $8, %%mm4;"		/* mm4 [Y1 00 Y3 00 Y5 00 Y7 00] */	\
+													\
+				"movq %%mm2, %%mm6;"							\
+				"movq %%mm3, %%mm7;"							\
+				"movq %%mm1, %%mm5;"							\
+													\
+				"paddsw %%mm0, %%mm2;"		/* CY[even] + DR */			\
+				"paddsw %%mm0, %%mm3;"		/* CY[even] + DG */			\
+				"paddsw %%mm0, %%mm1;"		/* CY[even] + DB */			\
+													\
+				"paddsw %%mm4, %%mm6;"		/* CY[odd] + DR */			\
+				"paddsw %%mm4, %%mm7;"		/* CY[odd] + DG */			\
+				"paddsw %%mm4, %%mm5;"		/* CY[odd] + DB */			\
+													\
+				"packuswb %%mm2, %%mm2;"	/* Clamp RGB to [0-255] */		\
+				"packuswb %%mm3, %%mm3;"						\
+				"packuswb %%mm1, %%mm1;"						\
+													\
+				"packuswb %%mm6, %%mm6;"						\
+				"packuswb %%mm7, %%mm7;"						\
+				"packuswb %%mm5, %%mm5;"						\
+													\
+				"punpcklbw %%mm6, %%mm2;"	/* mm2 [R0 R1 R2 R3 R4 R5 R6 R7] */	\
+				"punpcklbw %%mm7, %%mm3;"	/* mm3 [G0 G1 G2 G3 G4 G5 G6 G7] */	\
+				"punpcklbw %%mm5, %%mm1;"	/* mm1 [B0 B1 B2 B3 B4 B5 B6 B7] */	\
+													\
+				"movq %%mm2, %%mm5;"		/* copy RGB */				\
+				"movq %%mm3, %%mm7;"							\
+				"movq %%mm1, %%mm6;"							\
+													\
+				"movq 40(%2), %%mm4;"							\
+				"punpcklbw %%mm2, %%mm1;"	/* mm1 [B0 R0 B1 R1 B2 R2 B3 R3] */	\
+				"punpcklbw %%mm4, %%mm3;"	/* mm4 [G0 FF G1 FF G2 FF G3 FF] */	\
+													\
+				"movq %%mm1, %%mm4;"		/* mm3 [G0 FF G1 FF G2 FF G3 FF] */	\
+													\
+				"punpcklbw %%mm3, %%mm1;"	/* mm2 [B0 G0 R0 FF B1 G1 R1 FF] */	\
+				"punpckhbw %%mm3, %%mm4;"	/* mm3 [B2 G2 R2 FF B3 G3 R3 FF] */	\
+													\
+				"movq %%mm1, (%1);"		/* output BGRA[0] BGRA[1] */ 		\
+				"movq %%mm4, 8(%1);"		/* output BGRA[2] BGRA[3] */ 		\
+													\
+				"movq 40(%2), %%mm4;"							\
+				"punpckhbw %%mm5, %%mm6;"	/* mm5 [B4 R4 B5 R6 B7 R7 B8 R8] */	\
+				"punpckhbw %%mm4, %%mm7;"	/* mm6 [G4 FF G5 FF G6 FF G7 FF] */	\
+													\
+				"movq %%mm6, %%mm4;"		/* mm7 [G4 FF G5 FF G6 FF G7 FF] */	\
+													\
+				"punpcklbw %%mm7, %%mm6;"	/* mm6 [B4 G4 R4 FF B5 G5 R5 FF] */	\
+				"punpckhbw %%mm7, %%mm4;"	/* mm7 [B6 G6 R6 FF B7 G7 R7 FF] */	\
+													\
+				"movq %%mm6, 16(%1);"		/* output BGRA[4] BGRA[5] */ 		\
+				"movq %%mm4, 24(%1);"		/* output BGRA[6] BGRA[7] */ 		\
+				: : "r" (y_plane), "r" (dest), "r" (&mmx_table));			\
+		} while (0);
 #endif
 
 static inline void YUV444ToBGRA(uint8_t Y, uint8_t U, uint8_t V, uint8_t *dst)
@@ -124,162 +196,49 @@ YUVConverter::Convert (uint8_t *src[], int srcStride[], int srcSlideY, int srcSl
 			__asm__ __volatile__ (
 				"psllq $64, %%mm7;"		/* mm7 [00 00 00 00 00 00 00 00] */ 
 
-				"movd (%2), %%mm1;"		/* mm1 [U0 U1 U2 U3 00 00 00 00] */
-				"movd (%3), %%mm2;"		/* mm2 [V0 V1 V2 V3 00 00 00 00] */
+				"movd (%0), %%mm1;"		/* mm1 [U0 U1 U2 U3 00 00 00 00] */
+				"movd (%1), %%mm2;"		/* mm2 [V0 V1 V2 V3 00 00 00 00] */
 
 				"punpcklbw %%mm7, %%mm1;"	/* mm1 [U0 00 U1 00 U2 00 U3 00] */	
 				"punpcklbw %%mm7, %%mm2;"	/* mm2 [V0 00 V1 00 V2 00 V3 00] */	
 
-				"movq 32(%7), %%mm7;"
+				"movq 32(%3), %%mm7;"
 				"psubsw %%mm7, %%mm1;"		/* U = U - 128 */
 				"psubsw %%mm7, %%mm2;"		/* V = V - 128 */
 
 				"movq %%mm1, %%mm3;"		/* mm3 = mm1 */
 				"movq %%mm2, %%mm4;"		/* mm4 = mm1 */
 
-				"movq (%7), %%mm7;"
+				"movq (%3), %%mm7;"
 				"pmullw %%mm7, %%mm2;"		/* calculate Dred */
 				"psraw $6, %%mm2;"		/* Dred = Dred / 64 */
 
-				"movq 16(%7), %%mm7;"
+				"movq 16(%3), %%mm7;"
 				"pmullw %%mm7, %%mm3;"		/* calculate Ugreen */ 
 				"psraw $6, %%mm3;"		/* Ugreen = Ugreen / 64 */
-				"movq 8(%7), %%mm7;"
+				"movq 8(%3), %%mm7;"
 				"pmullw %%mm7, %%mm4;"		/* calculate Vgreen */ 
 				"psraw $6, %%mm4;"		/* Vgreen = Vgreen / 64 */
 				"paddsw %%mm4, %%mm3;"		/* Dgreen = Ugreen + Vgreen */
 
-				"movq 24(%7), %%mm7;"
+				"movq 24(%3), %%mm7;"
 				"pmulhw %%mm7, %%mm1;"		/* calculate Dblue */
 				"psraw $6, %%mm1;"		/* Dblue = Dblue / 64 */
 
-				"movq %%mm2, (%4);"		/* backup Dred */
-				"movq %%mm3, 8(%4);"		/* backup Dgreen */
-				"movq %%mm1, 16(%4);"		/* backup Dblue */
+				"movq %%mm2, (%2);"		/* backup Dred */
+				"movq %%mm3, 8(%2);"		/* backup Dgreen */
+				"movq %%mm1, 16(%2);"		/* backup Dblue */
+				: : "r" (u_plane), "r" (v_plane), "r" (&rgb_uv), "r" (&mmx_table));
 
-				"movq (%0), %%mm0;"		/* mm0 [Y0 Y1 Y2 Y3 Y4 Y5 Y6 Y7] */
-				"movq %%mm0, %%mm4;"		/* mm4 == mm0 */
+			YUV2RGB_MMX(y_row1, dest_row1);
 
-				"psllw $8, %%mm0;"		/* mm0 [00 Y0 00 Y2 00 Y4 00 Y6] */
-				"psrlw $8, %%mm0;"		/* mm0 [Y0 00 Y2 00 Y4 00 Y6 00] */
-				"psrlw $8, %%mm4;"		/* mm4 [Y1 00 Y3 00 Y5 00 Y7 00] */
+			__asm__ __volatile__ (
+				"movq (%0), %%mm2;"		/* restore Dred */
+				"movq 8(%0), %%mm3;"		/* restore Dgreen */
+				"movq 16(%0), %%mm1;"		/* restore Dblue */
+				: : "r" (&rgb_uv));
 
-				"movq %%mm2, %%mm6;"
-				"movq %%mm3, %%mm7;"
-				"movq %%mm1, %%mm5;"
-
-				"paddsw %%mm0, %%mm2;"		/* CY[even] + DR */
-				"paddsw %%mm0, %%mm3;"		/* CY[even] + DG */
-				"paddsw %%mm0, %%mm1;"		/* CY[even] + DB */
-				
-				"paddsw %%mm4, %%mm6;"		/* CY[odd] + DR */
-				"paddsw %%mm4, %%mm7;"		/* CY[odd] + DG */
-				"paddsw %%mm4, %%mm5;"		/* CY[odd] + DB */
-
-				"packuswb %%mm2, %%mm2;"	/* Clamp RGB to [0-255] */
-				"packuswb %%mm3, %%mm3;"	
-				"packuswb %%mm1, %%mm1;"
-
-				"packuswb %%mm6, %%mm6;"	
-				"packuswb %%mm7, %%mm7;"	
-				"packuswb %%mm5, %%mm5;"	
-
-				"punpcklbw %%mm6, %%mm2;"	/* mm2 [R0 R1 R2 R3 R4 R5 R6 R7] */
-				"punpcklbw %%mm7, %%mm3;"	/* mm3 [G0 G1 G2 G3 G4 G5 G6 G7] */
-				"punpcklbw %%mm5, %%mm1;"	/* mm1 [B0 B1 B2 B3 B4 B5 B6 B7] */
-
-				"movq %%mm2, %%mm5;"		/* copy RGB */
-				"movq %%mm3, %%mm7;"
-				"movq %%mm1, %%mm6;"
-
-				"movq 40(%7), %%mm4;"
-				"punpcklbw %%mm2, %%mm1;"	/* mm1 [B0 R0 B1 R1 B2 R2 B3 R3] */
-				"punpcklbw %%mm4, %%mm3;"	/* mm4 [G0 FF G1 FF G2 FF G3 FF] */
-
-				"movq %%mm1, %%mm4;"		/* mm3 [G0 FF G1 FF G2 FF G3 FF] */
-				
-				"punpcklbw %%mm3, %%mm1;"	/* mm2 [B0 G0 R0 FF B1 G1 R1 FF] */
-				"punpckhbw %%mm3, %%mm4;"	/* mm3 [B2 G2 R2 FF B3 G3 R3 FF] */
-
-				"movq %%mm1, (%5);"		/* output BGRA[0] BGRA[1] */ 
-				"movq %%mm4, 8(%5);"		/* output BGRA[2] BGRA[3] */ 
-				
-				"movq 40(%7), %%mm4;"
-				"punpckhbw %%mm5, %%mm6;"	/* mm5 [B4 R4 B5 R6 B7 R7 B8 R8] */
-				"punpckhbw %%mm4, %%mm7;"	/* mm6 [G4 FF G5 FF G6 FF G7 FF] */
-
-				"movq %%mm6, %%mm4;"		/* mm7 [G4 FF G5 FF G6 FF G7 FF] */
-				
-				"punpcklbw %%mm7, %%mm6;"	/* mm6 [B4 G4 R4 FF B5 G5 R5 FF] */
-				"punpckhbw %%mm7, %%mm4;"	/* mm7 [B6 G6 R6 FF B7 G7 R7 FF] */
-
-				"movq %%mm6, 16(%5);"		/* output BGRA[4] BGRA[5] */ 
-				"movq %%mm4, 24(%5);"		/* output BGRA[6] BGRA[7] */ 
-				
-				/* Process the second line */
-
-				"movq (%4), %%mm2;"		/* restore Dred */
-				"movq 8(%4), %%mm3;"		/* restore Dgreen */
-				"movq 16(%4), %%mm1;"		/* restore Dblue */
-
-				"movq (%1), %%mm0;"		/* mm0 [Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0] */
-				"movq %%mm0, %%mm4;"		/* mm4 == mm0 */
-
-				"psllw $8, %%mm0;"		/* mm0 [00 Y0 00 Y2 00 Y4 00 Y6] */
-				"psrlw $8, %%mm0;"		/* mm0 [Y0 00 Y2 00 Y4 00 Y6 00] */
-				"psrlw $8, %%mm4;"		/* mm4 [00 Y7 00 Y5 00 Y3 00 Y1] */
-
-				"movq %%mm2, %%mm6;"
-				"movq %%mm3, %%mm7;"
-				"movq %%mm1, %%mm5;"
-
-				"paddsw %%mm0, %%mm2;"		/* CY[even] + DR */
-				"paddsw %%mm0, %%mm3;"		/* CY[even] + DG */
-				"paddsw %%mm0, %%mm1;"		/* CY[even] + DB */
-				
-				"paddsw %%mm4, %%mm6;"		/* CY[odd] + DR */
-				"paddsw %%mm4, %%mm7;"		/* CY[odd] + DG */
-				"paddsw %%mm4, %%mm5;"		/* CY[odd] + DB */
-
-				"packuswb %%mm2, %%mm2;"	/* Clamp RGB to [0-255] */
-				"packuswb %%mm3, %%mm3;"	
-				"packuswb %%mm1, %%mm1;"
-				"packuswb %%mm6, %%mm6;"	
-				"packuswb %%mm7, %%mm7;"	
-				"packuswb %%mm5, %%mm5;"	
-
-				"punpcklbw %%mm6, %%mm2;"	/* mm2 [R0 R1 R2 R3 R4 R5 R6 R7] */
-				"punpcklbw %%mm7, %%mm3;"	/* mm3 [G0 G1 G2 G3 G4 G5 G6 G7] */
-				"punpcklbw %%mm5, %%mm1;"	/* mm1 [B0 B1 B2 B3 B4 B5 B6 B7] */
-
-				"movq %%mm2, %%mm5;"		/* copy RGB */
-				"movq %%mm3, %%mm7;"
-				"movq %%mm1, %%mm6;"
-
-				"movq 40(%7), %%mm4;"
-				"punpcklbw %%mm2, %%mm1;"	/* mm1 [B0 R0 B1 R1 B2 R2 B3 R3] */
-				"punpcklbw %%mm4, %%mm3;"	/* mm4 [G0 FF G1 FF G2 FF G3 FF] */
-
-				"movq %%mm1, %%mm4;"		/* mm3 [G0 FF G1 FF G2 FF G3 FF] */
-				
-				"punpcklbw %%mm3, %%mm1;"	/* mm2 [B0 G0 R0 FF B1 G1 R1 FF] */
-				"punpckhbw %%mm3, %%mm4;"	/* mm3 [B2 G2 R2 FF B3 G3 R3 FF] */
-
-				"movq %%mm1, (%6);"		/* output BGRA[0] BGRA[1] */ 
-				"movq %%mm4, 8(%6);"		/* output BGRA[2] BGRA[3] */ 
-				
-				"movq 40(%7), %%mm4;"
-				"punpckhbw %%mm5, %%mm6;"	/* mm5 [B4 R4 B5 R6 B7 R7 B8 R8] */
-				"punpckhbw %%mm4, %%mm7;"	/* mm6 [G4 FF G5 FF G6 FF G7 FF] */
-
-				"movq %%mm6, %%mm4;"		/* mm7 [G4 FF G5 FF G6 FF G7 FF] */
-				
-				"punpcklbw %%mm7, %%mm6;"	/* mm6 [B4 G4 R4 FF B5 G5 R5 FF] */
-				"punpckhbw %%mm7, %%mm4;"	/* mm7 [B6 G6 R6 FF B7 G7 R7 FF] */
-
-				"movq %%mm6, 16(%6);"		/* output BGRA[4] BGRA[5] */ 
-				"movq %%mm4, 24(%6);"		/* output BGRA[6] BGRA[7] */ 
-				: : "r" (y_row1), "r" (y_row2), "r" (u_plane), "r" (v_plane), "r" (&rgb_uv), "r" (dest_row1), "r" (dest_row2), "r" (&mmx_table));
+			YUV2RGB_MMX(y_row2, dest_row2);
 		}
 		/* We currently dont MMX convert these pixel
 		 * the srcStride is padded enough that we could; but we'd have to track and clamp output
