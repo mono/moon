3eaa6412fec98af2e2d9309a7593994db8aaf5d6
Michael Dominic K. <mdk@me.com>
2008-03-25 13:38:55 +0000

    2008-03-25  Michael Dominic K.  <mdk@mdk.am>
    	* src/shape.cpp: When setting the clipping for the shape also include
    	the transformations coming from stretching. This fixes
    	test-shape-line-stretch3 test and the missing stripees on Silverlight
    	showcase.
    
    svn path=/trunk/moon/; revision=98964

--- src/shape.cpp
+++ src/shape.cpp
@@ -339,8 +339,11 @@ Shape::Clip (cairo_t *cr)
 		if (!vw)
 			return;
 
+		cairo_save (cr);
+		cairo_transform (cr, &stretch_transform);
 		cairo_rectangle (cr, 0, 0, vw->AsDouble (), vh->AsDouble ());
 		cairo_clip (cr);
+		cairo_restore (cr);
 		cairo_new_path (cr);
 	}
 }
