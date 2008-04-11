04f49570cc91abb4c9fd52a0eb58968ebb0ec76d
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-04-11 22:43:42 +0000

    Comment out ref/unref, it crashes 100% of the time.
    svn path=/trunk/moon/; revision=100484

--- src/dependencyobject.cpp
+++ src/dependencyobject.cpp
@@ -385,7 +385,7 @@ EventObject::Emit (int event_id, EventArgs *calldata)
 	
 	// We need to ref ourselves during event emission, since we may end up 
 	// deleting the object calling us/holding a ref to us.
-	ref ();
+	//ref ();
 	events [event_id].emitting++;
 
 	length = events [event_id].event_list->Length ();
@@ -422,7 +422,7 @@ EventObject::Emit (int event_id, EventArgs *calldata)
 	}
 	
 	g_free (closures);
-	unref ();
+	//unref ();
 
 	return true;
 }
