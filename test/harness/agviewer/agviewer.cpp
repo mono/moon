5275683f38c122ab2dbb6dd51b472153ee946a64
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-05-23 22:38:34 +0000

    * agviewer/agviewer.cpp: Install a signal handler to print stack traces   when we crash.
    svn path=/trunk/moon/; revision=103964

--- test/harness/agviewer/agviewer.cpp
+++ test/harness/agviewer/agviewer.cpp
@@ -34,6 +34,10 @@
 #include <gdk/gdkkeysyms.h>
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-bindings.h>
+#include <signal.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <dlfcn.h>
 
 #if HAVE_GECKO_1_9
 #include <gtkmozembed.h>
@@ -85,6 +89,8 @@ static bool wait_for_next_test (int* timeout);
 static void mark_test_as_complete_and_start_next_test (bool successful);
 static void log_message (const char* test_name, const char* message);
 
+static void agviewer_handle_native_sigsegv (int signal);
+static void agviewer_add_signal_handler ();
 
 
 static void
@@ -161,6 +167,8 @@ main(int argc, char **argv)
 	int frame_height = 800;
 	bool working_dir_set = false;
 
+	agviewer_add_signal_handler ();
+	
 	gtk_init (&argc, &argv);
 
 	int i;
@@ -178,7 +186,7 @@ main(int argc, char **argv)
 		if (!g_strcasecmp ("-working-dir", argv [i]) && i < argc) {
 			working_dir_set = true;
 			if (chdir (argv [++i]) != 0) {
-				g_warning ("Unable to set working directory.\n");
+				g_warning ("Unable to set working directory to :%s\n", argv [i]);
 				exit (-1);
 			}
 			
@@ -490,3 +498,125 @@ log_message (const char* test_name, const char* message)
 	dbus_g_proxy_call_no_reply (dbus_proxy, "Log", G_TYPE_STRING, test_name, G_TYPE_STRING, "Error", G_TYPE_STRING, message, G_TYPE_INVALID);
 }
 
+
+static void
+agviewer_add_signal_handler ()
+{
+	struct sigaction sa;
+
+	sa.sa_handler = agviewer_handle_native_sigsegv;
+	sigemptyset (&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	g_assert (sigaction (SIGSEGV, &sa, NULL) != -1);
+}
+
+/*
+ * agviewer_handle_native_sigsegv: 
+ *   (this is a slightly modified version of mono_handle_native_sigsegv from mono/mono/mini/mini-exceptions.c)
+ *
+ */
+ 
+static bool handling_sigsegv = false;
+
+typedef void void_method ();
+
+void
+agviewer_handle_native_sigsegv (int signal)
+{
+	const char *signal_str = (signal == SIGSEGV) ? "SIGSEGV" : "SIGABRT";
+	
+	if (handling_sigsegv) {
+		fprintf (stderr, "\nGot a SIGSEGV while executing the SIGSEGV handler, aborting.\n");
+		abort ();
+		return;
+	}
+
+	/* To prevent infinite loops when the stack walk causes a crash */
+	handling_sigsegv = true;
+
+	/*
+	 * A SIGSEGV indicates something went very wrong so we can no longer depend
+	 * on anything working. So try to print out lots of diagnostics, starting 
+	 * with ones which have a greater chance of working.
+	 */
+	fprintf (stderr,
+			 "\n"
+			 "=============================================================\n"
+			 "Got a %s while executing native code.                        \n"
+			 " We'll first ask gdb for a stack trace, then try our own     \n"
+			 " stack walking method (usually not as good as gdb, but it    \n"
+			 " can managed and native stack traces together)               \n"
+			 "=============================================================\n"
+			 "\n", signal_str);
+	
+	/* Try to get more meaningful information using gdb */
+#if !defined(PLATFORM_WIN32)
+	/* From g_spawn_command_line_sync () in eglib */
+	int res;
+	int stdout_pipe [2] = { -1, -1 };
+	pid_t pid;
+	const char *argv [16];
+	char buf1 [128];
+	int status;
+	char buffer [1024];
+
+	res = pipe (stdout_pipe);
+	g_assert (res != -1);
+		
+	/*
+	 * glibc fork acquires some locks, so if the crash happened inside malloc/free,
+	 * it will deadlock. Call the syscall directly instead.
+	 */
+	pid = syscall (SYS_fork);
+	if (pid == 0) {
+		close (stdout_pipe [0]);
+		dup2 (stdout_pipe [1], STDOUT_FILENO);
+
+		for (int i = getdtablesize () - 1; i >= 3; i--)
+			close (i);
+
+		argv [0] = g_find_program_in_path ("gdb");
+		if (argv [0] == NULL) {
+			close (STDOUT_FILENO);
+			exit (1);
+		}
+
+		argv [1] = "-ex";
+		sprintf (buf1, "attach %ld", (long)getpid ());
+		argv [2] = buf1;
+		argv [3] = "--ex";
+		argv [4] = "info threads";
+		argv [5] = "--ex";
+		argv [6] = "thread apply all bt";
+		argv [7] = "--batch";
+		argv [8] = 0;
+
+		execv (argv [0], (char**)argv);
+		exit (1);
+	}
+
+	close (stdout_pipe [1]);
+
+	fprintf (stderr, "\nDebug info from gdb:\n\n");
+
+	while (1) {
+		int nread = read (stdout_pipe [0], buffer, 1024);
+
+		if (nread <= 0)
+			break;
+		write (STDERR_FILENO, buffer, nread);
+	}		
+
+	waitpid (pid, &status, WNOHANG);
+	
+#endif
+
+	fprintf (stderr, "\nDebug info from libmoon:\n\n");
+	void *libmoon = dlopen ("libmoon.so", RTLD_LOCAL | RTLD_LAZY);
+	void_method *print_stack_trace = (libmoon == NULL) ? NULL : (void_method *) dlsym (libmoon, "print_stack_trace");
+	if (print_stack_trace)
+		print_stack_trace ();
+	
+	abort ();
+}
