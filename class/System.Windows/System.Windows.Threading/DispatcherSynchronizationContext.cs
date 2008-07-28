fbf731d6bcc86e0e1b9f0cf403e9d2b2450d3539
Chris Toshok <toshok@novell.com>
2008-07-28 18:46:40 +0000

    in ./: 2008-07-28  Chris Toshok  <toshok@ximian.com>
            * Makefile.am (system_windows_sources): add the
            System.Windows.Threading directory.
    
    in System.Windows.Thread/:
    2008-07-28  Chris Toshok  <toshok@ximian.com>
    
            * Dispatcher.cs: new stubs.
    
            * DispatcherTimer.cs: same.
    
            * DispatcherSynchronizationContext.cs: same.
    
            * DispatcherOperation.cs: same.
    
    
    
    svn path=/trunk/moon/; revision=109064

---
+++ class/System.Windows/System.Windows.Threading/DispatcherSynchronizationContext.cs
@@ -0,0 +1,70 @@
+//
+// DispatcherTimer.cs
+//
+// Copyright 2008 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Security;
+using System.Threading;
+
+namespace System.Windows.Threading {
+
+	public class DispatcherSynchronizationContext 
+#if notyet
+	: SynchronizationContext
+#endif
+	{
+		public DispatcherSynchronizationContext (Dispatcher dispatcher)
+		{
+			throw new NotImplementedException ();
+		}
+
+#if NET_2_1
+		[SecuritySafeCritical]
+#endif
+		public DispatcherSynchronizationContext ()
+		{
+			throw new NotImplementedException ();
+		}
+
+#if notyet
+		public SynchronizationContext CreateCopy ()
+		{
+			throw new NotImplementedException ();
+		}
+#endif
+
+		public virtual void Send (SendOrPostCallback cb, object arg)
+		{
+			throw new NotImplementedException ();
+		}
+
+#if NET_2_1
+		[SecuritySafeCritical]
+#endif
+		public virtual void Post (SendOrPostCallback cb, object arg)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
---
+++ class/System.Windows/System.Windows.Threading/DispatcherTimer.cs
@@ -0,0 +1,69 @@
+//
+// DispatcherTimer.cs
+//
+// Copyright 2008 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Security;
+
+namespace System.Windows.Threading {
+
+	public class DispatcherTimer {
+
+#if NET_2_1
+		[SecuritySafeCritical]
+#endif
+		public void Start ()
+		{
+			throw new NotImplementedException ();
+		}
+
+#if NET_2_1
+		[SecuritySafeCritical]
+#endif
+		public void Stop ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public TimeSpan Interval {
+#if NET_2_1
+			[SecuritySafeCritical]
+#endif
+			get { throw new NotImplementedException (); }
+
+#if NET_2_1
+			[SecuritySafeCritical]
+#endif
+			set { throw new NotImplementedException (); }
+			
+		}
+
+		public bool IsEnabled {
+			get { throw new NotImplementedException (); }
+		}
+
+		public event EventHandler Tick;
+	}
+
+}
