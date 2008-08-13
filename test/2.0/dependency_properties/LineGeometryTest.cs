0484011f32739d122d6a9a3525ea1b189d53d53f
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-08-13 18:06:53 +0000

    * test/xaml/drtlist.xml, test/xaml/test-fillrule.xaml,   test/harness/masters/test-fillruleMaster.png: Added   test-fillrule.xaml. * test/2.0/dependency_properties/LineGeometryTest.cs: Added   LineGeometryTest.
    svn path=/trunk/moon/; revision=110387

---
+++ test/2.0/dependency_properties/LineGeometryTest.cs
@@ -0,0 +1,50 @@
+/*
+ * LineGeometryTest.cs.
+ *
+ * Contact:
+ *   Moonlight List (moonlight-list@lists.ximian.com)
+ *
+ * Copyright 2008 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ * 
+ */
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+using dependency_properties;
+
+namespace MoonTest.System.Windows.Media
+{
+	[TestClass]
+	public class LineGeometryTest
+	{
+		[TestMethod]
+		public void FillRuleTest ()
+		{
+			string xaml =
+@"<Canvas xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">
+	<Path Fill=""#FF0000"" Stroke=""Green"" StrokeThickness=""2"">
+		<Path.Data>
+			<LineGeometry FillRule=""EvenOdd"" StartPoint=""10,60"" EndPoint=""110,60"" />
+		</Path.Data>
+	</Path>
+</Canvas>";
+			Assert.Throws (delegate {
+				XamlReader.Load (xaml);
+			}, typeof (XamlParseException));
+		}
+	}
+}
