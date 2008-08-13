596634115f16c45b38a81b0585420612a5688898
Chris Toshok <toshok@novell.com>
2008-08-13 08:04:39 +0000

    add a test for PFC<T>.RemoveAt
    svn path=/trunk/moon/; revision=110348

---
+++ test/2.0/dependency_properties/PresentationFrameworkCollectionTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+using dependency_properties;
+
+namespace MoonTest.System.Windows
+{
+	[TestClass]
+	public class PresentationFrameworkCollectionTest
+	{
+		[TestMethod]
+		public void RemoveAt ()
+		{
+			UIElementCollection col = new UIElementCollection();
+			col.Add (new Rectangle());
+			col.RemoveAt (0);
+			Assert.Throws (delegate { col.RemoveAt (1); }, typeof (ArgumentOutOfRangeException));
+		}
+	}
+}
