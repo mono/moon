1cac5960b24148b39e9b713ef5546cdba9854a4c
Jackson Harper <jackson@novell.com>
2008-08-14 01:05:28 +0000

    New test for parsing value types.
    svn path=/trunk/moon/; revision=110445

---
+++ test/2.0/ValueTypes/Page.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ValueTypes {
+	public partial class Page : UserControl
+	{
+		public Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
