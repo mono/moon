37f69ac6aabe5f0b3794bc400b8fa2320ce03994
Jackson Harper <jackson@novell.com>
2008-08-15 16:20:33 +0000

    New test/demo, setting a managed attached property.
    svn path=/trunk/moon/; revision=110575

---
+++ test/2.0/ManagedAttachedProps/Page.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ManagedAttachedProps {
+	public partial class Page : UserControl
+	{
+		public Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
