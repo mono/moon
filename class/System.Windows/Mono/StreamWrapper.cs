d1364348bb7bed4f22b5dd749da52c2518c18924
Geoff Norton <grompf@sublimeintervention.com>
2008-08-17 21:54:44 +0000

    2008-08-17  Geoff Norton  <gnorton@novell.com>
            * StreamWrapper.cs: Remove a ton of debug spew.
    
    svn path=/trunk/moon/; revision=110699

--- class/System.Windows/Mono/StreamWrapper.cs
+++ class/System.Windows/Mono/StreamWrapper.cs
@@ -54,54 +54,39 @@ namespace Mono
 		
 		public static bool CanSeek (IntPtr handle) {
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.CanSeek");
 			bool result = wrapper.stream.CanSeek;
-			Console.WriteLine ("StreamWrapper.CanSeek: {0}", result);
 			return result;
 		}
 		
 		public static bool CanRead (IntPtr handle) {
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.CanRead");
 			bool result = wrapper.stream.CanRead;
-			Console.WriteLine ("StreamWrapper.CanRead: {0}", result);
 			return result;
 		}
 
 		public static long Length (IntPtr handle) {
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.Length");
 			long result = wrapper.stream.Length;
-			Console.WriteLine ("StreamWrapper.Length: {0}", result);
 			return result;
 		}
 		
 		public static long Position (IntPtr handle) {
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.Position");
 			long result = wrapper.stream.Position;
-			Console.WriteLine ("StreamWrapper.Position: {0}", result);
 			return result;
 		}
 		
 		public static int Read (IntPtr handle, [In (), Out (), MarshalAs (UnmanagedType.LPArray, SizeParamIndex=3)] byte [] buffer, int offset, int count)
 		{
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.Read ({0}, {1}, {2})", buffer, offset, count);
 			int result = wrapper.stream.Read (buffer, offset, count);
-			Console.WriteLine ("StreamWrapper.Read ({0}, {1}, {2}): {3}", buffer, offset, count, result);
-			for (int i = 0; i < global::System.Math.Min (count / 4, 4); i++) {
-				Console.WriteLine (" #{0} {1:X} {2:X} {3:X} {4:X}", i, buffer [i * 4], buffer [i * 4 + 1], buffer [i * 4 + 2], buffer [i * 4 + 3]);
-			}
 			return result;
 		}
 		
 		public static void Seek (IntPtr handle, long offset, SeekOrigin origin)
 		{
 			StreamWrapper wrapper = (StreamWrapper) Helper.GCHandleFromIntPtr (handle).Target;
-			Console.WriteLine ("StreamWrapper.Seek ({0}, {1})", offset, origin);
 			wrapper.stream.Seek (offset, origin);
-			Console.WriteLine ("StreamWrapper.Seek ({0}, {1}) [Done]", offset, origin);
 		}
 	}
 }
