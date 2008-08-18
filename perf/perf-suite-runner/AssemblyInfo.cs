a913de844f487a3698b2a49bfa7065900302407a
Michael Dominic K. <mdk@me.com>
2008-08-18 16:25:28 +0000

    Ooops, that was missing for a long time.
    svn path=/trunk/moon/; revision=110788

---
+++ perf/perf-suite-runner/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Mono.GetOptions;
+using System.Reflection;
+
+[assembly: AssemblyTitle ("perf-suite-runner.exe")]
+[assembly: AssemblyVersion ("0.1.*")]
+[assembly: AssemblyDescription ("Moonlight performance suite runner")]
+[assembly: AssemblyCopyright ("MIT license")]
+[assembly: Mono.UsageComplement ("")]
+[assembly: Mono.About("Use this program to run the performance suite and put results in database.")]
+[assembly: Mono.Author ("Novell Moonlight Team")]
+
