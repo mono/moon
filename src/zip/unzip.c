3d41fffeebef27b8a727f4f067553876a0fcca29
Jeffrey Stedfast <fejj@novell.com>
2008-08-19 21:47:31 +0000

    2008-08-19  Jeffrey Stedfast  <fejj@novell.com>
    	* zip/unzip.c: Get rid of unused variable.
    
    
    svn path=/trunk/moon/; revision=111008

--- src/zip/unzip.c
+++ src/zip/unzip.c
@@ -1537,7 +1537,6 @@ extern int ZEXPORT unzGetGlobalComment (file, szComment, uSizeBuf)
     char *szComment;
     uLong uSizeBuf;
 {
-    int err=UNZ_OK;
     unz_s* s;
     uLong uReadThis ;
     if (file==NULL)
