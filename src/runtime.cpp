8d3ec6ea98608b1e23f2e582c48cc4b47a861fa4
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-08-20 11:54:01 +0000

    * src/runtime.cpp: Fix 1.0 build. No idea if the fix is correct.
    svn path=/trunk/moon/; revision=111106

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -1783,7 +1783,9 @@ Surface::FocusElement (UIElement *focused)
 	   3. the element must be part of the plugin's visual tree, and must have had its Loaded event fired.
 	*/
 	if (!focused->GetRenderVisible()
+#if SL_2_0
 	    || (!focused->Is(Type::CONTROL) || !((Control*)focused)->GetIsTabStop())
+#endif
 	    || !focused->IsLoaded()
 	    || focused->GetSurface () != this)
 		return false;
