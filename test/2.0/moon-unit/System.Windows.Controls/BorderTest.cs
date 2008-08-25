a24940167ab4a936cc48a18245bcf7dc5a090b51
Chris Toshok <toshok@novell.com>
2008-08-25 17:37:24 +0000

    add some arrange tests
    svn path=/trunk/moon/; revision=111540

--- test/2.0/moon-unit/System.Windows.Controls/BorderTest.cs
+++ test/2.0/moon-unit/System.Windows.Controls/BorderTest.cs
@@ -59,6 +59,20 @@ namespace MoonTest.System.Windows.Controls
 		}
 
 		[TestMethod]
+		public void ChildlessPropertiesMeasureTest3 ()
+		{
+			Border c = new Border ();
+
+			c.Padding = new Thickness (20);
+			c.BorderThickness = new Thickness (5);
+			c.Background = new SolidColorBrush (Colors.Black);
+
+			c.Measure (new Size (100, 100));
+
+			Assert.AreEqual (new Size (50,50), c.DesiredSize, "DesiredSize");
+		}
+
+		[TestMethod]
 		public void ChildMeasureTest1 ()
 		{
 			Border c = new Border ();
@@ -163,6 +177,122 @@ namespace MoonTest.System.Windows.Controls
 
 			Assert.AreEqual (new Size (80,80), c.DesiredSize);
 		}
-	}
 
+		[TestMethod]
+		public void ArrangeTest_RenderSize_ChildSmallerThanFinalRect ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+
+			Assert.AreEqual (new Size (0, 0), r.RenderSize);
+
+			c.Child = r;
+
+			Assert.AreEqual (new Size (0, 0), r.RenderSize);
+
+			r.Width = 50;
+			r.Height = 50;
+
+			Assert.AreEqual (new Size (0, 0), r.RenderSize);
+
+			// we intentionally give border less room to
+			// work with than the configured width/height
+			// of the child
+			c.Measure (new Size (25, 25));
+
+			Assert.AreEqual (new Size (0, 0), r.RenderSize);
+
+			c.Arrange (new Rect (0, 0, 25, 25));
+
+			Assert.AreEqual (new Size (25, 25), r.DesiredSize);
+			Assert.AreEqual (new Size (50, 50), r.RenderSize);
+		}
+
+		[TestMethod]
+		public void ArrangeTest_ChildSmallerThanFinalRect ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+
+			c.Child = r;
+
+			r.Width = 50;
+			r.Height = 50;
+
+			// we intentionally give border less room to
+			// work with than the configured width/height
+			// of the child
+			c.Measure (new Size (25, 25));
+			c.Arrange (new Rect (0, 0, 25, 25));
+
+			Assert.AreEqual (new Size (25, 25), r.DesiredSize);
+		}
+
+		[TestMethod]
+		public void ArrangeTest_ChildSmallerThanFinalRect_WithBorderMargin ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+
+			c.Child = r;
+
+			c.Margin = new Thickness (5);
+
+			r.Width = 50;
+			r.Height = 50;
+
+			// we intentionally give border less room to
+			// work with than the configured width/height
+			// of the child
+			c.Measure (new Size (25, 25));
+			c.Arrange (new Rect (0, 0, 25, 25));
+
+			Assert.AreEqual (new Size (15, 15), r.DesiredSize);
+		}
+
+		[TestMethod]
+		public void ArrangeTest_ChildSmallerThanFinalRect_WithBorderBrushAndThickness ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+
+			c.Child = r;
+
+			c.BorderBrush = new SolidColorBrush (Colors.Black);
+			c.BorderThickness = new Thickness (5);
+
+			r.Width = 50;
+			r.Height = 50;
+
+			// we intentionally give border less room to
+			// work with than the configured width/height
+			// of the child
+			c.Measure (new Size (25, 25));
+			c.Arrange (new Rect (0, 0, 25, 25));
+
+			Assert.AreEqual (new Size (15, 15), r.DesiredSize);
+		}
+
+		[TestMethod]
+		public void ArrangeTest_ChildSmallerThanFinalRect_WithBorderThickness ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+
+			c.Child = r;
+
+			c.BorderThickness = new Thickness (5);
+
+			r.Width = 50;
+			r.Height = 50;
+
+			// we intentionally give border less room to
+			// work with than the configured width/height
+			// of the child
+			c.Measure (new Size (25, 25));
+			c.Arrange (new Rect (0, 0, 25, 25));
+
+			Assert.AreEqual (new Size (15, 15), r.DesiredSize);
+		}
+	}
 }
