42ecaf0cc67e54fb2727786f4bea46af059bb7c7
Chris Toshok <toshok@novell.com>
2008-08-29 18:46:04 +0000

    add an x:Name test
    svn path=/trunk/moon/; revision=111937

---
+++ test/2.0/moon-unit/System.Windows/FrameworkElementTest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using Mono.Moonlight.UnitTesting;
+
+namespace MoonTest.System.Windows
+{
+	[TestClass]
+	public class FrameworkElementTest
+	{
+		[TestMethod]
+		[KnownFailure]
+		public void TestName ()
+		{
+			Assert.AreEqual ("", (new Canvas()).Name);
+		}
+	}
+}
