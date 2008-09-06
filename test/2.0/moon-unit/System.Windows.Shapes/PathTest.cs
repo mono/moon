f6771f7fe9c31ca36a4f8c644a3e90d5842c0a1f
Larry Ewing <lewing@novell.com>
2008-09-06 06:03:05 +0000

    Some shape Measure tests.
    svn path=/trunk/moon/; revision=112417

---
+++ test/2.0/moon-unit/System.Windows.Shapes/PathTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Mono.Moonlight.UnitTesting;
+using System.Windows.Shapes;
+
+namespace MoonTest.System.Windows.Shapes
+{
+	[TestClass]
+	public class PathTest
+	{
+		[TestMethod]
+		public void MeasureRectangleTest1 ()
+		{
+			Path p = new Path ();
+			RectangleGeometry r = new RectangleGeometry ();
+			r.Rect = new Rect (10, 10, 20, 20);
+			p.Data = r;
+
+			p.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), p.DesiredSize);
+		}
+
+		[TestMethod]
+		public void MeasureRectangleBorderTest ()
+		{
+			Border b = new Border ();
+			Path p = new Path ();
+			p.Fill = new SolidColorBrush (Colors.Black);
+			RectangleGeometry r = new RectangleGeometry ();
+			r.Rect = new Rect (10, 10, 20, 20);
+			p.Data = r;
+			b.Child = p;
+
+			b.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), b.DesiredSize);
+		}
+	}
+}
---
+++ test/2.0/moon-unit/System.Windows.Shapes/RectangleTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Mono.Moonlight.UnitTesting;
+using System.Windows.Shapes;
+
+namespace MoonTest.System.Windows.Shapes
+{
+	[TestClass]
+	public class RectangleTest
+	{
+		[TestMethod]
+		public void MeasureTest1 ()
+		{
+			Rectangle r = new Rectangle ();
+			r.Width = 10;
+			r.Height = 20;
+
+			r.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), r.DesiredSize);
+		}
+
+		[TestMethod]
+		public void MeasureTest2 ()
+		{
+			Canvas c = new Canvas ();
+			Rectangle r = new Rectangle ();
+			r.Width = 10;
+			r.Height = 20;
+
+			r.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), r.DesiredSize);
+			
+			c.Children.Add (r);
+			c.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), c.DesiredSize, "Canvas desired");
+			Assert.AreEqual (new Size (0, 0), r.DesiredSize, "Rectangle desired");
+		}
+
+		[TestMethod]
+		public void MeasureTest3 ()
+		{
+			Border c = new Border ();
+			Rectangle r = new Rectangle ();
+			r.Width = 10;
+			r.Height = 20;
+
+			r.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (0, 0), r.DesiredSize);
+			
+			c.Child = r;
+			c.Measure (new Size (50, 50));
+
+			Assert.AreEqual (new Size (10, 20), c.DesiredSize, "Border desired");
+			Assert.AreEqual (new Size (10, 20), r.DesiredSize, "Rectangle desired");
+		}
+	}
+}
