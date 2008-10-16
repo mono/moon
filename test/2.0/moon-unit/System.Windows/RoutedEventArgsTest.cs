82b9490bf3feac7e72c152828ff5e69b17bd0e7b
Jb Evain <jbevain@gmail.com>
2008-10-16 13:37:20 +0000

    comment test out
    svn path=/trunk/moon/; revision=115997

--- test/2.0/moon-unit/System.Windows/RoutedEventArgsTest.cs
+++ test/2.0/moon-unit/System.Windows/RoutedEventArgsTest.cs
@@ -17,33 +17,37 @@ namespace MoonTest.System.Windows
 	[TestClass]
 	public class RoutedEventArgsTest
 	{
+		/*
+		 * FIXME: RoutedEventArgs.set_OriginalSource is internal
+
 		[TestMethod]
 		public void SourceTest ()
 		{
 			RoutedEventArgs args = new RoutedEventArgs ();
-			Assert.IsNull (args.Source);
+			Assert.IsNull (args.OriginalSource);
 
 			// try object
-			Assert.Throws (delegate () { args.Source = new object(); }, typeof (ArgumentException));
+			Assert.Throws (delegate () { args.OriginalSource = new object(); }, typeof (ArgumentException));
 
 			object o;
 
 			// try a DependencyObject subclass
 			o = new SolidColorBrush();
-			args.Source = o;
-			Assert.AreEqual (o, args.Source);
+			args.OriginalSource = o;
+			Assert.AreEqual (o, args.OriginalSource);
 
 			// try a UIElement subclass
 			o = new Ellipse();
-			args.Source = o;
-			Assert.AreEqual (o, args.Source);
+			args.OriginalSource = o;
+			Assert.AreEqual (o, args.OriginalSource);
 
 #if notyet
 			// try null.  B2 crashes on this line (no
 			// exception, firefox goes away with a crash
 			// in coreclr.dll)
-			args.Source = null;
+			args.OriginalSource = null;
 #endif
 		}
+		*/
 	}
 }
