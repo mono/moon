3484be063572a5aaa763788b54dad5e62892a6ab
Geoff Norton <grompf@sublimeintervention.com>
2008-10-16 23:50:14 +0000

    2008-10-16  Geoff Norton  <gnorton@novell.com>
            * pipeline.cpp: Unbreak the world.
    
    svn path=/trunk/moon/; revision=116125

--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -186,8 +186,13 @@ Media::RegisterMSCodecs (void)
 	const char *functions [] = {"register_mswma", "register_mswmv", "register_msmp3"};
 	registering_ms_codecs = true;
 
-	if (libmscodecs_path == NULL || !(g_file_test (libmscodecs_path, G_FILE_TEST_EXISTS) && g_file_test (libmscodecs_path, G_FILE_TEST_IS_REGULAR)))
-		libmscodecs_path = g_build_filename (g_get_home_dir (), ".mozilla", "plugins", CODEC_LIBRARY_NAME);
+	if (libmscodecs_path == NULL || !(g_file_test (libmscodecs_path, G_FILE_TEST_EXISTS) && g_file_test (libmscodecs_path, G_FILE_TEST_IS_REGULAR))) {
+		char *home = g_get_home_dir ();
+		if (home != NULL)
+			libmscodecs_path = g_build_filename (g_get_home_dir (), ".mozilla", "plugins", CODEC_LIBRARY_NAME);
+		else
+			libmscodecs_path = g_strdup (CODEC_LIBRARY_NAME);
+	}
 	
 	dl = dlopen (libmscodecs_path, RTLD_LAZY);
 	if (dl != NULL) {
