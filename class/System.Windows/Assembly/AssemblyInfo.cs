a514cc210cd39cf3fa1f15c03af17e585a2a9bd5
Rolf Bjarne Kvinge <RKvinge@novell.com>
2008-11-19 11:34:09 +0000

    * Assembly/AssemblyInfo.cs: Added missing attributes.
    svn path=/trunk/moon/; revision=119296

--- class/System.Windows/Assembly/AssemblyInfo.cs
+++ class/System.Windows/Assembly/AssemblyInfo.cs
@@ -29,6 +29,7 @@
 //
 
 using System;
+using System.Diagnostics;
 using System.Reflection;
 using System.Resources;
 using System.Security;
@@ -46,24 +47,52 @@ using System.Windows.Markup;
 [assembly: SatelliteContractVersion ("2.0.5.0")]
 #endif
 
+//[assembly: AssemblyDelaySign(true)]
+[assembly: AssemblyFileVersion ("2.0.31005.0")]
 [assembly: AssemblyTitle ("System.Windows.dll")]
 [assembly: AssemblyDescription ("System.Windows.dll")]
-[assembly: AssemblyConfiguration ("Development version")]
 [assembly: AssemblyCompany ("MONO development team")]
 [assembly: AssemblyProduct ("MONO CLI")]
-[assembly: AssemblyCopyright ("(c) 2007 Various Authors")]
-[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCopyright ("(c) 2007 - 2008 Various Authors")]
 
 [assembly: AssemblyDefaultAlias ("System.Windows.dll")]
 [assembly: AssemblyInformationalVersion ("0.0.0.1")]
 [assembly: NeutralResourcesLanguage ("en-US")]
 
+[assembly: Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+//[assembly: CLSCompliant (true)]
 [assembly: ComVisible (false)]
+[assembly: CompilationRelaxations (CompilationRelaxations.NoStringInterning)]
 
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/client/2007", "System.Windows.Controls")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/client/2007", "System.Windows.Controls.Primitives")] 
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls.Primitives")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Controls")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Controls.Primitives")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Data")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Documents")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Ink")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Input")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Markup")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Media")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Media.Animation")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Media.Imaging")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Shapes")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/client/2007", "System.Windows.Automation")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls.Primitives")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Data")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Documents")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Ink")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Input")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Markup")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Media")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Media.Animation")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Media.Imaging")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Shapes")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Automation")]
+[assembly: XmlnsDefinition ("http://schemas.microsoft.com/winfx/2006/xaml", "System.Windows.Markup")]
+[assembly: XmlnsPrefix ("http://schemas.microsoft.com/winfx/2006/xaml", "x")]
+[assembly: XmlnsPrefix ("http://schemas.microsoft.com/client/2007", "x")]
 
 #if !NET_2_1
 	[assembly: InternalsVisibleTo ("gtksilver, PublicKey=002400000480000094000000060200000024000052534131000400001100000005E62DA51722818A2ADC73D5CE64289260012A442031582E808F5C290EF155F10AB93441F92A7A59736D3481245ED4E0E864F5E1ACCADD217D53EE0263E6E3852FE94AB6B708984C6C69BA79F40A0896E1FFF820B7C55D4968C8F41CAE2AABC136B16B8AF83D013946CE190BC03C2A6C8DE8C0CB135ED656F46BF9A2D03E8188")]
