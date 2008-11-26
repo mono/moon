mono/moon

commit 8f784d96676fefa1df7673b517baa1d648f7ea26

* moon/class/System.Windows/System.Windows/Setter.cs: Silverlight throws   a null ref in this case
* moon/src/style.h:
* moon/src/error.h:
* moon/src/style.cpp:
* moon/src/cbinding.h:
* moon/src/cbinding.cpp:
* moon/src/collection.h:
* moon/src/dependencyproperty.g.cpp:
* moon/class/Mono.Moonlight/Mono/NativeMethods.cs:
* moon/class/System.Windows/System.Windows/Style.cs:
* moon/class/Mono.Moonlight/Mono/GeneratedPInvokes.cs:
* moon/class/System.Windows/System.Windows/SetterBase.cs:
* moon/class/System.Windows/System.Windows/DependencyProperty.g.cs:
* moon/class/System.Windows/System.Windows/SetterBaseCollection.cs:
  Added full support for sealing styles.

svn path=/trunk/moon/; revision=119963

src/collection.h
@@ -48,7 +48,7 @@ public:
 	virtual int GetCount ();
 	
 	int Add (Value value);
-	virtual int Add (Value *value);
+	int Add (Value *value);
 	
 	/* @GenerateCBinding,GeneratePInvoke */
 	virtual bool Clear ();
@@ -59,11 +59,11 @@ public:
 	int IndexOf (Value *value);
 	
 	bool Insert (int index, Value value);
-	virtual bool Insert (int index, Value *value);
+	bool Insert (int index, Value *value);
 	
 	bool Remove (Value value);
 	/* @GenerateCBinding,GeneratePInvoke */
-	virtual bool Remove (Value *value);
+	bool Remove (Value *value);
 	
 	bool RemoveAt (int index);
 	
@@ -71,15 +71,15 @@ public:
 	Value *GetValueAt (int index);
 
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	int AddWithError (Value *value, MoonError *error);
+	virtual int AddWithError (Value *value, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	int InsertWithError (int index, Value *value, MoonError *error);
+	virtual int InsertWithError (int index, Value *value, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	Value *GetValueAtWithError (int index, MoonError *error);
+	virtual Value *GetValueAtWithError (int index, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	bool SetValueAtWithError (int index, Value *value, MoonError *error);
+	virtual bool SetValueAtWithError (int index, Value *value, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	bool RemoveAtWithError (int index, MoonError *error);
+	virtual bool RemoveAtWithError (int index, MoonError *error);
 
 protected:
 	GPtrArray *array;

src/style.cpp
@@ -14,18 +14,83 @@
 
 #include <config.h>
 #include "style.h"
+#include "error.h"
 
 Style::Style ()
 {
 	SetValue (Style::SettersProperty, Value::CreateUnref (new SetterBaseCollection()));
+	GetSetters ()->style = this;
+}
+
+void
+Style::Seal ()
+{
+	SetIsSealed (true);
+	GetSetters ()->Seal ();
 }
 
 SetterBaseCollection::SetterBaseCollection ()
 {
+	this->style = NULL;
+}
+
+int
+SetterBaseCollection::AddWithError (Value *value, MoonError *error)
+{
+	if (style && style->GetIsSealed ()) {
+		MoonError::FillIn (error, MoonError::EXCEPTION, "Cannot modify a style after it is used");
+		return -1;
+	}
+	return DependencyObjectCollection::AddWithError (value, error);	
+}
+
+int
+SetterBaseCollection:: InsertWithError (int index, Value *value, MoonError *error)
+{
+	if (style && style->GetIsSealed ()) {
+		MoonError::FillIn (error, MoonError::EXCEPTION, "Cannot modify a style after it is used");
+		return -1;
+	}
+	return DependencyObjectCollection::InsertWithError (index, value, error);	
+}
+
+void
+SetterBaseCollection::Seal ()
+{
+	SetIsSealed (true);
+	CollectionIterator *iter = GetIterator ();
+
+	int error = 0;
+	Value *current;
+	while (iter->Next () && (current = iter->GetCurrent (&error))) {
+		SetterBase *setter = current->AsSetterBase ();
+		setter->Seal ();
+	}
+}
+
+bool
+SetterBaseCollection::SetValueAtWithError (int index, Value *value, MoonError *error)
+{
+	if (style && style->GetIsSealed ()) {
+		MoonError::FillIn (error, MoonError::EXCEPTION, "Cannot modify a style after it is used");
+		return false;
+	}
+	return DependencyObjectCollection::SetValueAtWithError (index, value, error);	
 }
 
 SetterBase::SetterBase ()
 {
+}
+
+bool
+SetterBase::SetValueWithErrorImpl (DependencyProperty *property, Value *value, MoonError *error)
+{
+	if (GetIsSealed ()) {
+		MoonError::FillIn (error, MoonError::UNAUTHORIZED_ACCESS, "Cannot modify a setter after it is used");
+		return false;
+	}
+	
+	return DependencyObject::SetValueWithErrorImpl (property, value, error);
 }
 
 Setter::Setter ()

src/style.h
@@ -27,6 +27,8 @@ class Style : public DependencyObject {
 	virtual ~Style () { }
 	
  public:
+  	/* @PropertyType=bool,DefaultValue=false,ManagedSetterAccess=Private,GenerateAccessors */
+	static DependencyProperty *IsSealedProperty;
  	/* @PropertyType=SetterBaseCollection,Access=Internal,ManagedFieldAccess=Private,ManagedAccess=Public,ManagedSetterAccess=Private,GenerateAccessors */
 	static DependencyProperty *SettersProperty;
  	/* @PropertyType=Managed,ManagedPropertyType=System.Type,Access=Internal,ManagedAccess=Public,ManagedFieldAccess=Internal */
@@ -39,6 +41,12 @@ class Style : public DependencyObject {
 
 	SetterBaseCollection* GetSetters();
 	void SetSetters (SetterBaseCollection* value);
+
+	bool GetIsSealed();
+	void SetIsSealed(bool value);
+
+	/* @GenerateCBinding,GeneratePInvoke */
+	void Seal ();
 };
 
 //
@@ -54,8 +62,21 @@ class SetterBaseCollection : public DependencyObjectCollection {
 	/* @GenerateCBinding,GeneratePInvoke */
 	SetterBaseCollection ();
 	
+  	/* @PropertyType=bool,DefaultValue=false,ManagedSetterAccess=Private,GenerateAccessors */
+	static DependencyProperty *IsSealedProperty;
+	
+	virtual int AddWithError (Value *value, MoonError *error);
+	virtual int InsertWithError (int index, Value *value, MoonError *error);
+	virtual bool SetValueAtWithError (int index, Value *value, MoonError *error);
+	
 	virtual Type::Kind GetObjectType () { return Type::SETTERBASE_COLLECTION; }
 	virtual Type::Kind GetElementType () { return Type::SETTERBASE; }
+	Style *style;
+
+	bool GetIsSealed();
+	void SetIsSealed(bool value);
+	
+	void Seal ();
 };
 
 //
@@ -68,10 +89,22 @@ class SetterBase : public DependencyObject {
 	virtual ~SetterBase () { }
 	
  public:
+   	/* @PropertyType=bool,DefaultValue=false,ManagedSetterAccess=Private,GenerateAccessors */
+	static DependencyProperty *IsSealedProperty;
+	
 	/* @GenerateCBinding,GeneratePInvoke,ManagedAccess=Internal */
 	SetterBase ();
-	
+
+	virtual bool SetValueWithErrorImpl (DependencyProperty *property, Value *value, MoonError *error);
 	virtual Type::Kind GetObjectType () { return Type::SETTERBASE; }
+
+	bool GetIsSealed();
+	void SetIsSealed(bool value);
+	
+	void Seal ()
+	{
+		SetIsSealed (true);
+	}
 };
 
 //
@@ -94,7 +127,7 @@ class Setter : public SetterBase {
 	
 	/* @GenerateCBinding,GeneratePInvoke */
 	Setter ();
-	
+
 	virtual Type::Kind GetObjectType () { return Type::SETTER; }
 };
 

class/Mono.Moonlight/Mono/GeneratedPInvokes.cs
@@ -54,6 +54,10 @@ namespace Mono {
 		public extern static IntPtr binding_expression_base_get_property (IntPtr instance);
 
 		[DllImport ("moon")]
+		// Value *binding_expression_base_get_value (BindingExpressionBase *instance);
+		public extern static IntPtr binding_expression_base_get_value (IntPtr instance);
+
+		[DllImport ("moon")]
 		// void binding_expression_base_set_binding (BindingExpressionBase *instance, Binding *binding);
 		public extern static void binding_expression_base_set_binding (IntPtr instance, IntPtr binding);
 
@@ -64,6 +68,10 @@ namespace Mono {
 		[DllImport ("moon")]
 		// void binding_expression_base_set_property (BindingExpressionBase *instance, DependencyProperty *property);
 		public extern static void binding_expression_base_set_property (IntPtr instance, IntPtr property);
+
+		[DllImport ("moon")]
+		// void binding_expression_base_update_source (BindingExpressionBase *instance, Value *value);
+		public extern static void binding_expression_base_update_source (IntPtr instance, ref Value value);
 
 		[DllImport ("moon")]
 		// BitmapImage *bitmap_image_new ();
@@ -1054,6 +1062,10 @@ namespace Mono {
 		[DllImport ("moon")]
 		// StrokeCollection *stroke_collection_new ();
 		public extern static IntPtr stroke_collection_new ();
+
+		[DllImport ("moon")]
+		// void style_seal (Style *instance);
+		public extern static void style_seal (IntPtr instance);
 
 		[DllImport ("moon")]
 		// Style *style_new ();

class/Mono.Moonlight/Mono/NativeMethods.cs
@@ -289,6 +289,8 @@ namespace Mono {
 				Exception e = (Exception)Activator.CreateInstance (t, BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { msg }, CultureInfo.CurrentCulture );
 				throw e;
 			}
+			case 7:
+				throw new UnauthorizedAccessException (msg);
 			}
 		}
 	}

class/System.Windows/System.Windows/DependencyProperty.g.cs
@@ -199,9 +199,33 @@ namespace System.Windows {
 		}
 	}
 
+	partial class SetterBase {
+		public static readonly DependencyProperty IsSealedProperty = DependencyProperty.Lookup (Kind.SETTERBASE, "IsSealed", typeof (bool));
+
+		public bool IsSealed {
+			get { return (bool) GetValue (IsSealedProperty); }
+			private set { SetValue (IsSealedProperty, value); }
+		}
+	}
+
+	partial class SetterBaseCollection {
+		public static readonly DependencyProperty IsSealedProperty = DependencyProperty.Lookup (Kind.SETTERBASE_COLLECTION, "IsSealed", typeof (bool));
+
+		public bool IsSealed {
+			get { return (bool) GetValue (IsSealedProperty); }
+			private set { SetValue (IsSealedProperty, value); }
+		}
+	}
+
 	partial class Style {
+		public static readonly DependencyProperty IsSealedProperty = DependencyProperty.Lookup (Kind.STYLE, "IsSealed", typeof (bool));
 		private static readonly DependencyProperty SettersProperty = DependencyProperty.Lookup (Kind.STYLE, "Setters", typeof (SetterBaseCollection));
 		internal static readonly DependencyProperty TargetTypeProperty = DependencyProperty.Lookup (Kind.STYLE, "TargetType", typeof (System.Type));
+
+		public bool IsSealed {
+			get { return (bool) GetValue (IsSealedProperty); }
+			private set { SetValue (IsSealedProperty, value); }
+		}
 
 		public SetterBaseCollection Setters {
 			get { return (SetterBaseCollection) GetValue (SettersProperty); }

class/System.Windows/System.Windows/Setter.cs
@@ -37,6 +37,9 @@ namespace System.Windows {
 
 		public Setter (DependencyProperty property, object value)
 		{
+			if (property == null)
+				throw new NullReferenceException ();
+			
 			Property = property;
 			Value = value;
 		}

class/System.Windows/System.Windows/SetterBase.cs
@@ -31,16 +31,7 @@ using Mono;
 namespace System.Windows {
 
 	public abstract partial class SetterBase : DependencyObject {
-		bool isSealed;
-		
-		public bool IsSealed {
-			get { return isSealed; }
-		}
 
-		internal void Seal ()
-		{
-			isSealed = true;
-		}
 	}
 
 }

commit c90f929e7f3a49aae9211dcd6bf7340e38ae21e4

* moon/src/style.h: * moon/src/style.cpp: * moon/src/collection.h: * moon/src/collection.cpp: InsertWithError should return a bool to match   Insert
* moon/src/media.h:
* moon/src/media.cpp: Fix regression introduced in r119963 when changing
  which collection functions were virtual

svn path=/trunk/moon/; revision=120068

src/collection.cpp
@@ -121,7 +121,7 @@ Collection::Insert (int index, Value value)
 	return Insert (index, &value);
 }
 
-int
+bool
 Collection::InsertWithError (int index, Value *value, MoonError *error)
 {
 	Value *added;

src/collection.h
@@ -73,7 +73,7 @@ public:
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
 	virtual int AddWithError (Value *value, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
-	virtual int InsertWithError (int index, Value *value, MoonError *error);
+	virtual bool InsertWithError (int index, Value *value, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */
 	virtual Value *GetValueAtWithError (int index, MoonError *error);
 	/* @GenerateCBinding,GeneratePInvoke,Version=2.0 */

src/media.cpp
@@ -968,7 +968,7 @@ MediaAttributeCollection::GetItemByName (const char *name)
 //
 
 int
-TimelineMarkerCollection::Add (Value *value)
+TimelineMarkerCollection::AddWithError (Value *value, MoonError *error)
 {
 	TimelineMarker *marker, *cur;
 	
@@ -977,18 +977,18 @@ TimelineMarkerCollection::Add (Value *value)
 	for (guint i = 0; i < array->len; i++) {
 		cur = ((Value *) array->pdata[i])->AsTimelineMarker ();
 		if (cur->GetTime () >= marker->GetTime ()) {
-			DependencyObjectCollection::Insert (i, value);
+			DependencyObjectCollection::InsertWithError (i, value, error);
 			return i;
 		}
 	}
 	
-	return DependencyObjectCollection::Insert (array->len, value) ? array->len - 1 : -1;
+	return DependencyObjectCollection::InsertWithError (array->len, value, error) ? array->len - 1 : -1;
 }
 
 bool
-TimelineMarkerCollection::Insert (int index, Value *value)
+TimelineMarkerCollection::InsertWithError (int index, Value *value, MoonError *error)
 {
-	return Add (value) != -1;
+	return AddWithError (value, error) != -1;
 }
 
 

src/media.h
@@ -89,8 +89,8 @@ class TimelineMarkerCollection : public DependencyObjectCollection {
 	virtual Type::Kind GetObjectType () { return Type::TIMELINEMARKER_COLLECTION; }
 	virtual Type::Kind GetElementType () { return Type::TIMELINEMARKER; }
 	
-	virtual int Add (Value *value);
-	virtual bool Insert (int index, Value *value);
+	virtual int AddWithError (Value *value, MoonError *error);
+	virtual bool InsertWithError (int index, Value *value, MoonError *error);
 };
 
 

src/style.cpp
@@ -44,7 +44,7 @@ SetterBaseCollection::AddWithError (Value *value, MoonError *error)
 	return DependencyObjectCollection::AddWithError (value, error);	
 }
 
-int
+bool
 SetterBaseCollection:: InsertWithError (int index, Value *value, MoonError *error)
 {
 	if (style && style->GetIsSealed ()) {

src/style.h
@@ -66,7 +66,7 @@ class SetterBaseCollection : public DependencyObjectCollection {
 	static DependencyProperty *IsSealedProperty;
 	
 	virtual int AddWithError (Value *value, MoonError *error);
-	virtual int InsertWithError (int index, Value *value, MoonError *error);
+	virtual bool InsertWithError (int index, Value *value, MoonError *error);
 	virtual bool SetValueAtWithError (int index, Value *value, MoonError *error);
 	
 	virtual Type::Kind GetObjectType () { return Type::SETTERBASE_COLLECTION; }