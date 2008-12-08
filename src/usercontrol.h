f334ffd327d5eec2a459b63645ff6400f5d6f248
Sebastien Pouliot <sebastien@ximian.com>
2008-12-08 21:41:36 +0000

    2008-12-08  Sebastien Pouliot  <sebastien@ximian.com>
    	* usercontrol.h: Tag with @CallInitialize since IsTabStop is false
    	for UserControl while it's true for Control
    
    
    svn path=/trunk/moon/; revision=121042

--- src/usercontrol.h
+++ src/usercontrol.h
@@ -23,6 +23,7 @@ G_BEGIN_DECLS
 /* @SilverlightVersion="2" */
 /* @ContentProperty="Content" */
 /* @Namespace=System.Windows.Controls */
+/* @CallInitialize */
 class UserControl : public Control {
 protected:
 	virtual ~UserControl ();
