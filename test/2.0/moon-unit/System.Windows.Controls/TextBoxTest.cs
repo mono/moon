290daa89d6c89c0698990a75bd779acacc08a4f1
Alan McGovern <alan.mcgovern@gmail.com>
2008-12-10 20:27:17 +0000

    2008-12-10  Alan McGovern  <amcgovern@novell.com>
    	* System.Windows.Controls/TextBoxTest: Added tests for
    	default values and invalid values.
    	* moon-unit.csproj: Added TextBoxTest.cs to the build
    
    svn path=/trunk/moon/; revision=121250

---
+++ test/2.0/moon-unit/System.Windows.Controls/TextBoxTest.cs
@@ -0,0 +1,109 @@
+﻿//
+// TextBlock Unit Tests
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright 2008 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Mono.Moonlight.UnitTesting
+{
+    [TestClass]
+    public class TextBoxTest
+    {
+        TextBox box;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            box = new TextBox();
+        }
+
+        [TestMethod]
+        public void Defaults()
+        {
+            Assert.AreEqual(false, box.AcceptsReturn, "#1");
+            Assert.AreEqual(null, box.FontSource, "#2");
+            Assert.AreEqual(ScrollBarVisibility.Hidden, box.HorizontalScrollBarVisibility, "#3");
+            Assert.AreEqual(false, box.IsReadOnly, "#4");
+            Assert.AreEqual(0, box.MaxLength, "#5");
+            Assert.AreEqual("", box.SelectedText, "#6");
+            Assert.AreEqual(null, box.SelectionBackground, "#7");
+            Assert.AreEqual(null, box.SelectionForeground, "#8");
+            Assert.AreEqual(0, box.SelectionLength, "#9");
+            Assert.AreEqual(0, box.SelectionStart, "#10");
+            Assert.AreEqual("", box.Text, "#11");
+            Assert.AreEqual(TextAlignment.Left, box.TextAlignment, "#12");
+            Assert.AreEqual(TextWrapping.NoWrap, box.TextWrapping, "#13");
+            Assert.AreEqual(ScrollBarVisibility.Hidden, box.VerticalScrollBarVisibility, "#14");
+        }
+
+        [TestMethod]
+        public void InvalidValues()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(delegate {
+                box.MaxLength = -1;
+            });
+            Assert.Throws<Exception>(delegate {
+                box.SetValue(PasswordBox.MaxLengthProperty, -1);
+            }, "#1");
+            box.SelectedText = "BLAH";
+            Assert.AreEqual("BLAH", box.Text, "#2");
+            Assert.Throws<ArgumentOutOfRangeException>(delegate {
+                box.SelectionLength = -1;
+            }, "#3");
+
+            box.SelectionStart = 6;
+            Assert.AreEqual(4, box.SelectionStart);
+            Assert.AreEqual("", box.SelectedText, "#3a");
+
+            box.SelectionStart = 2;
+            box.SelectionLength = 10;
+            Assert.AreEqual(2, box.SelectionLength, "#3b");
+            Assert.AreEqual("AH", box.SelectedText, "#3c");
+
+            Assert.Throws<ArgumentOutOfRangeException>(delegate {
+                box.SelectionStart = -1;
+            }, "#5");
+            Assert.Throws<ArgumentNullException>(delegate {
+                box.Text = null;
+            }, "#6");
+            Assert.Throws<Exception>(delegate {
+                box.SetValue(TextBlock.TextProperty, null);
+            }, "#7");
+        }
+    }
+}
