43cb1fee6d6d0a16930be9432f5c5fcebf9e86d2
Jackson Harper <jackson@novell.com>
2008-12-10 19:29:16 +0000

    Updated test.
    svn path=/trunk/moon/; revision=121240

--- test/2.0/ManagedAttachedProps/Page.xaml.cs
+++ test/2.0/ManagedAttachedProps/Page.xaml.cs
@@ -1,12 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
-namespace ManagedAttachedProps {
+namespace ManagedAttachedProps
+{
 	public partial class Page : UserControl
 	{
 		public Page()
 		{
 			InitializeComponent();
+			Console.WriteLine("everything initialized!");
+
+			// Stretcher s = (Stretcher) FindName("the_stretcher");
+			// string amount = Stretcher.GetStretchAmount(the_rect);
 		}
+
 	}
 }
---
+++ test/2.0/ManagedAttachedProps/Stretcher.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Net;
+using System.Windows;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ManagedAttachedProps
+{
+
+	public class StretchAmountConverter : TypeConverter {
+
+		public override bool CanConvertFrom (ITypeDescriptorContext context, Type source_type)
+		{
+			if (source_type == typeof (string))
+				return true;
+			return base.CanConvertFrom (context, source_type);
+		}
+
+		public override object ConvertFrom (ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			string sv = value as string;
+			if (sv != null) {
+				Stretch s = new Stretch ();
+				int amount;
+				if (Int32.TryParse (sv, out amount)) {
+					s.Amount = amount;
+					return s;
+				}
+			}
+			return base.ConvertFrom (context, culture, value);
+		}
+
+
+	}
+
+	[TypeConverter (typeof (StretchAmountConverter))]
+	public class Stretch
+	{
+		private int amount;
+
+		public int Amount {
+			get { return amount; }
+			set { amount = value; }
+		}
+	}
+
+        public class Stretcher : Panel
+        {
+		public static readonly DependencyProperty StretchAmountProperty = DependencyProperty.RegisterAttached("StretchAmount", typeof(Stretch), typeof(Stretcher), null);
+
+		public Stretcher()
+		{
+		}
+
+		public static void SetStretchAmount(DependencyObject obj, Stretch amount)
+		{
+			FrameworkElement elem = obj as FrameworkElement;
+			if (elem != null)
+			{
+				elem.Width *= amount.Amount;
+			}
+
+			obj.SetValue(StretchAmountProperty, amount);
+
+			Console.WriteLine("SET STRETCH AMOUNT");
+		}
+
+		public static string GetStretchAmount(DependencyObject obj)
+		{
+			return (string) obj.GetValue(StretchAmountProperty);
+		}
+        }
+}
