b1aefab242c6bf0368f5314e4406abf2d15270c8
Sebastien Pouliot <sebastien@ximian.com>
2008-12-15 18:41:02 +0000

    2008-12-15  Sebastien Pouliot  <sebastien@ximian.com>
    	* System.Windows/ThicknessTest.cs: Add test cases for special Double
    	values wrt validation (none) and ToString (NaN == Auto).
    
    
    svn path=/trunk/moon/; revision=121532

--- test/2.0/moon-unit/System.Windows/ThicknessTest.cs
+++ test/2.0/moon-unit/System.Windows/ThicknessTest.cs
@@ -1,17 +1,34 @@
-using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Collections.Generic;
-using Mono.Moonlight.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+//
+// Thickness Unit Tests
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright 2008 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
+using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MoonTest.System.Windows
 {
@@ -29,6 +46,49 @@ namespace MoonTest.System.Windows
 
 			t = new Thickness (-1);
 			Assert.AreEqual ("-1,-1,-1,-1", t.ToString());
+		}
+
+		[TestMethod]
+		public void SpecialCases ()
+		{
+			Thickness t = new Thickness (Double.MinValue);
+			Assert.AreEqual ("-1.79769313486232E+308,-1.79769313486232E+308,-1.79769313486232E+308,-1.79769313486232E+308", t.ToString (), "MinValue");
+
+			t = new Thickness (Double.MaxValue);
+			Assert.AreEqual ("1.79769313486232E+308,1.79769313486232E+308,1.79769313486232E+308,1.79769313486232E+308", t.ToString (), "MaxValue");
+
+			t = new Thickness(Double.NegativeInfinity);
+			Assert.AreEqual ("-Infinity,-Infinity,-Infinity,-Infinity", t.ToString (), "-Infinity");
+
+			t = new Thickness(Double.PositiveInfinity);
+			Assert.AreEqual ("Infinity,Infinity,Infinity,Infinity", t.ToString (), "Infinity");
+
+			t = new Thickness (Double.NaN);
+			Assert.AreEqual ("Auto,Auto,Auto,Auto", t.ToString ());
+
+			t = new Thickness (Double.MinValue, Double.MaxValue, Double.NegativeInfinity, Double.PositiveInfinity);
+			Assert.AreEqual ("-1.79769313486232E+308,1.79769313486232E+308,-Infinity,Infinity", t.ToString (), "Mix");
+			Assert.IsTrue (t.Equals (t), "Mix.Equals");
+
+			t.Bottom = Double.NaN;
+			Assert.IsFalse (t.Equals (t), "MixNaN.Equals");
+		}
+
+		[TestMethod]
+		public void NaN ()
+		{
+			Thickness t1 = new Thickness (Double.NaN);
+			Assert.IsFalse (t1.Equals (t1), "NaN-1");
+
+			Assert.IsTrue (Double.IsNaN (t1.Left), "Left");
+			Assert.IsTrue (Double.IsNaN (t1.Top), "Top");
+			Assert.IsTrue (Double.IsNaN (t1.Right), "Right");
+			Assert.IsTrue (Double.IsNaN (t1.Bottom), "Bottom");
+
+			Thickness t2 = new Thickness (Double.NaN);
+			Assert.IsFalse (t1.Equals (t2), "NaN-2");
+			Assert.IsFalse (t1 == t2, "NaN-3");
+			Assert.IsTrue (t1 != t2, "NaN-4");
 		}
 	}
 }
