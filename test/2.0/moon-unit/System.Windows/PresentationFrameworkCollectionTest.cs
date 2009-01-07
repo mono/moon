73ad4942c781a470f5c53877c61bc7ba676c1798
Sebastien Pouliot <sebastien@ximian.com>
2009-01-07 19:10:43 +0000

    System.Windows/PresentationFrameworkCollectionTest.cs: Add test cases for checking public arguments.
    svn path=/trunk/moon/; revision=122697

--- test/2.0/moon-unit/System.Windows/PresentationFrameworkCollectionTest.cs
+++ test/2.0/moon-unit/System.Windows/PresentationFrameworkCollectionTest.cs
@@ -30,5 +30,24 @@ namespace MoonTest.System.Windows
 
 			Assert.Throws<ArgumentOutOfRangeException> (() => children.RemoveAt (1));
 		}
+
+		[TestMethod]
+		public void ArgumentChecks ()
+		{
+			Canvas canvas = new Canvas ();
+			UIElementCollection c = canvas.Children;
+
+			Assert.Throws<ArgumentNullException> (() => c.Add (null), "Add");
+			Assert.Throws<ArgumentNullException> (() => c.Insert (0, null), "Insert null");
+			Assert.Throws<ArgumentOutOfRangeException> (() => c.Insert (-1, new Rectangle ()), "Insert negative");
+			Assert.Throws<ArgumentOutOfRangeException> (() => c.RemoveAt (-1), "RemoveAt negative");
+			Assert.Throws<ArgumentOutOfRangeException> (() => Console.WriteLine (c [-1] == null), "this getter");
+			Assert.Throws<ArgumentOutOfRangeException> (() => c [-1] = new Rectangle (), "this setter");
+
+			Assert.IsFalse (c.Contains (null), "Contains");
+			Assert.IsFalse (c.Remove (null), "Remove");
+
+			Assert.AreEqual (-1, c.IndexOf (null), "IndexOf");
+		}
 	}
 }
