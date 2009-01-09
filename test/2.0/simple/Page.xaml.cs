74f20f9cecebf1d250c1d6d67c54045e0ead509f
Larry Ewing <lewing@novell.com>
2009-01-09 22:00:36 +0000

    Add a test image for url loading problems.
    svn path=/trunk/moon/; revision=122950

--- test/2.0/simple/Page.xaml.cs
+++ test/2.0/simple/Page.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
@@ -17,6 +18,10 @@ namespace simple
 		public Page()
 		{
 			InitializeComponent();
+
+			var image = new Image ();
+			Children.Add (image);
+			image.Source = new BitmapImage (new Uri ("mono.png", UriKind.RelativeOrAbsolute));
 		}
 	}
 }
