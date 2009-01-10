9ac98052aa5e4a2dc677915dac24db7c52e2dd4c
Jeffrey Stedfast <fejj@novell.com>
2009-01-10 20:50:12 +0000

    2009-01-10  Jeffrey Stedfast  <fejj@novell.com>
    	* plugin.cpp (moonlight_authors): Updated.
    
    
    svn path=/trunk/moon/; revision=122991

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -52,23 +52,35 @@ extern guint32 moonlight_flags;
 
 /* gleaned from svn log of the moon module, as well as olive/class/{agclr,agmono,System.Silverlight} */
 static const char *moonlight_authors[] = {
+	"Aaron Bockover <abockover@novell.com>",
+	"Alan McGovern <amcgovern@novell.com>",
+	"Alp Toker <alp@nuanti.com>",
 	"Andreia Gaita <avidigal@novell.com>",
+	"Andrew Jorgensen <ajorgensen@novell.com>",
+	"Argiris Kirtzidis <akyrtzi@gmail.com>",
 	"Atsushi Enomoto <atsushi@ximian.com>",
 	"Chris Toshok <toshok@ximian.com>",
 	"Dick Porter <dick@ximian.com>",
 	"Everaldo Canuto <ecanuto@novell.com>",
+	"Fernando Herrera <fherrera@novell.com>",
+	"Geoff Norton <gnorton@novell.com>",
 	"Jackson Harper <jackson@ximian.com>",
+	"Jb Evain <jbevain@novell.com>",
 	"Jeffrey Stedfast <fejj@novell.com>",
 	"Larry Ewing <lewing@novell.com>",
+	"Manuel Ceron <ceronman@unicauca.edu.co>",
 	"Marek Habersack <mhabersack@novell.com>",
+	"Michael Dominic K. <mdk@mdk.am>",
+	"Michael Hutchinson <mhutchinson@novell.com>",
 	"Miguel de Icaza <miguel@novell.com>",
+	"Paolo Molaro <lupus@ximian.com>",
+	"Raja R Harinath <harinath@hurrynot.org>",
 	"Rodrigo Kumpera <rkumpera@novell.com>",
 	"Rolf Bjarne Kvinge <RKvinge@novell.com>",
+	"Rusty Howell <rhowell@novell.com>",
 	"Sebastien Pouliot <sebastien@ximian.com>",
-	"Jb Evain <jbevain@novell.com>",
-	"Geoff Norton <gnorton@novell.com>",
-	"Michael Dominic K. <mdk@mdk.am>",
 	"Stephane Delcroix <sdelcroix@novell.com>",
+	"Zoltan Varga <vargaz@gmail.com>",
 	NULL
 };
 
