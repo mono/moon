2008c19890986a3623ca67a1010a7553cea5953b
Jeffrey Stedfast <fejj@novell.com>
2009-01-14 20:37:56 +0000

    do much more useful stuff for testing selection and textual content
    svn path=/trunk/moon/; revision=123407

--- test/2.0/TextBoxExample/Page.xaml.cs
+++ test/2.0/TextBoxExample/Page.xaml.cs
@@ -9,7 +9,6 @@ namespace TextBoxExample {
 		public Page ()
 		{
 			InitializeComponent ();
-			SetVisualTreeText (txtTextBox);
 			
 			txtTextBox.SelectionChanged += OnSelectionChanged;
 			txtTextBox.TextChanged += OnTextChanged;
@@ -19,40 +18,27 @@ namespace TextBoxExample {
 			//cbAcceptReturn.Checked += OnChecked;
 		}
 		
-		void DumpVisualTree (StringBuilder sb, DependencyObject obj, int depth)
-		{
-			DependencyObject child;
-			
-			for (int i = 0; i < VisualTreeHelper.GetChildrenCount (obj); i++) {
-				child = VisualTreeHelper.GetChild (obj, i);
-				sb.Append (' ', depth * 4);
-				sb.Append (child.ToString ());
-				sb.Append ('\n');
-				
-				if (VisualTreeHelper.GetChildrenCount (child) > 0)
-					DumpVisualTree (sb, child, depth + 1);
-			}
-		}
-		
-		void SetVisualTreeText (TextBox textbox)
-		{
-			StringBuilder sb = new StringBuilder ();
-			
-			sb.Append ("The current Visual Tree for the TextBox content is:\n");
-			DumpVisualTree (sb, textbox, 0);
-			
-			txtVisualTree.Text = sb.ToString ();
-		}
-		
 		void OnSelectionChanged (object sender, EventArgs args)
 		{
-			SetVisualTreeText (txtTextBox);
+			string text = txtTextBox.SelectedText;
+			
+			if (text != "") {
+				txtSelection.Foreground = new SolidColorBrush (Colors.White);
+				rectSelection.Fill = new SolidColorBrush (Colors.Blue);
+				txtSelection.Text = text;
+			} else {
+				txtSelection.Foreground = new SolidColorBrush (Colors.Black);
+				rectSelection.Fill = new SolidColorBrush (Colors.White);
+				txtSelection.Text = "No text selected";
+			}
+			
+			rectSelection.Height = txtSelection.ActualHeight;
+			rectSelection.Width = txtSelection.ActualWidth;
 		}
 		
 		void OnTextChanged (object sender, EventArgs args)
 		{
 			txtTextBlock.Text = txtTextBox.Text;
-			SetVisualTreeText (txtTextBox);
 		}
 		
 		//void OnUnchecked (object sender, EventArgs args)
