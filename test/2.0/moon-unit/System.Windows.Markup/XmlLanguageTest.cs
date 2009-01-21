c6587d7c716450844e9f967fb015991f20874e83
Sebastien Pouliot <sebastien@ximian.com>
2009-01-21 14:36:36 +0000

    2009-01-21  Sebastien Pouliot  <sebastien@ximian.com>
    	* System.Windows.Markup/XmlLanguageTest.cs: New.
    
    
    svn path=/trunk/moon/; revision=124035

---
+++ test/2.0/moon-unit/System.Windows.Markup/XmlLanguageTest.cs
@@ -0,0 +1,67 @@
+//
+// Unit tests for XmlLanguageTest
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright (C) 2009 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Markup;
+
+using Mono.Moonlight.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoonTest.System.Windows.Documents {
+
+	[TestClass]
+	public class XmlLanguageTest {
+
+		[TestMethod]
+		public void GetLanguage ()
+		{
+			Assert.Throws<NullReferenceException> (delegate {
+				XmlLanguage.GetLanguage (null);
+			}, "null");
+
+			XmlLanguage empty = XmlLanguage.GetLanguage (String.Empty);
+			Assert.AreEqual (String.Empty, empty.IetfLanguageTag, "Empty/IetfLanguageTag");
+
+			XmlLanguage doesnotexists = XmlLanguage.GetLanguage ("fr-qc");
+			Assert.AreEqual ("fr-qc", doesnotexists.IetfLanguageTag, "doesnotexists/IetfLanguageTag");
+		}
+
+		[TestMethod]
+		public void LowerCase ()
+		{
+			XmlLanguage enus = XmlLanguage.GetLanguage ("en-us");
+			XmlLanguage enUS = XmlLanguage.GetLanguage ("en-US");
+			Assert.AreEqual (enus, enUS, "Equals");
+
+			Assert.AreEqual ("en-us", enus.IetfLanguageTag, "en-us");
+			Assert.AreEqual ("en-us", enUS.IetfLanguageTag, "en-US");
+		}
+	}
+}
