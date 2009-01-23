d8ff38b0245fce7d4a3150c49b7afc810a9d2757
Sebastien Pouliot <sebastien@ximian.com>
2009-01-23 13:59:26 +0000

    System.Windows.Media/ColorTest.cs: Add test case for defaults including ToString. Re-enable parsing Color from XAML (crasher fixed)
    svn path=/trunk/moon/; revision=124325

--- test/2.0/moon-unit/System.Windows.Media/ColorTest.cs
+++ test/2.0/moon-unit/System.Windows.Media/ColorTest.cs
@@ -1,31 +1,61 @@
+//
+// Unit tests for Color
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright (C) 2009 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using System;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using System.Collections.Generic;
+
 using Mono.Moonlight.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MoonTest.System.Windows.Media
-{
+namespace MoonTest.System.Windows.Media {
+
 	[TestClass]
-	public class ColorTest
-	{
+	public class ColorTest {
 
 		[TestMethod]
-		[Ignore ("Crashes moonlight at the moment")]
+		public void Defaults ()
+		{
+			Color c = new Color ();
+			Assert.AreEqual (0, c.R, "R");
+			Assert.AreEqual (0, c.G, "G");
+			Assert.AreEqual (0, c.B, "B");
+			Assert.AreEqual (0, c.A, "A");
+			Assert.AreEqual ("#00000000", c.ToString (), "ToString");
+		}
+
+		[TestMethod]
 		public void ParseValidColor ()
 		{
-			Assert.Throws (delegate { XamlReader.Load (@"<Color xmlns=""http://schemas.microsoft.com/client/2007"">#ffffff</Color>");
-				},
-				typeof (XamlParseException));
+			Assert.Throws<XamlParseException> (delegate {
+				XamlReader.Load (@"<Color xmlns=""http://schemas.microsoft.com/client/2007"">#ffffff</Color>");
+			}, "bad xaml");
 		}
 	}
 }
