d415bc3978467adbd93a36cbf4bf6747240e5837
Chris Toshok <toshok@novell.com>
2009-01-31 01:37:49 +0000

    handle ObjectAnimationUsingKeyFrames
    svn path=/trunk/moon/; revision=125193

--- class/System.Windows/Mono/NativeDependencyObjectHelper.cs
+++ class/System.Windows/Mono/NativeDependencyObjectHelper.cs
@@ -258,6 +258,7 @@ namespace Mono {
 			case Kind.MATRIXTRANSFORM: return new MatrixTransform (raw);
 			case Kind.MEDIAELEMENT: return new MediaElement (raw);
 			case Kind.MULTISCALEIMAGE: return new MultiScaleImage (raw);
+			case Kind.OBJECTANIMATIONUSINGKEYFRAMES: return new ObjectAnimationUsingKeyFrames (raw);
 			case Kind.PASSWORDBOX: return new PasswordBox (raw);
 			case Kind.PATHFIGURE_COLLECTION: return new PathFigureCollection (raw);
 			case Kind.PATHFIGURE: return new PathFigure (raw);
