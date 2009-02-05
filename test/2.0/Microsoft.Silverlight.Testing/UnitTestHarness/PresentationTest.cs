5f81f0b056f25deb7cc9d54a3154b7ae9de0feae
Alan McGovern <alan.mcgovern@gmail.com>
2009-02-05 11:21:16 +0000

    2009-02-05  Alan McGovern  <amcgovern@novell.com>
    	* PresentationTest.cs: Add a helper method (CreateAsyncTest) to allow
    	asyncronous testing of controls using the TestPanel.
    
    svn path=/trunk/moon/; revision=125844

--- test/2.0/Microsoft.Silverlight.Testing/UnitTestHarness/PresentationTest.cs
+++ test/2.0/Microsoft.Silverlight.Testing/UnitTestHarness/PresentationTest.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using Microsoft.Silverlight.Testing.UnitTesting;
 using Microsoft.Silverlight.Testing.UnitTesting.UI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
 
 namespace Microsoft.Silverlight.Testing
 {
@@ -21,6 +22,8 @@ namespace Microsoft.Silverlight.Testing
     /// </summary>
     public abstract class PresentationTest : WorkItemTest
     {
+        public static int VisualDelayInMilliseconds = 100;
+
         /// <summary>
         /// Gets the test panel.
         /// </summary>
@@ -28,6 +31,30 @@ namespace Microsoft.Silverlight.Testing
         public Panel TestPanel
         {
             get { return TestPanelManager.Instance.TestPanel; }
+        }
+
+        protected void CreateAsyncTest(FrameworkElement element, params Action[] actions)
+        {
+            Assert.IsNotNull(element);
+            actions = actions ?? new Action[] { };
+
+            // Add the element to the test surface and wait until it's loaded
+            bool isLoaded = false;
+            element.Loaded += delegate { isLoaded = true; };
+            TestPanel.Children.Add(element);
+            EnqueueConditional(() => isLoaded);
+
+            // Perform the test actions
+            foreach (Action action in actions)
+            {
+                Action capturedAction = action;
+                EnqueueCallback(() => capturedAction());
+                EnqueueSleep(VisualDelayInMilliseconds);
+            }
+
+            // Remove the element and complete the test
+            EnqueueCallback(() => TestPanel.Children.Remove(element));
+            EnqueueTestComplete();
         }
     }
 }
