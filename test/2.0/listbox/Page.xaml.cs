b0d480792344972534f1ac3bf6a2990e7629441a
Chris Toshok <toshok@novell.com>
2009-02-14 06:28:35 +0000

    move back to using a grid, add a second row.  also set foreground on a couple of the textblocks
    svn path=/trunk/moon/; revision=126915

--- test/2.0/listbox/Page.xaml.cs
+++ test/2.0/listbox/Page.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -40,6 +41,27 @@ namespace listbox
 		public void databoundButton_Click (object sender, RoutedEventArgs e)
 		{
 			stringCollection.Add (string.Format ("string {0}", stringCollection.Count));
+		}
+
+		public void horizButtonClicked (object sender, RoutedEventArgs e)
+		{
+			StringBuilder builder = new StringBuilder ();
+			DumpTree (horizontalItemsControl, builder);
+			log.Text = builder.ToString();
+		}
+
+		void DumpTree (DependencyObject root, StringBuilder builder)
+		{
+			builder.AppendFormat (string.Format ("{0}", root.ToString()));
+			if (VisualTreeHelper.GetChildrenCount (root) > 0) {
+				builder.Append (" -> { \n");
+				for (int i = 0; i < VisualTreeHelper.GetChildrenCount (root); i ++) {
+					DumpTree (VisualTreeHelper.GetChild (root, i), builder);
+					if (i < VisualTreeHelper.GetChildrenCount (root) - 1)
+						builder.Append (",\n");
+				}
+				builder.Append (" }");
+			}
 		}
 
 		int button_number;
