ce5f9a33f4398dfadf01fe2c349a4424b8b942d2
Jeffrey Stedfast <fejj@novell.com>
2009-02-20 19:29:20 +0000

    2009-02-20  Jeffrey Stedfast  <fejj@novell.com>
    	* System.Windows/PropertyPathTest.cs: Added new unit tests for
    	PropertyPath.
    
    
    svn path=/trunk/moon/; revision=127552

---
+++ test/2.0/moon-unit/System.Windows/PropertyPathTest.cs
@@ -0,0 +1,64 @@
+//
+// Unit tests for PropertyPath
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright (C) 2009 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+using Mono.Moonlight.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoonTest.System.Windows
+{
+	[TestClass]
+	public class PropertyPathTest
+	{
+		[TestMethod]
+		public void PropertyPathCtors ()
+		{
+			PropertyPath prop;
+			
+			Assert.Throws<ArgumentOutOfRangeException>(delegate {
+				prop = new PropertyPath (null, "arg1");
+			}, "null path throws ArgumentOutOfRangeException");
+			
+			prop = new PropertyPath (Rectangle.WidthProperty);
+			Assert.AreEqual ("(0)", prop.Path, "Normal PropertyPath");
+			
+			prop = new PropertyPath (Canvas.LeftProperty);
+			Assert.AreEqual ("(0)", prop.Path, "Attached PropertyPath");
+			
+			prop = new PropertyPath (5);
+			Assert.IsNull (prop.Path, "numeric PropertyPath is null");
+			
+			prop = new PropertyPath (null);
+			Assert.IsNull (prop.Path, "null PropertyPath is null");
+		}
+	}
+}
