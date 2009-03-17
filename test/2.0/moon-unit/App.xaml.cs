c4b7d8fa2ff68b319d10469e75f61d227f6b9372
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-03-17 14:41:37 +0000

    2009-03-17  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* test/2.0/moon-unit/Makefile.am:
    	* test/2.0/moon-unit/App.xaml.cs:
    	* test/2.0/moon-unit/moon-unit.mdp:
    	* test/2.0/moon-unit/site/moon-unit-log.xsl:
    	* test/2.0/moon-unit/site/MoonLogProvider.aspx:
    	* test/2.0/moon-unit/site/MoonLogProvider.aspx.cs:
    	* test/2.0/moon-unit/Mono.Moonlight.UnitTesting/MoonLogProvider.cs:
    	* test/2.0/moon-unit/Mono.Moonlight.UnitTesting/MoonlightBugAttribute.cs:
    	* test/2.0/moon-unit/Mono.Moonlight.UnitTesting/SilverlightBugAttribute.cs:
    	  Updated to use Mono.Moonlight.UnitTesting.dll.
    
    svn path=/trunk/moon/; revision=129589

--- test/2.0/moon-unit/App.xaml.cs
+++ test/2.0/moon-unit/App.xaml.cs
@@ -20,86 +20,16 @@ namespace Mono.Moonlight.UnitTesting
 {
 	public partial class App : Application
 	{
-		MoonLogProvider moonlog;
-		UIElement test_page;
-
 		public App()
 		{
 			this.Startup += this.Application_Startup;
-			this.Exit += this.Application_Exit;
-			this.UnhandledException += this.Application_UnhandledException;
 
 			InitializeComponent();
 		}
 
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			UnitTestSettings settings = new UnitTestSettings ();
-			moonlog = new MoonLogProvider ();
-			settings.TestHarness = new Microsoft.Silverlight.Testing.UnitTesting.Harness.UnitTestHarness ();
-			settings.TestAssemblies.Add (Assembly.GetExecutingAssembly ());
-			UnitTestSystem.PrepareCustomLogProviders (settings);
-			settings.LogProviders.Add (moonlog);
-			if (!string.IsNullOrEmpty (HtmlPage.Document.DocumentUri.Query))
-				settings.TagExpression = HtmlPage.Document.DocumentUri.Query.Substring (1);
-			test_page = UnitTestSystem.CreateTestPage (settings);
-			settings.TestHarness.TestHarnessCompleted += new EventHandler<TestHarnessCompletedEventArgs> (Harness_Completed);
-			this.RootVisual = test_page;
-
-		}
-
-		void Harness_Completed (object sender, TestHarnessCompletedEventArgs e)
-		{
-			try {
-				if (moonlog.ProcessCompleted (e, ShutdownHarness)) {
-					ShutdownHarness ();
-				}
-			} catch (Exception ex) {
-				Console.WriteLine (ex.Message);
-			}
-		}
-
-		void ShutdownHarness ()
-		{
-			try {
-				if (!test_page.CheckAccess ())
-					test_page.Dispatcher.BeginInvoke (ShutdownHarness);
-				else
-					HtmlPage.Window.Eval ("try { ShutdownHarness (); } catch (e) { }");
-			} catch (Exception ex) {
-				Console.WriteLine (ex.Message);
-			}
-		}
-
-		private void Application_Exit(object sender, EventArgs e)
-		{
-
-		}
-		private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
-		{
-			// If the app is running outside of the debugger then report the exception using
-			// the browser's exception mechanism. On IE this will display it a yellow alert 
-			// icon in the status bar and Firefox will display a script error.
-			if (!System.Diagnostics.Debugger.IsAttached)
-			{
-
-				// NOTE: This will allow the application to continue running after an exception has been thrown
-				// but not handled. 
-				// For production applications this error handling should be replaced with something that will 
-				// report the error to the website and stop the application.
-				e.Handled = true;
-
-				try
-				{
-					string errorMsg = e.ExceptionObject.Message + e.ExceptionObject.StackTrace;
-					errorMsg = errorMsg.Replace('"', '\'').Replace("\r\n", @"\n");
-
-					System.Windows.Browser.HtmlPage.Window.Eval("throw new Error(\"Unhandled Error in Silverlight 2 Application " + errorMsg + "\");");
-				}
-				catch (Exception)
-				{
-				}
-			}
+			this.RootVisual = Testing.CreateTestPage (this);
 		}
 	}
 
