d48293d9a97fe0baba994926f5c9c39698f44829
Alan McGovern <alan.mcgovern@gmail.com>
2009-03-30 14:37:25 +0000

    2009-03-30  Alan McGovern  <amcgovern@novell.com>
    	* test/2.0/moon-unit/BasicCanvas.xaml:
    	* test/2.0/moon-unit/System.Windows.Controls.Primitives/PopupTest.cs:
    	  Make the popup tests less sucky
    
    svn path=/trunk/moon/; revision=130553

--- test/2.0/moon-unit/System.Windows.Controls.Primitives/PopupTest.cs
+++ test/2.0/moon-unit/System.Windows.Controls.Primitives/PopupTest.cs
@@ -10,41 +10,16 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Windows.Controls.Primitives;
-using NS;
 using Mono.Moonlight.UnitTesting;
 using Microsoft.Silverlight.Testing;
 using System.Threading;
 
-namespace NS
-{
-    public partial class PopupTestClass : global::System.Windows.Controls.UserControl
-    {
-        public PopupTestClass()
-            : this(new Uri("/moon-unit;component/System.Windows/BasicCanvas.xaml", UriKind.Relative))
-        {
-        }
-
-        public PopupTestClass(Uri uri)
-        {
-            Application.LoadComponent(this, uri);
-            InitializeComponent();
-            LayoutRoot = (Canvas) FindName("LayoutRoot");
-        }
-    }
-}
 namespace MoonTest.System.Windows.Controls.Primitives
 {
     [TestClass]
-    public class PopupTest : Microsoft.Silverlight.Testing.SilverlightTest
+	public class ___PopupTest : Microsoft.Silverlight.Testing.SilverlightTest
     {
-        public static readonly DependencyProperty ChildProperty;
-        public static readonly DependencyProperty HorizontalOffsetProperty;
-        public static readonly DependencyProperty IsOpenProperty;
-        public static readonly DependencyProperty VerticalOffsetProperty;
-
         [TestMethod]
-        [MoonlightBug]
-        [Ignore]
         public void Defaults()
         {
             Popup p = new Popup();
@@ -55,13 +30,11 @@ namespace MoonTest.System.Windows.Controls.Primitives
         }
 
         [TestMethod]
-        [MoonlightBug]
-        [Ignore]
+		[MoonlightBug]
         public void NotInVisualTree()
         {
-            PopupTestClass c = new PopupTestClass();
             Rectangle r = new Rectangle{ Fill = new SolidColorBrush (Colors.Blue)};
-            c.LayoutRoot.Children.Add(r);
+            TestPanel.Children.Add (r);
             Popup pop = new Popup();
             Assert.Throws<ArgumentException>(delegate {
                 pop.Child = r;
@@ -70,47 +43,27 @@ namespace MoonTest.System.Windows.Controls.Primitives
 
         [TestMethod]
         [Asynchronous]
-        [Ignore ("This causes mono to crash horribly")]
         public void OpenCloseEventTest()
         {
+			bool opened = false;
             Button b = new Button { Content = "Close" };
-            Canvas canvas = new Canvas { Width = 100, Height = 100 };
-            canvas.Children.Add(b);
-            Border border = new Border
-            {
-                BorderBrush = new SolidColorBrush(Colors.Blue),
-                BorderThickness = new Thickness(1),
-                Child = canvas
-            };
-            Popup p = new Popup { Child = border };
+            Popup p = new Popup { Child = b };
 
-            ManualResetEvent handle = new ManualResetEvent(false);
-            p.Opened += delegate {
-                if (handle.WaitOne(10))
-                    throw new Exception("Already open");
-                handle.Set();
-            };
-            p.Closed += delegate {
-                if (handle.WaitOne(10))
-                    throw new Exception("Not already open");
-                handle.Set();
-            };
-            global::System.Threading.ThreadPool.QueueUserWorkItem(delegate {
-                p.Dispatcher.BeginInvoke(delegate {
-                    p.IsOpen = true;
-                    object o = p.Parent;
-                    o = null;
-                });
-                if (!handle.WaitOne(500))
-                    throw new Exception("Popup wasn't opened");
-                handle.Reset();
-                global::System.Threading.Thread.Sleep(10000);
-                p.Dispatcher.BeginInvoke(delegate { p.IsOpen = false; });
-                if (!handle.WaitOne(500))
-                    throw new Exception("Popup wasn't closed");
-                handle.Reset();
-                EnqueueTestComplete();
-            });
+			p.Opened += delegate { Assert.IsFalse (opened, "#1"); opened = true; };
+			p.Closed += delegate { Assert.IsTrue (opened, "#2"); opened = false; };
+
+			Enqueue (() => p.IsOpen = true);
+			Enqueue (() => {
+				Assert.IsTrue (p.IsOpen);
+				Assert.IsNull (p.Parent);
+				p.IsOpen = false;
+			});
+			Enqueue (() => {
+				Assert.IsFalse (p.IsOpen);
+				Assert.IsNull (p.Parent);
+			});
+
+			EnqueueTestComplete ();
         }
     }
 }
