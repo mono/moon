fc3de97baca7e695db90158578abaa11e7566f00
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2009-04-01 20:32:32 +0000

    2009-04-01  Stephane Delcroix  <sdelcroix@novell.com>
    	* application.cpp (GetResource): use the uri path, not
    	originalstring
    
    svn path=/trunk/moon/; revision=130797

--- src/application.cpp
+++ src/application.cpp
@@ -70,7 +70,7 @@ gpointer
 Application::GetResource (Uri *uri, int *size)
 {
 	if (get_resource_cb) {
-		return get_resource_cb (uri->GetOriginalString (), size);
+		return get_resource_cb (uri->GetPath (), size);
 	}
 
 	*size = 0;
