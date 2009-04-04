49698f9c5c40f6b3da889f3d9b103eb81853e2bd
Chris Toshok <toshok@novell.com>
2009-04-04 05:29:18 +0000

    add a nice demo to graph the easing functions
    svn path=/trunk/moon/; revision=131045

---
+++ test/3.0/EasingFunctions/EasingView.xaml.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EasingFunctions
+{
+	public partial class EasingView : UserControl
+	{
+		public static DependencyProperty EasingFunctionProperty = DependencyProperty.Register ("EasingFunction", typeof (EasingFunctionBase), typeof (EasingView), null);
+
+		public EasingView ()
+		{
+			InitializeComponent();
+
+			Loaded += delegate {
+
+				Polyline polyline = new Polyline ();
+
+				for (int px = 0; px < rect.ActualWidth; px++) {
+					double x = (double) px / rect.ActualWidth;
+					double y = EasingFunction.Ease (x);
+
+					polyline.Points.Add (new Point (px,rect.ActualHeight - y * rect.ActualHeight));
+				}
+
+				polyline.Stroke = new SolidColorBrush (Colors.Blue);
+				polyline.Fill = null;
+
+				polyline.Margin = new Thickness(5);
+
+				LayoutRoot.Children.Add (polyline);
+			};
+		}
+
+		public EasingFunctionBase EasingFunction {
+			get { return (EasingFunctionBase)GetValue(EasingFunctionProperty); }
+			set { SetValue (EasingFunctionProperty, value); }
+		}
+	}
+}
