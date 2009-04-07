a06241df9c66a42d2c696df0e266554f0d2fa732
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-04-07 13:30:42 +0000

    2009-04-07  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* input.h: Add license.
    
    svn path=/trunk/moon/; revision=131228

--- test/harness/shocker/input.h
+++ test/harness/shocker/input.h
@@ -1,4 +1,33 @@
-
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * Author:
+ *   Jackson Harper (jackson@ximian.com)
+ *
+ * Copyright 2007-2009 Novell, Inc. (http://www.novell.com)
+ *
+ *
+ * input.h: Simulate user input.
+ *
+ */
 
 #ifndef __INPUT_H__
 #define __INPUT_H__
@@ -9,7 +38,6 @@
 #include <X11/X.h>
 #include <X11/extensions/XTest.h>
 #undef Visual
-
 
 #include "netscape.h"
 
