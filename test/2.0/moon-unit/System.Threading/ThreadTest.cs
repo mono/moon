a70bb417a96a9fabe2db5e5ca6b47fe900abcfb3
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-04-17 12:49:47 +0000

    2009-04-17  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* test/2.0/moon-unit/System.Threading:
    	* test/2.0/moon-unit/System.Threading/ThreadTest.cs: Add a
    	  test case for not crashing when a thread doesn't handle an
    	  exception.
    
    	* test/2.0/moon-unit/System.Threading/ChangeLog: Added.
    
    svn path=/trunk/moon/; revision=132003

---
+++ test/2.0/moon-unit/System.Threading/ThreadTest.cs
@@ -0,0 +1,75 @@
+//Permission is hereby granted, free of charge, to any person obtaining
+//a copy of this software and associated documentation files (the
+//"Software"), to deal in the Software without restriction, including
+//without limitation the rights to use, copy, modify, merge, publish,
+//distribute, sublicense, and/or sell copies of the Software, and to
+//permit persons to whom the Software is furnished to do so, subject to
+//the following conditions:
+//
+//The above copyright notice and this permission notice shall be
+//included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//Copyright (c) 2008 Novell, Inc.
+//
+//Authors:
+//	Rolf Bjarne Kvinge  (RKvinge@novell.com)
+//
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mono.Moonlight.UnitTesting;
+using Microsoft.Silverlight.Testing;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace MoonTest.System.Threading
+{
+	[TestClass]
+	public class ThreadTest : SilverlightTest
+	{
+		static EventHandler<ApplicationUnhandledExceptionEventArgs> ex_handler;
+
+		[TestMethod]
+		[Ignore] // We don't pass this quite yet.
+		[Asynchronous]
+		public void ThreadExceptionTest ()
+		{
+			bool unhandled_exception_raised = false;
+			Dispatcher dispatcher;
+
+			dispatcher = Mono.Moonlight.UnitTesting.App.Instance.RootVisual.Dispatcher;
+
+			Thread t = new Thread (delegate () { throw new Exception ("This shouldn't crash the process"); });
+
+			ex_handler = delegate (object sender, ApplicationUnhandledExceptionEventArgs e) {
+				unhandled_exception_raised = true;
+				e.Handled = true;
+				dispatcher.BeginInvoke (delegate () 
+				{
+					Mono.Moonlight.UnitTesting.App.Instance.CustomUnhandledExceptionHandler -= ex_handler;
+					ex_handler = null;
+				});
+			};
+			Mono.Moonlight.UnitTesting.App.Instance.CustomUnhandledExceptionHandler += ex_handler;
+			
+			EnqueueConditional (() => unhandled_exception_raised);
+			EnqueueConditional (() => ex_handler == null); // wait until the cleanup has happened too
+			EnqueueTestComplete ();
+
+			t.Start ();
+		}
+	}
+}
