43a7e21af80e8cc95243380969c39a47f2899bc3
Geoff Norton <grompf@sublimeintervention.com>
2009-04-21 22:30:13 +0000

    Revert "Add tests for GetContainerForItem."
    This reverts commit 8554093a4242c8313ddb13f1f16d6250b16c5c28.
    
    svn path=/trunk/moon/; revision=132332

--- test/2.0/moon-unit/System.Windows.Controls/ListBoxTest.cs
+++ test/2.0/moon-unit/System.Windows.Controls/ListBoxTest.cs
@@ -38,7 +38,6 @@ using System.Windows.Shapes;
 using System.Windows.Media;
 using Microsoft.Silverlight.Testing;
 using System.Windows.Controls.Primitives;
-using System.Windows.Markup;
 
 namespace MoonTest.System.Windows.Controls {
 	[TestClass]
@@ -320,20 +319,6 @@ namespace MoonTest.System.Windows.Controls {
 			Assert.IsNull (((ListBoxItem)container).Content, "content is null");
 			Assert.IsFalse (((ListBoxItem)container).IsSelected, "!isselected");
 			Assert.AreEqual (0, VisualTreeHelper.GetChildrenCount (container), "no children"); // its template hasn't been applied
-		}
-
-		[TestMethod]
-		public void GetContainerForItem3 ()
-		{
-			ListBoxPoker list = new ListBoxPoker ();
-			list.ApplyTemplate ();
-			list.ItemContainerStyle = new Style (typeof (ListBoxItem));
-			DependencyObject oItem = list.Call_GetContainerForItemOverride ();
-			Assert.IsNotNull (oItem, "#1");
-			Assert.IsInstanceOfType (oItem, typeof (ListBoxItem), "#2");
-			ListBoxItem item = (ListBoxItem) oItem;
-			Assert.IsNull (item.Content, "#3");
-			Assert.AreSame (list.ItemContainerStyle, item.Style, "#4");
 		}
 
 		[TestMethod]
