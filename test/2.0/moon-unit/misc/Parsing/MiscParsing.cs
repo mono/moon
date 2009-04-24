bb4da756f375080a16919a9191eab4673b96b945
Jackson Harper <jackson@novell.com>
2009-04-24 15:48:53 +0000

    2009-04-24  Jackson Harper  <jackson@ximian.com>
            * misc/Parsing/MiscParsing.cs:
            * misc/Parsing/MiscParsingAnimation.xaml: New test for managed
            namespaces in property paths.
    
    
    
    svn path=/trunk/moon/; revision=132599

--- test/2.0/moon-unit/misc/Parsing/MiscParsing.cs
+++ test/2.0/moon-unit/misc/Parsing/MiscParsing.cs
@@ -5,6 +5,7 @@ using System.Resources;
 using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 using Mono.Moonlight.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,11 +14,19 @@ using Microsoft.Silverlight.Testing;
 
 namespace MoonTest.Misc.Parsing
 {
-	internal class MiscParsingTestPage : Canvas {
+	public class MiscParsingTestPage : Canvas {
 
-		public MiscParsingTestPage ()
+		public MiscParsingTestPage (string url)
 		{
-			Application.LoadComponent (this, new Uri ("/moon-unit;component/misc/Parsing/MiscParsingTestPage.xaml", UriKind.Relative));
+			Application.LoadComponent (this, new Uri (url, UriKind.Relative));
+		}
+	}
+
+	public class MiscParsingTestAnimation : Canvas {
+
+		public MiscParsingTestAnimation (string url)
+		{
+			Application.LoadComponent (this, new Uri (url, UriKind.Relative));
 		}
 	}
 
@@ -29,13 +38,28 @@ namespace MoonTest.Misc.Parsing
 		}
 	}
 
+	public class HalfHeightControl : UserControl {
+
+		public static readonly DependencyProperty HalfHeightProperty = DependencyProperty.RegisterAttached ("HalfHeight", typeof (double), typeof (HalfHeightControl), new PropertyMetadata (OnHalfHeightChanged));
+
+		public double HalfHeight {
+			get { return (double) GetValue (HalfHeightProperty); }
+			set { SetValue (HalfHeightProperty, value); }
+		}
+
+		public static void OnHalfHeightChanged (DependencyObject sender, DependencyPropertyChangedEventArgs args)
+		{
+			(sender as FrameworkElement).Height = (double) args.NewValue * 2;
+		}
+	}
+
 	[TestClass]
 	public class MiscParsingTest : SilverlightTest
 	{
 		[TestMethod]
 		public void FindNameOfManagedControl ()
 		{
-			MiscParsingTestPage page = new MiscParsingTestPage ();
+			MiscParsingTestPage page = new MiscParsingTestPage ("/moon-unit;component/misc/Parsing/MiscParsingTestPage.xaml");
 			
 
 			Assert.IsNotNull (page, "1");
@@ -62,6 +86,42 @@ namespace MoonTest.Misc.Parsing
 			Canvas sub_canvas = canvas.FindName ("the_canvas") as Canvas;
 			Assert.IsNotNull (sub_canvas, "8");
 			Assert.AreEqual (0, sub_canvas.Children.Count, "9");
+		}
+
+		[TestMethod]
+		[Asynchronous]
+		[MoonlightBug]
+		public void ResolveManagedPropertyPath1 ()
+		{
+			MiscParsingTestAnimation page = new MiscParsingTestAnimation ("/moon-unit;component/misc/Parsing/MiscParsingAnimation.xaml");
+
+			Storyboard s = (Storyboard) page.FindName ("the_storyboard");
+			HalfHeightControl control = (HalfHeightControl) page.FindName ("the_control");
+			DoubleAnimation the_animation = (DoubleAnimation) page.FindName ("the_animation");
+			
+			s.Begin ();
+			Enqueue (() => Assert.AreEqual (25, control.HalfHeight, "#1"));
+
+			// Make sure the path isn't magically expanded by the parser
+			Enqueue (() => Assert.AreEqual ("(moon:ManagedUserControl.HalfHeight)", Storyboard.GetTargetProperty (the_animation).Path, "#2"));
+
+			// Try setting the path programmatically (reset it first)
+			Enqueue (() => s.Stop ());
+			Enqueue (() => Storyboard.SetTargetProperty (the_animation, new PropertyPath ("Height")));
+			Enqueue (() => s.Begin ());
+			Enqueue (() => Assert.AreEqual (25, control.Height, "#3"));
+
+			Enqueue (() => s.Stop ());
+			Enqueue (() => Storyboard.SetTargetProperty (the_animation, new PropertyPath ("(moon:ManagedUserControl.HalfHeight)")));
+			Enqueue (() => s.Begin ());
+			Enqueue (() => Assert.AreEqual (25, control.HalfHeight, "#4"));
+
+			// OK, this shouldn't work and makes little sense, but it does (notice the xmlns)
+			Enqueue (() => s.Stop ());
+			Enqueue (() => Storyboard.SetTargetProperty (the_animation, new PropertyPath ("(monkey:ManagedUserControl.HalfHeigh)")));
+			Enqueue (() => Assert.Throws<InvalidOperationException> (() => s.Begin ()));
+
+			EnqueueTestComplete ();
 		}
 	}
 }
