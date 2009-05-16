de860ec29db540665b5dad47726ade5e555c2b04
Alan McGovern <alan.mcgovern@gmail.com>
2009-05-16 18:12:01 +0000

    Whoops, rename the other instance of the security env var
    svn path=/trunk/moon/; revision=134280

--- src/security.c
+++ src/security.c
@@ -79,7 +79,7 @@ void
 security_enable_coreclr (const char *platform_dir)
 {
 	if (g_getenv ("MOON_DISABLE_SECURITY_PREVIEW_04") != NULL) {
-		g_warning ("CORECLR was DISABLED using MOON_DISABLE_SECURITY_PREVIEW_01 override");
+		g_warning ("CORECLR was DISABLED using MOON_DISABLE_SECURITY_PREVIEW_04 override");
 		g_warning ("this disables both code verification and metadata verification on code\n"
 			   "downloaded from untrusted sources, and therefore opens up your machine\n"
 			   "to a wide variety of attack vectors. Don't do this unless you know what\n"
