8231d34e900896eac896093562c56b4a5890ddf9
Jeffrey Stedfast <fejj@novell.com>
2009-05-28 18:40:41 +0000

    2009-05-28  Jeffrey Stedfast  <fejj@novell.com>
    	* plugin-utils.cpp: New source file which has utility functions
    	such as check_arg_list().
    
    	* plugin-class.h.in: Removed static impl of check_arg_list() -
    	this was causing compile warnings about unused functions and also
    	bloating up the binary ;-)
    
    
    svn path=/trunk/moon/; revision=134979

--- plugin/plugin-class.h
+++ plugin/plugin-class.h
@@ -15,8 +15,8 @@
  *
  */
 
-#ifndef PLUGIN_CLASS
-#define PLUGIN_CLASS
+#ifndef __PLUGIN_CLASS_H__
+#define __PLUGIN_CLASS_H__
 
 #include "moonlight.h"
 #include "error.h"
@@ -24,130 +24,6 @@
 
 // debug javascript
 #define DEBUG_JAVASCRIPT 1
-
-
-bool npobject_is_dependency_object (NPObject *obj);
-
-enum MethodArgType {
-	MethodArgTypeNone   = (0),
-	MethodArgTypeVoid   = (1 << NPVariantType_Void),
-	MethodArgTypeNull   = (1 << NPVariantType_Null),
-	MethodArgTypeBool   = (1 << NPVariantType_Bool),
-	MethodArgTypeInt32  = (1 << NPVariantType_Int32),
-	MethodArgTypeDouble = (1 << NPVariantType_Double),
-	MethodArgTypeString = (1 << NPVariantType_String),
-	MethodArgTypeObject = (1 << NPVariantType_Object),
-	MethodArgTypeAny    = (0xff)
-};
-
-static MethodArgType
-decode_arg_ctype (char c)
-{
-	switch (c) {
-	case 'v': return MethodArgTypeVoid;
-	case 'n': return MethodArgTypeNull;
-	case 'b': return MethodArgTypeBool;
-	case 'i': return MethodArgTypeInt32;
-	case 'd': return MethodArgTypeDouble;
-	case 's': return MethodArgTypeString;
-	case 'o': return MethodArgTypeObject;
-	case '*': return MethodArgTypeAny;
-	default:
-		return MethodArgTypeNone;
-	}
-}
-
-static MethodArgType
-decode_arg_type (const char **in)
-{
-	MethodArgType t, type = MethodArgTypeNone;
-	register const char *inptr = *in;
-
-	if (*inptr == '(') {
-		inptr++;
-		while (*inptr && *inptr != ')') {
-			t = decode_arg_ctype (*inptr);
-			type = (MethodArgType) ((int) type | (int) t);
-			inptr++;
-		}
-	} else {
-		type = decode_arg_ctype (*inptr);
-	}
-
-	inptr++;
-	*in = inptr;
-
-	return type;
-}
-
-/**
- * check_arg_list:
- * @arglist: a string representing an arg-list token (see grammar below)
- * @args: NPVariant argument count
- * @argv: NPVariant argument vector
- *
- * Checks that the NPVariant arguments satisfy the argument count and
- * types expected (provided via @typestr).
- *
- * The @typestr argument should follow the following syntax:
- *
- * simple-arg-type ::= "v" / "n" / "b" / "i" / "d" / "s" / "o" / "*"
- *                     ; each char represents one of the following
- *                     ; NPVariant types: Void, Null, Bool, Int32,
- *                     ; Double, String, Object and wildcard
- *
- * arg-type        ::= simple-arg-type / "(" 1*(simple-arg-type) ")"
- *
- * optional-args   ::= "[" *(arg-type) "]"
- *
- * arg-list        ::= *(arg-type) (optional-args)
- *
- *
- * Returns: %true if @argv matches the arg-list criteria specified in
- * @arglist or %false otherwise.
- **/
-static bool
-check_arg_list (const char *arglist, guint32 argc, const NPVariant *argv)
-{
-	const char *inptr = arglist;
-	MethodArgType mask;
-	guint32 i = 0;
-
-	// check all of the required arguments
-	while (*inptr && *inptr != '[' && i < argc) {
-		mask = decode_arg_type (&inptr);
-		if (!(mask & (1 << argv[i].type))) {
-			// argv[i] does not match any of the expected types
-			return false;
-		}
-
-		i++;
-	}
-
-	if (*inptr && *inptr != '[' && i < argc) {
-		// we were not provided enough arguments
-		return false;
-	}
-
-	// now check all of the optional arguments
-	inptr++;
-	while (*inptr && *inptr != ']' && i < argc) {
-		mask = decode_arg_type (&inptr);
-		if (!(mask & (1 << argv[i].type))) {
-			// argv[i] does not match any of the expected types
-			return false;
-		}
-
-		i++;
-	}
-
-	if (i < argc) {
-		// we were provided too many arguments
-		return false;
-	}
-
-	return true;
-}
 
 #define DEPENDENCY_OBJECT_FROM_VARIANT(obj) (((MoonlightDependencyObjectObject*) NPVARIANT_TO_OBJECT (obj))->GetDependencyObject ())
 
@@ -165,8 +41,6 @@ check_arg_list (const char *arglist, guint32 argc, const NPVariant *argv)
 		NPN_SetException (obj, message);	\
 		g_free (message);	\
 	} while (0);	\
-
-
 
 
 enum PluginPropertyId {
@@ -315,6 +189,10 @@ struct MoonNameIdMapping {
 
 
 G_BEGIN_DECLS
+bool npobject_is_dependency_object (NPObject *obj);
+
+bool check_arg_list (const char *arglist, guint32 argc, const NPVariant *argv);
+
 void plugin_init_classes (void);
 void plugin_destroy_classes (void);
 
@@ -1166,4 +1044,4 @@ void browser_do_alert (PluginInstance *plugin, char *msg);
 
 G_END_DECLS
 
-#endif /* PLUGIN_CLASS */
+#endif /* __PLUGIN_CLASS_H__ */
