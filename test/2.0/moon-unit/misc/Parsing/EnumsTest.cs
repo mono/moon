6daa1840f9ad338e354ca02de0a39980cd05c9cd
Jackson Harper <jackson@novell.com>
2009-06-05 20:37:02 +0000

    * misc/Parsing/EnumsTest.cs: Make sure we handle flags and         managed enums properly.
    svn path=/trunk/moon/; revision=135555

--- test/2.0/moon-unit/misc/Parsing/EnumsTest.cs
+++ test/2.0/moon-unit/misc/Parsing/EnumsTest.cs
@@ -13,6 +13,11 @@ using Microsoft.Silverlight.Testing;
 
 namespace MoonTest.Misc
 {
+	[Flags]
+	public enum Alpha {
+		A, B, C, D,E
+	}
+
 	[TestClass]
 	public class EnumsTest : SilverlightTest
 	{
@@ -45,6 +50,36 @@ namespace MoonTest.Misc
 			Assert.IsNotNull (c.Resources ["visibility"], "1");
 			Assert.AreEqual (typeof (Visibility), c.Resources ["visibility"].GetType (), "2");
 			Assert.AreEqual (Visibility.Collapsed, c.Resources ["visibility"], "3");
+		}
+
+		[TestMethod]
+		public void ParseCustomEnum ()
+		{
+			var c = (Canvas) XamlReader.Load (@"<Canvas xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+							   	    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+								    xmlns:c=""clr-namespace:MoonTest.Misc;assembly=moon-unit"">
+								      <Canvas.Resources>
+									<c:Alpha x:Key=""alpha"">E</c:Alpha>
+					                              </Canvas.Resources>
+							    </Canvas>");
+
+			Assert.IsNotNull (c.Resources ["alpha"], "1");
+			Assert.AreEqual (Alpha.E, c.Resources ["alpha"]);
+		}
+		
+		[TestMethod]
+		public void ParseWpfStyleFlags ()
+		{
+					var c = (Canvas) XamlReader.Load (@"<Canvas xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+							   	    xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+								    xmlns:c=""clr-namespace:MoonTest.Misc;assembly=moon-unit"">
+								      <Canvas.Resources>
+									<c:Alpha x:Key=""alpha"">A,B,C</c:Alpha>
+					                              </Canvas.Resources>
+							    </Canvas>");
+
+			Assert.IsNotNull (c.Resources ["alpha"], "1");
+			Assert.AreEqual (Alpha.A | Alpha.B | Alpha.C, c.Resources ["alpha"]);
 		}
 	}
 }
