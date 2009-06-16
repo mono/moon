6cc3aa55014a9940bee9ffbf21b88b4a5fc9cd33
Andreia Gaita <avidigal@novell.com>
2009-06-16 21:38:01 +0000

    * DependencyObject.cs: a little code reuse and slight tweaking of the dropref explanation. Also, drop the native ref on new objects, too.
    svn path=/trunk/moon/; revision=136255

--- class/System.Windows/System.Windows/DependencyObject.cs
+++ class/System.Windows/System.Windows/DependencyObject.cs
@@ -76,21 +76,18 @@ namespace System.Windows {
 			moonlight_thread = Thread.CurrentThread;
 		}
 
-		protected DependencyObject ()
+		// This created objects with a managed lifetime, so the native ref will be dropped
+		// as soon as the object is created and handed to ToggleRef
+		protected DependencyObject () : this (NativeMethods.dependency_object_new (), true)
 		{
-			native = NativeMethods.dependency_object_new ();
-			NativeMethods.event_object_set_object_type (native, GetKind ());
 		}
 
 		internal DependencyObject (IntPtr raw, bool dropref)
 		{
 			native = raw;
 			NativeMethods.event_object_set_object_type (native, GetKind ());
-			// NOTE: This is horrible.  The managed case:
-			//	DependencyObject do = new DependencyObject ();
-			// Creates 2 refs, 1 in the dependency_object_new call, and 1 in the toggleref
-			// As such we need to drop the one from the new call, since we are self-managed
-			// by the toggleref here.
+			// Objects created on the managed side have a normal managed lifetime,
+			// so drop the native ref hold
 			if (dropref)
 				NativeMethods.event_object_unref (native);
 		}
