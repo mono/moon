9bcd31ee6a2a86790a3ecdb1ca7647b9837e35c8
Sebastien Pouliot <sebastien@ximian.com>
2009-06-23 19:40:38 +0000

    2009-06-23  Sebastien Pouliot  <sebastien@ximian.com>
    	* detect-sc.cs: Mark non-visible, non-virtual or abstract icalls
    	as [SecurityCritical]. Also fix a bug when comparing methods.
    	* detect-ssc.cs: Mark visible or virtual (special rules applies)
    	icalls as [SecuritySafeCritical].
    
    
    svn path=/trunk/moon/; revision=136738

--- class/tuning/SecurityAttributes/detect-sc.cs
+++ class/tuning/SecurityAttributes/detect-sc.cs
@@ -77,15 +77,15 @@ class Program {
 	{
 		if (m1.Name != m2.Name)
 			return false;
-		if (m1.HasParameters && m2.HasParameters) {
-			if (m1.Parameters.Count != m2.Parameters.Count)
+
+		if (m1.Parameters.Count != m2.Parameters.Count)
+			return false;
+
+		for (int i = 0; i < m1.Parameters.Count; i++) {
+			ParameterDefinition p1 = m1.Parameters [i];
+			ParameterDefinition p2 = m2.Parameters [i];
+			if (p1.ParameterType.FullName != p2.ParameterType.FullName)
 				return false;
-			for (int i = 0; i < m1.Parameters.Count; i++) {
-				ParameterDefinition p1 = m1.Parameters [i];
-				ParameterDefinition p2 = m2.Parameters [i];
-				if (p1.ParameterType.FullName != p2.ParameterType.FullName)
-					return false;
-			}
 		}
 		return (m1.ReturnType.ReturnType.FullName == m2.ReturnType.ReturnType.FullName);
 	}
@@ -174,6 +174,13 @@ class Program {
 						}
 					}
 				}
+			}
+		} else {
+			// note: we don't want to break the override rules above (resulting in TypeLoadException)
+			// an icall that is NOT part of the visible API is considered as critical (like a p/invoke)
+			if (method.IsInternalCall && !method.IsVisible ()) {
+				sc = true;
+				comment = "internal call";
 			}
 		}
 
