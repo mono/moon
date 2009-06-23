e73f9ea8e5fa7f754bdee15cb33b40af1fde2e3f
Alan McGovern <alan.mcgovern@gmail.com>
2009-06-23 15:10:48 +0000

    2009-06-23  Alan McGovern  <amcgovern@novell.com>
    	* class/System.Windows/System.Windows.Data/Binding.cs: Don't
    	  null check value before using it, we should throw a nullref
    	  if it's null.
    
    svn path=/trunk/moon/; revision=136710

--- class/System.Windows/System.Windows.Data/Binding.cs
+++ class/System.Windows/System.Windows.Data/Binding.cs
@@ -91,10 +91,9 @@ namespace System.Windows.Data {
 				return path;
 			}
 			set {
-				if (value != null) {
-					if (value.NativeDP != IntPtr.Zero)
-						throw new ArgumentException ("PropertyPaths which are instantiated with a DependencyProperty are not supported");
-				}
+				if (value.NativeDP != IntPtr.Zero)
+					throw new ArgumentException ("PropertyPaths which are instantiated with a DependencyProperty are not supported");
+
 				CheckSealed ();
 				path = value;
 			}
