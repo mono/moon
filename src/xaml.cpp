9de506b9766f25f8bd391a1bf89a0342910b689d
Larry Ewing <lewing@novell.com>
2009-06-28 19:18:17 +0000

    2009-06-28  Larry Ewing  <lewing@novell.com>
    	* xaml.cpp (value_from_str_with_parser): allow NAN to be NAN too.
    
    svn path=/trunk/moon/; revision=137050

--- src/xaml.cpp
+++ src/xaml.cpp
@@ -3209,7 +3209,7 @@ value_from_str_with_parser (XamlParserInfo *p, Type::Kind type, const char *prop
 		if (is_nan || errno || endptr == s || *endptr) {
 			if (prop_name
 			    && (!strcmp (prop_name, "Width") || !strcmp (prop_name, "Height"))
-			    && !g_ascii_strcasecmp (s, "Auto"))
+			    && (!g_ascii_strcasecmp (s, "Auto") || is_nan))
 				d = NAN;
 			else {
 				g_free (s);
