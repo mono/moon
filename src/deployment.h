d96999dda24da96655f98ab1ecccc159aded76dd
Rolf Bjarne Kvinge <RKvinge@novell.com>
2009-07-16 10:37:08 +0000

    In plugin: 2009-07-16  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* plugin-entry.cpp: Push/Pop the current deployment on every
    	transition to the browser. Makes MS DRT #2003 not spew a lot
    	of deployment mismatch warnings.
    
    In src:
    2009-07-16  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* deployment.h: DeploymentStack: Added to ease push/pop current
    	deployment when calling into a browser.
    
    svn path=/trunk/moon/; revision=138020

--- src/deployment.h
+++ src/deployment.h
@@ -161,4 +161,27 @@ private:
 	static MonoDomain *root_domain;
 };
 
+/*
+ * DeploymentStack: 
+ *  for all calls into javascript we need to push/pop the current deployment.
+ *  this class is (ab)uses C++ for this, just put a "DeploymentStack deployment_push_pop;" 
+ *  in every method which needs to push/pop the current deployment. the compiler will
+ *  all the ctor in the beginning of the method, and the dtor in the end.
+ */
+class DeploymentStack {
+public:
+	DeploymentStack ()
+	{
+		deployment = Deployment::GetCurrent ();
+	}
+	
+	~DeploymentStack ()
+	{
+		Deployment::SetCurrent (deployment);
+	}
+
+private:
+	Deployment *deployment;
+};
+
 #endif /* __DEPLOYMENT_H__ */
