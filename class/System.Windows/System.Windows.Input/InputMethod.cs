79a9d80bf1fd628dd18b3538a78226de536aca88
Sebastien Pouliot <sebastien@ximian.com>
2009-10-01 14:18:09 +0000

    Add some missing validations for InputMethod
    svn path=/trunk/moon/; revision=143155

--- class/System.Windows/System.Windows.Input/InputMethod.cs
+++ class/System.Windows/System.Windows.Input/InputMethod.cs
@@ -4,25 +4,35 @@
  * Contact:
  *   Moonlight List (moonlight-list@lists.ximian.com)
  *
- * Copyright 2008 Novell, Inc. (http://www.novell.com)
+ * Copyright 2008-2009 Novell, Inc. (http://www.novell.com)
  *
  * See the LICENSE file included with the distribution for details.
  * 
  */
 
-using System;
+using System.Windows.Controls;
 
 namespace System.Windows.Input
 {	
 	public sealed partial class InputMethod : DependencyObject
 	{
+		static void CheckTarget (DependencyObject target)
+		{
+			if (target == null)
+				throw new ArgumentNullException ("target");
+			if (!(target is TextBox))
+				throw new ArgumentException ("target is not a TextBox", "target");
+		}
+
 		public static bool GetIsInputMethodEnabled (DependencyObject target)
 		{
+			CheckTarget (target);
 			return (bool) target.GetValue (InputMethod.IsInputMethodEnabledProperty);
 		}
 
 		public static void SetIsInputMethodEnabled (DependencyObject target, bool value)
 		{
+			CheckTarget (target);
 			target.SetValue (InputMethod.IsInputMethodEnabledProperty, value);
 		}
 	}
--- test/2.0/moon-unit/System.Windows.Input/InputMethodTest.cs
+++ test/2.0/moon-unit/System.Windows.Input/InputMethodTest.cs
@@ -1,13 +1,7 @@
 using System;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mono.Moonlight.UnitTesting;
 
@@ -17,7 +11,6 @@ namespace MoonTest.System.Windows.Input {
 	public class InputMethodTest {
 
 		[TestMethod]
-		[MoonlightBug]
 		public void SetIsInputMethodEnabled ()
 		{
 			Assert.Throws<ArgumentException> (() => InputMethod.SetIsInputMethodEnabled (new Button (), true), "#1");
@@ -29,7 +22,106 @@ namespace MoonTest.System.Windows.Input {
 
 			InputMethod.SetIsInputMethodEnabled (box, false);
 			Assert.IsFalse (InputMethod.GetIsInputMethodEnabled (box), "#5");
+		}
 
+		class ConcreteDependencyObject : DependencyObject {
+		}
+
+		class MyTextBox : TextBox {
+		}
+
+		[TestMethod]
+		public void GetIsInputMethodEnabled_Validations ()
+		{
+			Assert.Throws<ArgumentNullException> (delegate {
+				InputMethod.GetIsInputMethodEnabled (null);
+			}, "null");
+
+			ConcreteDependencyObject cdo = new ConcreteDependencyObject ();
+			Assert.Throws<ArgumentException> (delegate {
+				InputMethod.GetIsInputMethodEnabled (cdo);
+			}, "not a TextBox");
+		}
+
+		[TestMethod]
+		public void GetIsInputMethodEnabled_TextBox ()
+		{
+			TextBox tb = new TextBox ();
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (tb), "TextBox");
+
+			MyTextBox mtb = new MyTextBox ();
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (mtb), "MyTextBox");
+		}
+
+		[TestMethod]
+		[MoonlightBug]
+		public void GetValue ()
+		{
+			ConcreteDependencyObject cdo = new ConcreteDependencyObject ();
+			Assert.Throws<Exception> (delegate {
+				cdo.GetValue (InputMethod.IsInputMethodEnabledProperty);
+			}, "GetValue");
+
+			TextBox tb = new TextBox ();
+			Assert.IsTrue ((bool) tb.GetValue (InputMethod.IsInputMethodEnabledProperty), "TextBox");
+
+			MyTextBox mtb = new MyTextBox ();
+			Assert.IsTrue ((bool) mtb.GetValue (InputMethod.IsInputMethodEnabledProperty), "MyTextBox");
+		}
+
+		[TestMethod]
+		public void SetIsInputMethodEnabled_Validations ()
+		{
+			Assert.Throws<ArgumentNullException> (delegate {
+				InputMethod.SetIsInputMethodEnabled (null, true);
+			}, "null,true");
+			Assert.Throws<ArgumentNullException> (delegate {
+				InputMethod.SetIsInputMethodEnabled (null, false);
+			}, "null,false");
+
+			ConcreteDependencyObject cdo = new ConcreteDependencyObject ();
+			Assert.Throws<ArgumentException> (delegate {
+				InputMethod.SetIsInputMethodEnabled (cdo, true);
+			}, "not a TextBox, true");
+			Assert.Throws<ArgumentException> (delegate {
+				InputMethod.SetIsInputMethodEnabled (cdo, false);
+			}, "not a TextBox, false");
+		}
+
+		[TestMethod]
+		public void SetIsInputMethodEnabled_TextBox ()
+		{
+			TextBox tb = new TextBox ();
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (tb), "TextBox-1");
+			InputMethod.SetIsInputMethodEnabled (tb, false);
+			Assert.IsFalse (InputMethod.GetIsInputMethodEnabled (tb), "TextBox-2");
+			InputMethod.SetIsInputMethodEnabled (tb, true);
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (tb), "TextBox-3");
+
+			MyTextBox mtb = new MyTextBox ();
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (mtb), "MyTextBox-1");
+			InputMethod.SetIsInputMethodEnabled (mtb, false);
+			Assert.IsFalse (InputMethod.GetIsInputMethodEnabled (mtb), "MyTextBox-2");
+			InputMethod.SetIsInputMethodEnabled (mtb, true);
+			Assert.IsTrue (InputMethod.GetIsInputMethodEnabled (mtb), "MyTextBox-3");
+		}
+
+		[TestMethod]
+		[MoonlightBug]
+		public void SetValue ()
+		{
+			ConcreteDependencyObject cdo = new ConcreteDependencyObject ();
+			Assert.Throws<Exception> (delegate {
+				cdo.SetValue (InputMethod.IsInputMethodEnabledProperty, true);
+			}, "SetValue");
+
+			TextBox tb = new TextBox ();
+			tb.SetValue (InputMethod.IsInputMethodEnabledProperty, false);
+			Assert.IsFalse (InputMethod.GetIsInputMethodEnabled (tb), "TextBox");
+
+			MyTextBox mtb = new MyTextBox ();
+			mtb.SetValue (InputMethod.IsInputMethodEnabledProperty, false);
+			Assert.IsFalse (InputMethod.GetIsInputMethodEnabled (mtb), "MyTextBox");
 		}
 	}
 }
