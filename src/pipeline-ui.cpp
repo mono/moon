76a454bfaee412e80e3cacd8584d1568a81f3ee7
Jeffrey Stedfast <fejj@novell.com>
2009-11-13 22:18:01 +0000

    2009-11-13  Jeffrey Stedfast  <fejj@novell.com>
    	* pipeline-ui.cpp (ToggleEula): When toggling the EULA on, make
    	the dialog resizable. Likewise, when hiding the EULA, disable
    	resize.
    	(Show): Make the EULA scrollview expand/fill available space so
    	that when the dialog gets resized, it expands as well.
    
    	r: kangaroo
    
    
    svn path=/branches/moon/moon-2-0/moon/; revision=146184

--- src/pipeline-ui.cpp
+++ src/pipeline-ui.cpp
@@ -308,9 +308,11 @@ CodecDownloader::AcceptClicked ()
 		env_url = getenv ("MOONLIGHT_CODEC_URL");
 		if (env_url != NULL)
 			dl->Open ("GET", env_url, NoPolicy);
-		else
-			dl->Open ("GET", CODEC_URL, NoPolicy);
-		
+		else {
+			char *codec_url = g_strdup_printf("%s", CODEC_URL);
+			dl->Open ("GET", codec_url, NoPolicy);
+			g_free (codec_url);
+		}
 		dl->Send ();
 
 		state = 3;
