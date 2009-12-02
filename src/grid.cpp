ea8b87e8156472d74805ba1d769b72990589fc73
Chris Toshok <toshok@novell.com>
2009-12-02 06:19:35 +0000

    backport r146286 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=147288

--- src/grid.cpp
+++ src/grid.cpp
@@ -321,7 +321,6 @@ Grid::MeasureOverride (Size availableSize)
 	for (int r = 0; r < row_count; r ++)
 		grid_size.height += row_matrix [r][r].size;
 
-	grid_size = grid_size.Max (GetWidth (), GetHeight ());
 
 	// This is where we do the final expansion of star rows. Right now the value
 	// of 'size' for star segments in row_matrix and col_matrix contains the
@@ -340,7 +339,7 @@ Grid::MeasureOverride (Size availableSize)
 		rows->unref ();
 	}
 	// now choose whichever is smaller, our chosen size or the availableSize.
-	return totalSize.Min (grid_size);
+	return grid_size;
 }
 
 void
