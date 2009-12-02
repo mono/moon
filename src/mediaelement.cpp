659edddda49381f60242abe8e2387d1c9b8d9b63
Chris Toshok <toshok@novell.com>
2009-12-02 06:31:20 +0000

    backport r146940 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=147368

--- src/mediaelement.cpp
+++ src/mediaelement.cpp
@@ -996,13 +996,6 @@ MediaElement::SeekingHandler (PlaylistRoot *playlist, EventArgs *args)
 	VERIFY_MAIN_THREAD;
 	
 	SetMarkerTimeout (false);
-	
-	if (state == MediaStatePlaying || state == MediaStatePaused) {
-		if (state == MediaStatePlaying)
-			flags |= PlayRequested;
-			
-		SetState (MediaStateBuffering);
-	}
 
 	if (GetBufferingProgress () != 0.0) {
 		SetBufferingProgress (0.0);
@@ -1015,9 +1008,6 @@ MediaElement::SeekCompletedHandler (PlaylistRoot *playlist, EventArgs *args)
 {
 	LOG_MEDIAELEMENT ("MediaElement::SeekCompletedHandler () state: %s\n", GetStateName (state));
 	VERIFY_MAIN_THREAD;
-	
-	if (state == MediaStatePlaying)
-		playlist->PlayAsync ();
 	
 	seek_to_position = -1;
 	SetMarkerTimeout (true);
@@ -1144,7 +1134,7 @@ MediaElement::BufferingProgressChangedHandler (PlaylistRoot *playlist, EventArgs
 	g_return_if_fail (pea != NULL);
 
 	if (GetBufferingProgress () < pea->progress) {
-		if (state == MediaStatePlaying || state == MediaStatePaused) {
+		if (state != MediaStateBuffering) {
 			if (state == MediaStatePlaying)
 				flags |= PlayRequested;
 			/* this is wrong when the user calls Play while we're still buffering because we'd jump back to the buffering state later (but we'd continue playing) */
@@ -1155,9 +1145,14 @@ MediaElement::BufferingProgressChangedHandler (PlaylistRoot *playlist, EventArgs
 		Emit (BufferingProgressChangedEvent);
 	}
 	
-	if (pea->progress >= 1.0 && GetState () == MediaStateBuffering) {
-		LOG_MEDIAELEMENT ("MediaElement::BufferingProgressChangedHandler (): buffer full, playing...\n");
-		PlayOrStop ();
+	if (pea->progress >= 1.0) {
+		if (GetState () == MediaStateBuffering) {
+			LOG_MEDIAELEMENT ("MediaElement::BufferingProgressChangedHandler (): buffer full, playing...\n");
+			PlayOrStop ();
+		} else if (flags & PlayRequested) {
+			LOG_MEDIAELEMENT ("MediaElement::BufferingProgressChangedHandler (): buffer full, state: %s PlayRequested: 1\n", GetStateName (state));
+			Play ();
+		}
 	}
 }
 
@@ -1420,7 +1415,10 @@ MediaElement::Seek (TimeSpan to, bool force)
 		seek_to_position = to;
 		seeked_to_position = to;
 		paused_position = to;
-		
+
+		if (state == MediaStatePlaying)
+			flags |= PlayRequested;
+
 		mplayer->NotifySeek (TimeSpan_ToPts (to));
 		playlist->SeekAsync (to);
 		Emit (MediaInvalidatedEvent);
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -477,12 +477,14 @@ Media::SeekAsync (guint64 pts)
 }
 
 void
-Media::ReportSeekCompleted (guint64 pts)
+Media::ReportSeekCompleted (guint64 pts, bool pending_seeks)
 {
 	LOG_PIPELINE ("Media::ReportSeekCompleted (%" G_GUINT64_FORMAT "), id: %i\n", pts, GET_OBJ_ID (this));
 	
 	buffering_progress = 0;
 	ClearQueue ();
+	if (!pending_seeks)
+		stopped = false;
 	EmitSafe (SeekCompletedEvent);
 }
 
@@ -3119,16 +3121,13 @@ IMediaDemuxer::ReportSeekCompleted (guint64 pts)
 		
 		stream->ReportSeekCompleted ();
 	}
-	
-	media->ReportSeekCompleted (pts);
-	media->unref ();
-	
+		
 	mutex.Lock ();
 	seeks.RemoveAt (0);
 	seeking = !seeks.IsEmpty ();
 	mutex.Unlock ();
 	
-	media->ReportSeekCompleted (pts);
+	media->ReportSeekCompleted (pts, seeking);
 	media->unref ();
 	
 	if (!seeking) {
