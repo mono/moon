277f916f2b7dfc7585ae5ac4869e96bce977bc7a
Jeffrey Stedfast <fejj@novell.com>
2009-12-04 19:16:06 +0000

    simplified
    svn path=/trunk/moon/; revision=147691

--- class/System.Windows/System.Windows.Controls.Primitives/ItemsChangedEventArgs.cs
+++ class/System.Windows/System.Windows.Controls.Primitives/ItemsChangedEventArgs.cs
@@ -28,37 +28,26 @@
 using System.Collections.Specialized;
 
 namespace System.Windows.Controls.Primitives {
-
 	
 	public class ItemsChangedEventArgs : EventArgs {
-
-		private NotifyCollectionChangedAction action;
-
-		private int item_count;
-		private int item_ui_count;
-
-		private GeneratorPosition position;
-		private GeneratorPosition old_position;
-		
-
 		public NotifyCollectionChangedAction Action {
-			get { return action; }
+			get; internal set;
 		}
 
 		public int ItemCount {
-			get { return item_count; }
+			get; internal set;
 		}
 
 		public int ItemUICount {
-			get { return item_ui_count; }
+			get; internal set;
 		}
 
 		public GeneratorPosition OldPosition {
-			get { return old_position; }
+			get; internal set;
 		}
 
 		public GeneratorPosition Position {
-			get { return position; }
+			get; internal set;
 		}
 	}
 }
