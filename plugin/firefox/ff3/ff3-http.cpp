5bd64d0062243bc3bbe9f5fa7bbf40bb44d39335
Jeffrey Stedfast <fejj@novell.com>
2009-12-04 18:18:50 +0000

    added copyright headers
    svn path=/trunk/moon/; revision=147686

--- plugin/firefox/ff3/ff3-http.cpp
+++ plugin/firefox/ff3/ff3-http.cpp
@@ -1,3 +1,16 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * ff3-http.cpp: Firefox 3.x bridge
+ *
+ * Contact:
+ *   Moonlight List (moonlight-list@lists.ximian.com)
+ *
+ * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ *
+ */
+
 // define this here so that protypes.h isn't included (and doesn't
 // muck with our npapi.h)
 #define NO_NSPR_10_SUPPORT
