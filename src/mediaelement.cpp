f470b53897ee4be6968734693d5dad7efdb464b7
Chris Toshok <toshok@novell.com>
2009-12-12 02:27:14 +0000

    backport r147612 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=148229

--- src/mediaelement.cpp
+++ src/mediaelement.cpp
@@ -702,6 +702,7 @@ MediaElement::Render (cairo_t *cr, Region *region, bool path_only)
 
         Size specified (GetActualWidth (), GetActualHeight ());
 	Size stretched = ApplySizeConstraints (specified);
+	bool adjust = specified != GetRenderSize ();
 
 	if (stretch != StretchUniformToFill)
 		specified = specified.Min (stretched);
@@ -751,6 +752,12 @@ MediaElement::Render (cairo_t *cr, Region *region, bool path_only)
 		}
 		cairo_pattern_set_filter (cairo_get_source (cr), filter);
 	}
+
+	if (adjust) {
+		specified = MeasureOverride (specified);
+		paint = Rect ((stretched.width - specified.width) * 0.5, (stretched.height - specified.height) * 0.5, specified.width, specified.height);
+	}
+
 
 	if (!path_only)
 		RenderLayoutClip (cr);
