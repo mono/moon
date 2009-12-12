30be1ec9243a50150df6429723e868c453f6450f
Chris Toshok <toshok@novell.com>
2009-12-12 02:28:30 +0000

    backport r147687 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=148238

--- plugin/firefox/ff2/ff2-bridge.cpp
+++ plugin/firefox/ff2/ff2-bridge.cpp
@@ -1,3 +1,16 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * ff2-bridge.cpp: Firefox 2.x bridge
+ *
+ * Contact:
+ *   Moonlight List (moonlight-list@lists.ximian.com)
+ *
+ * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ *
+ */
+
 #include "plugin.h"
 
 #include "ff2-bridge.h"
--- plugin/firefox/ff2/ff2-bridge.h
+++ plugin/firefox/ff2/ff2-bridge.h
@@ -1,3 +1,16 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * ff2-bridge.h: Firefox 2.x bridge
+ *
+ * Contact:
+ *   Moonlight List (moonlight-list@lists.ximian.com)
+ *
+ * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ *
+ */
+
 #ifndef FF2_BRIDGE
 #define FF2_BRIDGE
 
