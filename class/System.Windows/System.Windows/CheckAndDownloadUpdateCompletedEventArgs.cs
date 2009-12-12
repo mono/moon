73177bb1fe6b72b52218a3bdfdd2f078ea392ff0
Chris Toshok <toshok@novell.com>
2009-12-12 02:29:01 +0000

    backport r147694 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=148242

---
+++ class/System.Windows/System.Windows/CheckAndDownloadUpdateCompletedEventArgs.cs
@@ -0,0 +1,12 @@
+
+using System;
+
+namespace System.Windows {
+
+	public sealed class CheckAndDownloadUpdateCompletedEventArgs : EventArgs {
+
+	}
+}
+
+
+
---
+++ class/System.Windows/System.Windows/CheckAndDownloadUpdateCompletedEventHandler.cs
@@ -0,0 +1,11 @@
+
+using System;
+
+namespace System.Windows {
+
+	public delegate void CheckAndDownloadUpdateCompletedEventHandler (object sender, CheckAndDownloadUpdateCompletedEventArgs e);
+
+}
+
+
+
