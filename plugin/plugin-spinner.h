83110321bfbf635626c6d9574732a0c58904bfb6
Chris Toshok <toshok@novell.com>
2009-12-16 22:19:23 +0000

    backport 148591 from moon trunk to the 2.0 branch
    svn path=/branches/moon/moon-2-0/moon/; revision=148603

--- plugin/plugin-spinner.h
+++ plugin/plugin-spinner.h
@@ -1,135 +1,161 @@
 /*
- * plugin-spinner.h: the xaml for the loading spinner.
+ * plugin-spinner.h: the xaml for the fullscreen message.
  *
  * Contact:
  *   Moonlight List (moonlight-list@lists.ximian.com)
  *
- * Copyright 2007 Novell, Inc. (http://www.novell.com)
+ * Copyright 2009 Novell, Inc. (http://www.novell.com)
  *
  * See the LICENSE file included with the distribution for details.
  * 
  */
 #define PLUGIN_SPINNER \
 "<Grid xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" " \
- "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" > " \
-"<Canvas Width=\"400\" Height=\"400\" > " \
-"	<Canvas Canvas.Top=\"200\" Canvas.Left=\"200\"> "\
-"	  <Canvas.Triggers> " \
-"	    <EventTrigger  RoutedEvent=\"Canvas.Loaded\" > "\
-"	      <BeginStoryboard> "\
-"		<Storyboard x:Name=\"sb_spin\" " \
-"			    BeginTime=\"0:0:0\"" \
-"			    RepeatBehavior=\"Forever\">" \
-"		  <DoubleAnimation x:Name=\"spin\" " \
-"				   Storyboard.TargetName=\"xform\"" \
-"				   Storyboard.TargetProperty=\"Angle\"" \
-"				   To=\"360\"" \
-"				   Duration=\"00:00:01\"" \
-"				   />" \
-"		</Storyboard>" \
-"	      </BeginStoryboard>" \
-"	    </EventTrigger>" \
-"	  </Canvas.Triggers>" \
-"	  <Canvas.RenderTransform>" \
-"	    <TransformGroup>" \
-"	      <RotateTransform x:Name=\"xform\" Angle=\"0\" />" \
-"	    </TransformGroup>" \
-"	  </Canvas.RenderTransform>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".15\" ScaleY=\".15\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".2\" ScaleY=\".2\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"30\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".25\" ScaleY=\".25\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"60\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".3\" ScaleY=\".3\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"90\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".35\" ScaleY=\".35\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"120\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".4\" ScaleY=\".4\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"150\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".45\" ScaleY=\".45\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"180\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".5\" ScaleY=\".5\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"210\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".55\" ScaleY=\".55\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"240\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".6\" ScaleY=\".6\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"270\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".65\" ScaleY=\".65\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"300\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	  <Ellipse Fill=\"White\" Stroke=\"Gray\" Width=\"25\" Height=\"25\">" \
-"	    <Ellipse.RenderTransform>" \
-"	      <TransformGroup>" \
-"		<ScaleTransform ScaleX=\".65\" ScaleY=\".65\" CenterX=\"12.5\" CenterY=\"12.5\" />" \
-"		<RotateTransform Angle=\"330\" />" \
-"	      </TransformGroup>" \
-"	    </Ellipse.RenderTransform>" \
-"	  </Ellipse>" \
-"	</Canvas>" \
-"</Canvas>" \
-"</Grid>"
+      "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\">" \
+  "<Grid.Resources>" \
+    "<Style x:Key=\"rectProp\" TargetType=\"Rectangle\">" \
+      "<Setter Property=\"Width\" Value=\"18\"/>" \
+      "<Setter Property=\"Height\" Value=\"18\"/>" \
+      "<Setter Property=\"Fill\" Value=\"#f0f8f8ff\"/>" \
+      "<Setter Property=\"Stroke\" Value=\"DarkGray\"/>" \
+      "<Setter Property=\"StrokeThickness\" Value=\"2\"/>" \
+      "<Setter Property=\"RadiusX\" Value=\"8\"/>" \
+      "<Setter Property=\"RadiusY\" Value=\"8\"/>" \
+    "</Style>" \
+    "<ElasticEase x:Key=\"throbEase\"/>" \
+    "<QuarticEase x:Key=\"throbOut\"/>" \
+  "</Grid.Resources>" \
+  "<Grid.Triggers>" \
+    "<EventTrigger RoutedEvent=\"Grid.Loaded\">" \
+      "<BeginStoryboard>" \
+	"<Storyboard x:Name=\"Throb\" Duration=\"00:00:04\" RepeatBehavior=\"Forever\">" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"One\" Storyboard.TargetProperty=\"Height\" BeginTime=\"00:00:00\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Two\" Storyboard.TargetProperty=\"Height\" BeginTime=\"00:00:00.5\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Three\" Storyboard.TargetProperty=\"Height\" BeginTime=\"00:00:01\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Four\" Storyboard.TargetProperty=\"Height\" BeginTime=\"00:00:01.5\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Five\" Storyboard.TargetProperty=\"Height\" BeginTime=\"-00:00:02\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Six\" Storyboard.TargetProperty=\"Height\" BeginTime=\"-00:00:01.5\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Seven\" Storyboard.TargetProperty=\"Height\" BeginTime=\"-00:00:01\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	  "<DoubleAnimationUsingKeyFrames Storyboard.TargetName=\"Eight\" Storyboard.TargetProperty=\"Height\" BeginTime=\"-00:00:00.5\">" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:01\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:02\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:04\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	    "<EasingDoubleKeyFrame KeyTime=\"00:00:05\" Value=\"22\" EasingFunction=\"{StaticResource throbEase}\"/>" \
+	    "<EasingDoubleKeyFrame keytime=\"00:00:06\" Value=\"18\" EasingFunction=\"{StaticResource throbOut}\"/>" \
+	  "</DoubleAnimationUsingKeyFrames>" \
+	"</Storyboard>" \
+      "</BeginStoryboard>" \
+    "</EventTrigger>" \
+  "</Grid.Triggers>" \
+  "<Grid>" \
+  "<Canvas Width=\"10\" Height=\"10\">" \
+    "<Canvas.RenderTransform>" \
+      "<TransformGroup>" \
+      "<TranslateTransform X=\"5\" Y=\"5\"/>" \
+      "<ScaleTransform ScaleX=\".66\" ScaleY=\".66\"/>" \
+      "</TransformGroup>" \
+    "</Canvas.RenderTransform>" \
+    "<Rectangle x:Name=\"One\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Two\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"45\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Three\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"90\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Four\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"135\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Five\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"180\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Six\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"225\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Seven\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"270\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+    "<Rectangle x:Name=\"Eight\" Style=\"{StaticResource rectProp}\">" \
+      "<Rectangle.RenderTransform>" \
+	"<TransformGroup>" \
+	  "<TranslateTransform Y=\"33\" X=\"-9\"/>" \
+	  "<RotateTransform Angle=\"315\"/>" \
+	"</TransformGroup>" \
+      "</Rectangle.RenderTransform>" \
+    "</Rectangle>" \
+  "</Canvas>" \
+  "</Grid>" \
+"</Grid>" \
+""
+
