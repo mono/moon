c00b5e9eb861dd6532c16dacc585efe481cf80db
Andreia Gaita <avidigal@novell.com>
2009-12-29 21:07:48 +0000

    2009-12-29  Andreia Gaita  <avidigal@novell.com>
    Initial support for indexed properties, like this[int index]. Only
    getters are supported right now.
    
    	* plugin/plugin-class.[h.in|cpp],
    	  System.Windows/Mono/NativeMethods.cs: Add parameters to the
    	GetProperty delegate, to support indexed property getters
    
    	* System.Windows.Browser/ScriptableObjectWrapper.cs:
    	Add support for indexed property getters via "item" property.
    	Add support for accessing Count and Length properties via a
    	single "length" property.
    
    	* plugin/plugin-class.h: regenerated
    
    svn path=/trunk/moon/; revision=148934

--- plugin/plugin-class.cpp
+++ plugin/plugin-class.cpp
@@ -4617,8 +4617,16 @@ MoonlightScriptableObjectObject::HasProperty (NPIdentifier name)
 
 #if ds(!)0
 	NPUTF8 *strname = NPN_UTF8FromIdentifier (name);
-	printf ("scriptable has property %x = %s\n", name, strname);
+	printf ("is indexer: %s\n", NPN_IdentifierIsString (name) ? "false" : "true");
+	if (NPN_IdentifierIsString (name)) {
+		printf ("scriptable has property %x = %s\n", name, strname);
+	} else {
+		printf ("scriptable has property this[%d]\n", NPN_IntFromIdentifier (name));
+	}
 #endif
+
+	if (!NPN_IdentifierIsString (name))
+		name = NPN_GetStringIdentifier ("item");
 
 	result = (g_hash_table_lookup (properties, name) != NULL
 		|| g_hash_table_lookup (events, name)) || MoonlightObject::HasProperty (name);
@@ -4637,6 +4645,18 @@ MoonlightScriptableObjectObject::GetProperty (int id, NPIdentifier name, NPVaria
 {
 	bool res;
 	
+	Value **vargs = NULL;
+	guint32 argCount = 0;
+	if (!NPN_IdentifierIsString (name)) {
+		argCount = 1;
+		vargs = new Value*[argCount];
+		vargs[0] = new Value (NPN_IntFromIdentifier (name));
+		name = NPN_GetStringIdentifier ("item");
+#if ds(!)0
+		printf ("index: %d\n", vargs[0]->AsInt32 ());
+#endif
+	}
+
 	NPUTF8 *strname = NPN_UTF8FromIdentifier (name);
 #if ds(!)0
 	printf ("getting scriptable object property %x = %s\n", name, strname);	
@@ -4647,9 +4667,9 @@ MoonlightScriptableObjectObject::GetProperty (int id, NPIdentifier name, NPVaria
 		res = MoonlightObject::GetProperty (id, name, result);
 	} else {
 		Value v;
-	
-		getprop (managed_scriptable, strname, &v);
-	
+
+		getprop (managed_scriptable, strname, vargs, argCount, &v);
+
 		value_to_variant (this, &v, result);
 		res = true;		
 	}
@@ -4657,6 +4677,13 @@ MoonlightScriptableObjectObject::GetProperty (int id, NPIdentifier name, NPVaria
 #if ds(!)0
 	printf ("getting scriptable object property %x = %s result: %i\n", name, strname, result);
 #endif
+
+	if (argCount > 0) {
+		for (int i = 0; i < argCount; i++)
+			delete vargs[i];
+		delete [] vargs;
+	}
+
 	NPN_MemFree (strname);
 
 	return res;
