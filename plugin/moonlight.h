5cd80e87b7960a8c1fbea03130554fa2a83fb50d
Sebastien Pouliot <sebastien@ximian.com>
2010-01-07 19:20:38 +0000

    2009-01-07  Sebastien Pouliot  <sebastien@ximian.com>
    	* moonlight.h: Set GDK_NATIVE_WINDOW_POINTER before including
    	gtk/gtkwidget.h (required for 64bits architectures)
    
    
    svn path=/trunk/moon/; revision=149183

--- plugin/moonlight.h
+++ plugin/moonlight.h
@@ -30,11 +30,11 @@
 #undef Visual
 
 #include <glib.h>
-#include <gtk/gtkwidget.h>
 
 #if GLIB_SIZEOF_VOID_P == 8
 #define GDK_NATIVE_WINDOW_POINTER 1
 #endif
+#include <gtk/gtkwidget.h>
 
 #include "libmoon.h"
 #include "silverlight-versions.h"
