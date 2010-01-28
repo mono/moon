0bfe04d05d3f7cac4462a558b31fee983371ae18
Chris Toshok <toshok@novell.com>
2010-01-28 03:14:26 +0000

    2010-01-27  Chris Toshok  <toshok@ximian.com>
    	* runtime.cpp (Surface::HideIncompleteSilverlightSupportMessage):
    	duplicate the logic used in the drm message code to clear
    	focused_element when it's the same as the
    	incomplete_support_message.
    
    svn path=/trunk/moon/; revision=150340

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -922,6 +922,8 @@ void
 Surface::HideIncompleteSilverlightSupportMessage ()
 {
 	if (incomplete_support_message) {
+		if (focused_element == incomplete_support_message)
+			focused_element = NULL;
 	        DetachLayer (incomplete_support_message);
 		incomplete_support_message->unref ();
 		incomplete_support_message = NULL;
