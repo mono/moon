a4f4b3f5082d61319f70f9fa0c6938a4a412faaf
Alan McGovern <alan.mcgovern@gmail.com>
2010-02-10 19:14:08 +0000

    add some tests
    svn path=/trunk/moon/; revision=151252

---
+++ test/2.0/moon-unit/System.Windows.Controls/ScrollInfo.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+namespace MoonTest.System.Windows.Controls
+{
+	public class ScrollInfo : Panel, IScrollInfo
+	{
+		public bool CalledCanHorizontallyScroll { get; set; }
+		public bool CalledCanVerticallyScroll { get; set; }
+		bool canHorizontallyScroll;
+		public bool CanHorizontallyScroll
+		{
+			get { CalledCanHorizontallyScroll = true; return canHorizontallyScroll; }
+			set { CalledCanHorizontallyScroll = true; canHorizontallyScroll = value; }
+		}
+
+		bool canVerticallyScroll;
+		public bool CanVerticallyScroll
+		{
+			get { CalledCanVerticallyScroll = true; return canVerticallyScroll; }
+			set { CalledCanVerticallyScroll = true; canVerticallyScroll = value; }
+		}
+
+		double extentHeight;
+		public double ExtentHeight
+		{
+			get { return extentHeight; }
+			set { extentHeight = value; }
+		}
+
+		double extentWidth;
+		public double ExtentWidth
+		{
+			get { return extentWidth; }
+			set { extentWidth = value; }
+		}
+
+		double horizontalOffset;
+		public double HorizontalOffset
+		{
+			get { return horizontalOffset; }
+			set { horizontalOffset = value; }
+		}
+
+		ScrollViewer scrollOwner;
+		public ScrollViewer ScrollOwner
+		{
+			get { return scrollOwner; }
+			set { scrollOwner = value; }
+		}
+
+		double verticalOffset;
+		public double VerticalOffset
+		{
+			get { return verticalOffset; }
+			set { verticalOffset = value; }
+		}
+
+		double viewportHeight;
+		public double ViewportHeight
+		{
+			get { return viewportHeight; }
+			set { viewportHeight = value; }
+		}
+
+		double viewportWidth;
+		public double ViewportWidth
+		{
+			get { return viewportWidth; }
+			set { viewportWidth = value; }
+		}
+
+
+		public ScrollInfo ()
+		{
+			Children.Add (new Rectangle { Width = 1000, Height = 1000, Fill = new SolidColorBrush (Colors.Red) });
+		}
+
+		protected override Size MeasureOverride (Size availableSize)
+		{
+			Children [0].Measure (availableSize);
+			ScrollOwner.InvalidateScrollInfo ();
+			return Children [0].DesiredSize;
+		}
+
+		protected override Size ArrangeOverride (Size finalSize)
+		{
+			Children [0].Arrange (new Rect (0, 0, finalSize.Width, finalSize.Height));
+			return finalSize;
+		}
+
+		public void LineDown ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void LineLeft ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void LineRight ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void LineUp ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Rect MakeVisible (UIElement visual, Rect rectangle)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void MouseWheelDown ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void MouseWheelLeft ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void MouseWheelRight ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void MouseWheelUp ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void PageDown ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void PageLeft ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void PageRight ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void PageUp ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void SetHorizontalOffset (double offset)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void SetVerticalOffset (double offset)
+		{
+			throw new NotImplementedException ();
+		}
+
+	}
+}
---
+++ test/2.0/moon-unit/System.Windows.Controls/ScrollViewer_IScrollInfoTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Silverlight.Testing;
+using System.Windows.Controls.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Markup;
+
+namespace MoonTest.System.Windows.Controls
+{
+	[TestClass]
+	[Ignore]
+	public class ___IScrollInfoTest : SilverlightTest
+	{
+		public IScrollInfo IScrollInfo
+		{
+			get { return ScrollInfo; }
+		}
+
+		public ScrollInfo ScrollInfo
+		{
+			get { return ScrollViewer.FindFirstChild<ScrollInfo> (); }
+		}
+
+		public ScrollViewer ScrollViewer
+		{
+			get; set;
+		}
+
+		[TestInitialize]
+		public void Initialize ()
+		{
+			var template = (ControlTemplate)XamlReader.Load (@"
+<ControlTemplate
+	xmlns=""http://schemas.microsoft.com/client/2007""
+	xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+	xmlns:clr=""clr-namespace:MoonTest.System.Windows.Controls;assembly=moon-unit"">
+	<ScrollContentPresenter>
+		<clr:ScrollInfo Width=""1000"" Height=""1000"" />
+	</ScrollContentPresenter>
+</ControlTemplate>
+");
+			ScrollViewer = new ScrollViewer {
+				Template = template,
+				Width = 50,
+				Height = 50,
+				Content = new Rectangle {
+					Width = 1000,
+					Height = 1000,
+					Fill = new RadialGradientBrush (Colors.Red, Colors.Green)
+				}
+			};
+		}
+
+		[TestMethod]
+		[Asynchronous]
+		public void CalledMethods ()
+		{
+			CreateAsyncTest (ScrollViewer, () => {
+				
+			}, () => {
+				Assert.IsTrue (ScrollInfo.CalledCanHorizontallyScroll, "#1");
+				Assert.IsTrue (ScrollInfo.CalledCanVerticallyScroll, "#2");
+			});
+		}
+	}
+}
