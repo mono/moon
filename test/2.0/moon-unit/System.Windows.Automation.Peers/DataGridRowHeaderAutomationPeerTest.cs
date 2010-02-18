6bcd626888d56ece18332f52b02caf2c094b0a6c
Mario Carrion <mario@carrion.mx>
2010-02-18 15:33:38 +0000

    2010-02-18  Mario Carrion  <mcarrion@novell.com>
    In test/2.0/moon-unit/:
    	* moon-unit.csproj: File added DataGridRowHeaderAutomationPeerTest
    	* System.Windows.Automation.Peers/DataGridRowHeaderAutomationPeerTest.cs:
    	Tests DataGridRowHeaderAutomationPeer.
    
    r: alan
    
    svn path=/trunk/moon/; revision=151996

---
+++ test/2.0/moon-unit/System.Windows.Automation.Peers/DataGridRowHeaderAutomationPeerTest.cs
@@ -0,0 +1,103 @@
+//
+// Unit tests for DataGridColumnsHeaderPresenterAutomationPeer
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace MoonTest.System.Windows.Automation.Peers {
+
+	[TestClass]
+	public class DataGridRowHeaderAutomationPeerTest : FrameworkElementAutomationPeerTest {
+
+		// All these methods are tested in AllTests
+		public override void GetAutomationControlType () {}
+		public override void ContentTest () {}
+		public override void GetClassName () {}
+		public override void IsContentElement () {}
+		public override void Null () {}
+
+		[TestMethod]
+		public void AllTests ()
+		{
+			AutomationPeer peer = FrameworkElementAutomationPeer.CreatePeerForElement (CreateConcreteFrameworkElement ());
+
+			// GetAutomationControlType
+			Assert.AreEqual (AutomationControlType.HeaderItem, peer.GetAutomationControlType (), "GetAutomationControlType");
+			
+			// ContentTest
+			Assert.IsNull (peer.GetChildren (), "#0");
+
+			// GetClassName
+			Assert.AreEqual ("DataGridRowHeader", peer.GetClassName (), "GetClassNameCore");
+
+			// IsContentElement
+			Assert.IsFalse (peer.IsContentElement (), "IsContentElement");
+
+			// Null
+			Assert.Throws<NullReferenceException> (delegate {
+				new DataGridRowHeaderAutomationPeer (null);
+			});
+		}
+
+		[TestMethod]
+		[Asynchronous]
+		public override void IsKeyboardFocusable ()
+		{
+			TestIsNotKeyboardFocusable ();
+		}
+
+		[TestMethod]
+		[Asynchronous]
+		public override void IsKeyboardFocusable_Event ()
+		{
+			TestIsNotKeyboardFocusableEvent ();
+		}
+
+		protected override FrameworkElement CreateConcreteFrameworkElement ()
+		{
+			return new DataGridRowHeader ();
+		}
+
+		protected override FrameworkElementAutomationPeerContract CreateConcreteFrameworkElementAutomationPeer (FrameworkElement element)
+		{
+			// Can't subclass DataGridRowHeaderAutomationPeer
+			return null;
+		}
+
+	}
+
+}
