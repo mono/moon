950216da96a53a1164b83bb8b206a0f957a98415
Mario Carrion <mario@carrion.mx>
2010-02-19 20:58:31 +0000

    2010-02-19  Mario Carrion  <mcarrion@novell.com>
    In test/2.0/moon-unit/:
    	* moon-unit.csproj: File added
    	DataGridDetailsPresenterAutomationPeerTest.
    	* System.Windows.Automation.Peers/DataGridDetailsPresenterAutomationPeerTest.cs:
    	Tests DataGridDetailsPresenterAutomationPeer.
    
    svn path=/trunk/moon/; revision=152093

---
+++ test/2.0/moon-unit/System.Windows.Automation.Peers/DataGridDetailsPresenterAutomationPeerTest.cs
@@ -0,0 +1,86 @@
+//
+// Unit tests for DataGridColumnsHeaderPresenterAutomationPeer
+//
+// Contact:
+//   Moonlight List (moonlight-list@lists.ximian.com)
+//
+// Copyright (C) 2010 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace MoonTest.System.Windows.Automation.Peers {
+
+	[TestClass]
+	public class DataGridDetailsPresenterAutomationPeerTest : FrameworkElementAutomationPeerTest {
+
+		// All these methods are tested in AllTests
+		public override void ContentTest () {}
+		public override void Null () {}
+		public override void IsContentElement () {}
+		public override void GetClassName () {}
+
+		[TestMethod]
+		public void AllTests ()
+		{
+			AutomationPeer peer
+				= FrameworkElementAutomationPeer.CreatePeerForElement (CreateConcreteFrameworkElement ());
+
+			// ContentTest
+			Assert.IsNull (peer.GetChildren (), "#0");
+
+			// Null
+			Assert.Throws<NullReferenceException> (delegate {
+				new DataGridDetailsPresenterAutomationPeer (null);
+			});
+
+			// IsContentElement
+			Assert.IsFalse (peer.IsContentElement (), "IsContentElement");
+
+			// GetClassName
+			Assert.AreEqual ("DataGridDetailsPresenter", peer.GetClassName (), "GetClassNameCore");
+		}
+
+		protected override FrameworkElement CreateConcreteFrameworkElement ()
+		{
+			return new DataGridDetailsPresenter ();
+		}
+
+		protected override FrameworkElementAutomationPeerContract CreateConcreteFrameworkElementAutomationPeer (FrameworkElement element)
+		{
+			// Can't subclass DataGridDetailsPresenterAutomationPeer
+			return null;
+		}
+
+	}
+
+}
