ce914dfc7a1b9abbfcfe11ee2f881f2fd8fd1cd1
Chris Toshok <toshok@novell.com>
2010-03-05 08:04:57 +0000

    simplify this a bunch by using the DOMEventListener as the bookkeeping class as well
    svn path=/trunk/moon/; revision=153095

--- class/System.Windows.Browser/System.Windows.Browser/HtmlObject.cs
+++ class/System.Windows.Browser/System.Windows.Browser/HtmlObject.cs
@@ -35,13 +35,126 @@ namespace System.Windows.Browser {
 
 	public abstract class HtmlObject : ScriptObject {
 
+		private Dictionary<string, Dictionary<object,DOMEventListener>> events;
+
+		
+		protected HtmlObject ()
+		{
+		}
+
+		internal HtmlObject (IntPtr handle, bool handleIsScriptableNPObject)
+			: base (handle, handleIsScriptableNPObject)
+		{
+		}
+
+		internal static void CheckEvent (string eventName, EventHandler handler, EventHandler<HtmlEventArgs> handler_args)
+		{
+			if (eventName == null)
+				throw new ArgumentNullException ("eventName");
+			if ((eventName.Length == 0) || (eventName.IndexOf ('\0') != -1))
+				throw new ArgumentException ("eventName");
+			if ((handler == null) && (handler_args == null))
+				throw new ArgumentNullException ("handler");
+		}
+
+		public bool AttachEvent (string eventName, EventHandler handler)
+		{
+			CheckEvent (eventName, handler, null);
+
+			DOMEventListener listener = new DOMEventListener (this, eventName, handler);
+
+			listener.AttachEvent();
+
+			return AddListener (handler, listener);
+		}
+
+		public bool AttachEvent (string eventName, EventHandler<HtmlEventArgs> handler)
+		{
+			CheckEvent (eventName, null, handler);
+
+			DOMEventListener listener = new DOMEventListener (this, eventName, handler);
+
+			listener.AttachEvent();
+
+			return AddListener (handler, listener);
+		}
+
+		private bool AddListener (object handler, DOMEventListener listener)
+		{
+			Dictionary<object,DOMEventListener> listeners;
+
+			if (listener == null)
+				return false;
+
+			if (events == null)
+				events = new Dictionary<string, Dictionary<object,DOMEventListener>> ();
+			
+			if (!events.TryGetValue (listener.EventName, out listeners)) {
+				listeners = new Dictionary<object,DOMEventListener> ();
+				events.Add (listener.EventName, listeners);
+			}
+			listeners[handler] = listener;
+			return true;
+		}
+		
+		public void DetachEvent (string eventName, EventHandler handler)
+		{
+			CheckEvent (eventName, handler, null);
+
+			DetachListener (eventName, handler);
+			
+		}
+
+		public void DetachEvent (string eventName, EventHandler<HtmlEventArgs> handler)
+		{
+			CheckEvent (eventName, null, handler);
+
+			DetachListener (eventName, handler);
+		}
+
+		private void DetachListener (string eventName, object handler)
+		{
+			Dictionary<object,DOMEventListener> listeners;
+
+			if (events == null)
+				return;
+			
+			if (!events.TryGetValue (eventName, out listeners))
+				return;
+
+			DOMEventListener listener;
+			if (!listeners.TryGetValue (handler, out listener))
+				return;
+
+			listener.DetachEvent();
+
+			listeners.Remove (handler);
+		}
+
+		protected override object ConvertTo (Type targetType, bool allowSerialization)
+		{
+			// documented as "not supported" in SL2 and to throw a ArgumentException "in all cases"
+			// not quite true since a null targetType throws a NRE but otherwise seems correct
+			throw new ArgumentException (targetType.ToString ());
+		}
+
+
+
 		private class DOMEventListener {
-			public DOMEventListener (HtmlObject obj, string name, EventHandler handler, EventHandler<HtmlEventArgs> handler_args)
+			public DOMEventListener (HtmlObject obj, string name, EventHandler<HtmlEventArgs> handler_args) : this (obj, name)
+			{
+				this.handler_args = handler_args;
+			}
+
+			public DOMEventListener (HtmlObject obj, string name, EventHandler handler) : this (obj, name)
+			{
+				this.handler = handler;
+			}
+
+			public DOMEventListener (HtmlObject obj, string name)
 			{
 				this.obj = obj;
 				this.name = name;
-				this.handler = handler;
-				this.handler_args = handler_args;
 			}
 
 			[ScriptableMember (ScriptAlias = "handleEvent")]
@@ -59,143 +172,32 @@ namespace System.Windows.Browser {
 				}
 			}
 
-			public HtmlObject obj;
-			public string name;
-			public EventHandler handler;
-			public EventHandler<HtmlEventArgs> handler_args;
-		}
-
-		private sealed class EventInfo {
-			public EventInfo (HtmlObject obj, string eventName, EventHandler handler, EventHandler<HtmlEventArgs> handler_args)
-			{
-				this.obj = obj;
-				this.event_name = eventName;
-				listener = new DOMEventListener (obj, EventNameMozilla, handler, handler_args);
+			public string EventName {
+				get { return name; }
 			}
 
-			public string EventNameMozilla {
+			private string EventNameMozilla {
 				get {
-					if (event_name.StartsWith ("on"))
-						return event_name.Substring (2);
-					return event_name;
+					if (name.StartsWith ("on"))
+						return name.Substring (2);
+					return name;
 				}
 			}
 
 			public void AttachEvent ()
 			{
-				obj.Invoke ("addEventListener", new object[] { EventNameMozilla, listener, false });
+				obj.Invoke ("addEventListener", new object[] { EventNameMozilla, this, false });
 			}
 
 			public void DetachEvent ()
 			{
-				obj.Invoke ("removeEventListener", new object[] { EventNameMozilla, listener, false });
+				obj.Invoke ("removeEventListener", new object[] { EventNameMozilla, this, false });
 			}
 
-			static internal void CheckEvent (string eventName, EventHandler handler, EventHandler<HtmlEventArgs> handler_args)
-			{
-				if (eventName == null)
-					throw new ArgumentNullException ("eventName");
-				if ((eventName.Length == 0) || (eventName.IndexOf ('\0') != -1))
-					throw new ArgumentException ("eventName");
-				if ((handler == null) && (handler_args == null))
-					throw new ArgumentNullException ("handler");
-			}
-			
-			public static EventInfo AttachEvent (string eventName, EventHandler handler, EventHandler<HtmlEventArgs> handler_args, HtmlObject obj)
-			{
-				CheckEvent (eventName, handler, handler_args);
-
-				EventInfo info = new EventInfo (obj, eventName, handler, handler_args);
-
-				info.AttachEvent ();
-
-				return info;
-			}
-
-			public HtmlObject obj;
-			public string event_name;
-			public DOMEventListener listener;
-		}
-
-		private Dictionary<string, List<EventInfo>> events;
-
-		
-		protected HtmlObject ()
-		{
-		}
-
-		internal HtmlObject (IntPtr handle, bool handleIsScriptableNPObject)
-			: base (handle, handleIsScriptableNPObject)
-		{
-		}
-
-		public bool AttachEvent (string eventName, EventHandler handler)
-		{
-			return AddEventInfo (EventInfo.AttachEvent (eventName, handler, null, this));
-		}
-
-		public bool AttachEvent (string eventName, EventHandler<HtmlEventArgs> handler)
-		{
-			return AddEventInfo (EventInfo.AttachEvent (eventName, null, handler, this));
-		}
-
-		private bool AddEventInfo (EventInfo info)
-		{
-			List<EventInfo> list;
-
-			if (info == null)
-				return false;
-
-			if (events == null)
-				events = new Dictionary<string, List<EventInfo>> ();
-			
-			if (!events.TryGetValue (info.event_name, out list)) {
-				list = new List<EventInfo> ();
-				events.Add (info.event_name, list);
-			}
-			list.Add (info);
-			return true;
-		}
-		
-		public void DetachEvent (string eventName, EventHandler handler)
-		{
-			DetachEvent (eventName, handler, null);
-		}
-
-		public void DetachEvent (string eventName, EventHandler<HtmlEventArgs> handler)
-		{
-			DetachEvent (eventName, null, handler);
-		}
-
-		private void DetachEvent (string eventName, EventHandler handler, EventHandler<HtmlEventArgs> handler_args)
-		{
-			EventInfo.CheckEvent (eventName, handler, handler_args);
-
-			List<EventInfo> list;
-
-			if (events == null)
-				return;
-			
-			if (!events.TryGetValue (eventName, out list)) {
-				return;
-			}
-
-			for (int i = list.Count - 1; i >= 0; i--) {
-				EventInfo info = list [i];
-				if (info.listener.handler == handler || info.listener.handler_args == handler_args) {
-					list.RemoveAt (i);
-					info.DetachEvent ();
-					// Do we continue looking for duplicates?
-					// break;
-				}
-			}
-		}
-
-		protected override object ConvertTo (Type targetType, bool allowSerialization)
-		{
-			// documented as "not supported" in SL2 and to throw a ArgumentException "in all cases"
-			// not quite true since a null targetType throws a NRE but otherwise seems correct
-			throw new ArgumentException (targetType.ToString ());
+			private HtmlObject obj;
+			private string name;
+			private EventHandler handler;
+			private EventHandler<HtmlEventArgs> handler_args;
 		}
 	}
 }
