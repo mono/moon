750ed058f9b863f26253ceb1618f38f475c3c0c3
Jeffrey Stedfast <fejj@novell.com>
2010-03-26 19:07:25 +0000

    2010-03-26  Jeffrey Stedfast  <fejj@novell.com>
    	* application.js: Turns out the arg[0] is a nsIURI, so get that
    	and then extract the path to set on the iframe as the source.
    
    
    svn path=/trunk/moon/; revision=154308

--- plugin/install/application.js
+++ plugin/install/application.js
@@ -1,6 +1,7 @@
-let nsISupports = Components.interfaces.nsISupports;
-let nsISupportsArray = Components.interfaces.nsISupportsArray;
-let nsISupportsString = Components.interfaces.nsISupportsString;
+const nsIURI = Components.interfaces.nsIURI;
+const nsISupports = Components.interfaces.nsISupports;
+const nsISupportsArray = Components.interfaces.nsISupportsArray;
+const nsISupportsString = Components.interfaces.nsISupportsString;
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
@@ -10,40 +11,20 @@ function ApplicationStartup()
 	var iframe = null;
 	var src = null;
 	
-	// window.arguments[0]: URI to load (string), or an nsISupportsArray of
-	//                      nsISupportsStrings to load, or a xul:tab of
-	//                      a tabbrowser, which will be replaced by this
-	//                      window (for this case, all other arguments are
-	//                      ignored).
-	//                 [1]: character set (string)
-	//                 [2]: referrer (nsIURI)
-	//                 [3]: postData (nsIInputStream)
-	//                 [4]: allowThirdPartyFixup (bool)
 	if ("arguments" in window && window.arguments[0])
 		uriToLoad = window.arguments[0];
 	
 	iframe = document.getElementById ("MoonlightIFrame");
 	
 	if (uriToLoad) {
-		if (uriToLoad instanceof nsISupportsArray) {
-			let count = uriToLoad.Count ();
-			
-			alert ("loading " + count + " uris");
-			for (let i = 0; i < count; i++) {
-				let str = uriToLoad.GetElementAt (i).QueryInterface (nsISupportsString);
-				alert ("urilist[" + i + "] = " + str);
-			}
-		} else if (uriToLoad instanceof XULElement) {
-			alert ("loading a XULElement dingus");
-		} else if (uriToLoad instanceof nsISupports) {
-			alert ("loading a nsISupports dingus");
+		if (uriToLoad instanceof nsISupports) {
+			let uri = uriToLoad.QueryInterface (nsIURI);
+			src = uri.path;
 		} else {
-			alert ("loading single uri " + uriToLoad);
 			src = uriToLoad;
 		}
-	} else {
-		alert ("no uris??");
 	}
 	
-	iframe.src = src;
+	if (iframe)
+		iframe.src = src;
 }
