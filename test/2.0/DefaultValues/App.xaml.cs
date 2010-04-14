6afa1b3cc5a55bf15b6a194c070ed8332fe5045e
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-04-14 08:49:56 +0000

    2010-04-14  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* DefaultValues/App.xaml.cs: Make formatting use the en-US locale.
    
    svn path=/trunk/moon/; revision=155360

--- test/2.0/DefaultValues/App.xaml.cs
+++ test/2.0/DefaultValues/App.xaml.cs
@@ -7,6 +7,8 @@ namespace DefaultValues {
 	public partial class App : Application {
 		public App ()
 		{
+			System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo ("en-US");
+			System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo ("en-US");
 			this.Startup += this.Application_Startup;
 			InitializeComponent ();
 		}
