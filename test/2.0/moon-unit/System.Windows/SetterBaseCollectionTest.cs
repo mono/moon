eeae65508379e7e6b370f1503d5b1ce7342bce95
Alan McGovern <alan.mcgovern@gmail.com>
2010-04-19 13:14:27 +0000

    2010-04-19  Alan McGovern  <amcgovern@novell.com>
    	* test/2.0/moon-unit/System.Windows/SetterBaseCollectionTest.cs:
    	  Remove outdated comment
    
    svn path=/trunk/moon/; revision=155733

--- test/2.0/moon-unit/System.Windows/SetterBaseCollectionTest.cs
+++ test/2.0/moon-unit/System.Windows/SetterBaseCollectionTest.cs
@@ -125,7 +125,7 @@ namespace MoonTest.System.Windows
 			Rectangle r = new Rectangle ();
 			r.Style = s;
 			Assert.IsTrue (s.IsSealed, "IsSealed-3");
-			Assert.IsFalse (s.Setters.IsSealed, "Setters.IsSealed-3"); // Fails in Silverlight 3
+			Assert.IsFalse (s.Setters.IsSealed, "Setters.IsSealed-3");
 		}
 
 		[TestMethod]
