a8db91bb82591022a6de19781e3eb2fc5485cd04
Alan McGovern <alan.mcgovern@gmail.com>
2010-04-23 13:35:23 +0000

    2010-04-23  Alan McGovern  <amcgovern@novell.com>
    	* test/2.0/moon-unit/System.Windows.Data/CollectionViewGroupTest.cs:
    	  Add tests for CollectionViewGroup. We already passed them
    	  all, yay!
    
    svn path=/trunk/moon/; revision=156008

--- test/2.0/moon-unit/System.Windows.Data/CollectionViewGroupTest.cs
+++ test/2.0/moon-unit/System.Windows.Data/CollectionViewGroupTest.cs
@@ -38,11 +38,84 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 
 namespace MoonTest.System.Windows.Data {
 
 	[TestClass]
 	public partial class CollectionViewGroupTest {
+
+		[TestMethod]
+		public void Constructor_Null ()
+		{
+			var g = new ConcreteCollectionViewGroup (null);
+			Assert.IsNull (g.Name, "#1");
+		}
+
+		[TestMethod]
+		public void Constructor_Object ()
+		{
+			object o = new object ();
+			var g = new ConcreteCollectionViewGroup (o);
+			Assert.AreSame (o, g.Name, "#1");
+		}
+
+		[TestMethod]
+		public void AddItem_DoNotIncrementItemCount ()
+		{
+			var g = new ConcreteCollectionViewGroup ("");
+			g.ProtectedItems.Add (new object ());
+
+			Assert.AreEqual (0, g.OnPropertyChangedCalled.Count, "#1");
+			Assert.AreEqual (0, g.ProtectedItemCount, "#2");
+			Assert.AreEqual (0, g.ItemCount, "#3");
+		}
+
+		[TestMethod]
+		public void AddItem_IncrementItemCount ()
+		{
+			var g = new ConcreteCollectionViewGroup ("");
+			g.ProtectedItems.Add (new object ());
+			g.ProtectedItemCount++;
+
+			Assert.AreEqual (1, g.OnPropertyChangedCalled.Count, "#1");
+			Assert.AreEqual ("ItemCount", g.OnPropertyChangedCalled [0], "#2");
+			Assert.AreEqual (1, g.ProtectedItemCount, "#3");
+			Assert.AreEqual (1, g.ItemCount, "#4");
+		}
+	}
+
+	class ConcreteCollectionViewGroup : CollectionViewGroup {
+		public List<string> OnPropertyChangedCalled = new List<string> ();
+		public List<string> PropertyChangedRaised = new List<string> ();
+
+		public ConcreteCollectionViewGroup (object name)
+			: base (name)
+		{
+			PropertyChanged += (o, e) => PropertyChangedRaised.Add (e.PropertyName);
+		}
+
+		public override bool IsBottomLevel
+		{
+			get { return true; }
+		}
 		
+		protected override void OnPropertyChanged (global::System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			OnPropertyChangedCalled.Add (e.PropertyName);
+			base.OnPropertyChanged (e);
+		}
+
+		public new int ProtectedItemCount {
+			get { return base.ProtectedItemCount; }
+			set { base.ProtectedItemCount = value; }
+		}
+
+		public new ObservableCollection<object> ProtectedItems
+		{
+			get { return base.ProtectedItems; }
+		}
 	}
 }
