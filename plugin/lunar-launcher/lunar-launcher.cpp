b534cf679a27d23fbc82ca231deebb654eb41b31
Jeffrey Stedfast <fejj@novell.com>
2010-04-26 20:30:56 +0000

    2010-04-26  Jeffrey Stedfast  <fejj@novell.com>
    	* lunar-launcher.cpp: Rewritten to not use gtkmozembed at all.
    
    
    svn path=/trunk/moon/; revision=156132

--- plugin/lunar-launcher/lunar-launcher.cpp
+++ plugin/lunar-launcher/lunar-launcher.cpp
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
  * lunar-launcher.cpp:
  *
@@ -9,314 +10,414 @@
  * See the LICENSE file included with the distribution for details.
  *
  */
- 
+
 #include <config.h>
 
-#include <string.h>
+#include <gtk/gtk.h>
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
-#include <signal.h>
-#include <sys/syscall.h>
-#include <sys/wait.h>
-#include <dlfcn.h>
 
-#include "nsXPCOMGlue.h"
-// this fixes a few redefined macros warnings:
-#define nscore_h__
+#include "runtime.h"
+#include "deployment.h"
+#include "downloader.h"
 
-#if HAVE_GECKO_1_9
-#include <gtkmozembed.h>
-#else
-#include "gtkembedmoz/gtkmozembed.h"
-#endif
-  
-#include "lunar-launcher.h"
+#include "gtk/window-gtk.h"
+#include "gtk/pal-gtk.h"
 
-#define LOG_LAUNCH(...) printf (__VA_ARGS__)
+#include "getopts.h"
 
-/*
- * LunarLauncher
- */
-LunarLauncher::LunarLauncher ()
+static const char *geometry = NULL;
+
+typedef struct {
+	GdkPixbufLoader *loader;
+	GtkWindow *window;
+} IconLoader;
+
+static void
+icon_loader_notify_cb (NotifyType type, gint64 args, gpointer user_data)
 {
-	application = NULL;
-	title = NULL;
-	width = 0;
-	height = 0;
+	IconLoader *icon = (IconLoader *) user_data;
+	GtkWindow *window = icon->window;
+	GdkPixbuf *pixbuf;
+	GList *icon_list;
+	
+	icon_list = gtk_window_get_icon_list (window);
+	
+	switch (type) {
+	case NotifyCompleted:
+		if (icon->loader) {
+			if (gdk_pixbuf_loader_close (icon->loader, NULL)) {
+				/* get the pixbuf and add it to our icon_list */
+				pixbuf = gdk_pixbuf_loader_get_pixbuf (icon->loader);
+				icon_list = g_list_prepend (icon_list, pixbuf);
+				gtk_window_set_icon_list (window, icon_list);
+				g_free (icon);
+				break;
+			}
+			
+			/* fall through as if we got a NotifyFailed */
+		}
+	case NotifyFailed:
+		if (icon->loader)
+			g_object_unref (icon->loader);
+		
+		g_free (icon);
+		break;
+	default:
+		break;
+	}
 }
 
-LunarLauncher::~LunarLauncher ()
+static void
+icon_loader_write_cb (void *buffer, gint32 offset, gint32 n, gpointer user_data)
 {
-	g_free (application);
-	g_free (title);
+	IconLoader *icon = (IconLoader *) user_data;
+	
+	if (icon->loader && !gdk_pixbuf_loader_write (icon->loader, (const guchar *) buffer, n, NULL)) {
+		/* loading failed, destroy the loader */
+		g_object_unref (icon->loader);
+		icon->loader = NULL;
+	}
 }
 
-void
-LunarLauncher::SetApplication (const char *value)
+static void
+load_window_icons (GtkWindow *window, Deployment *deployment, IconCollection *icons)
 {
-	g_free (application);
-	application = g_strdup (value);
-}
-
-void
-LunarLauncher::SetTitle (const char *value)
-{
-	g_free (title);
-	title = g_strdup (value);
-}
-
-void
-LunarLauncher::ShowUsage ()
-{
-	fprintf (stderr,
-"Usage is: lunar-launcher [options]\n"
-"\n"
-"Options:\n"
-"  -moonapp <application>\n"
-"  -moontitle <title>\n"
-"  -moonwidth <width>\n"
-"  -moonheight <height\n");
-}
-
-int
-LunarLauncher::Launch (int argc, char **argv)
-{
-	// fprintf (fp, "firefox -moonapp \"file://%s/index.html\" -moonwidth %d -moonheight %d -moontitle \"%s\"\n", app_dir, width, height, settings->GetShortName());
-
-	for (int i = 1; i < argc; i += 2) {
-		if (!strcmp (argv [i], "-moonapp")) {
-			SetApplication (argv [i + 1]);
-		} else if (!strcmp (argv [i], "-moonwidth")) {
-			SetWidth (atoi (argv [i + 1]));
-		} else if (!strcmp (argv [i], "-moonheight")) {
-			SetHeight (atoi (argv [i + 1]));
-		} else if (!strcmp (argv [i], "-moontitle")) {
-			SetTitle (argv [i + 1]);
-		} else {
-			ShowUsage ();
-			return 1;
+	Application *application = deployment->GetCurrentApplication ();
+	IconLoader *loader;
+	int count, i;
+	
+	/* load the icons */
+	if (icons && (count = icons->GetCount ()) > 0) {
+		for (i = 0; i < count; i++) {
+			Value *value = icons->GetValueAt (i);
+			Icon *icon = value->AsIcon ();
+			Uri *uri = icon->GetSource ();
+			
+			loader = g_new (IconLoader, 1);
+			loader->loader = gdk_pixbuf_loader_new ();
+			loader->window = window;
+			
+			application->GetResource (NULL, uri, icon_loader_notify_cb, icon_loader_write_cb, MediaPolicy, NULL, loader);
 		}
 	}
+}
 
-	if (application == NULL || title == NULL || height == 0 || width == 0) {
-		ShowUsage ();
-		return 1;
+static void
+resize_surface (GtkWidget *widget, GtkAllocation *allocated, Surface *surface)
+{
+	surface->Resize (allocated->width, allocated->height);
+}
+
+static bool
+load_app (Deployment *deployment, const char *app_id)
+{
+	MoonInstallerService *installer;
+	const char *base_dir;
+	MoonAppRecord *app;
+	Surface *surface;
+	char *path, *uri;
+	
+	installer = runtime_get_installer_service ();
+	if (!(app = installer->GetAppRecord (app_id))) {
+		g_warning ("Could not find application: %s", app_id);
+		return false;
 	}
+	
+	/* use the App's origin URI as the surface's SourceLocation so that it
+	 * can do proper web download checks */
+	surface = deployment->GetSurface ();
+	surface->SetSourceLocation (app->origin);
+	delete app;
+	
+	/* get the install path/uri for the Xap */
+	base_dir = installer->GetBaseInstallDir ();
+	path = g_build_filename (base_dir, app_id, "Application.xap", NULL);
+	uri = g_strdup_printf ("file://%s", path);
+	
+	deployment->SetXapFilename (path);
+	deployment->SetXapLocation (uri);
+	g_free (path);
+	g_free (uri);
+	
+	/* load the xap */
+	return deployment->InitializeManagedDeployment (NULL, NULL, NULL);
+}
 
-	LOG_LAUNCH ("[%i lunar launcher] Launching %s (%s width: %i height; %i)\n", getpid (), title, application, width, height);
+static GtkWidget *
+create_window (Deployment *deployment, const char *geometry, const char *app_id)
+{
+	MoonWindowingSystem *winsys;
+	GtkWidget *widget, *window;
+	OutOfBrowserSettings *oob;
+	WindowSettings *settings;
+	MoonWindow *moon_window;
+	int width, height;
+	Surface *surface;
+	
+	winsys = runtime_get_windowing_system ();
+	
+	/* create the moonlight widget */
+	moon_window = winsys->CreateWindow (false, width, height);
+	surface = new Surface (moon_window);
+	deployment->SetSurface (surface);
+	moon_window->SetSurface (surface);
+	
+	if (!load_app (deployment, app_id))
+		return NULL;
+	
+	widget = ((MoonWindowGtk *) moon_window)->GetWidget ();
+	g_signal_connect (widget, "size-allocate", G_CALLBACK (resize_surface), surface);
+	gtk_widget_show (widget);
+	
+	/* create the window */
+	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_widget_set_app_paintable (window, true);
+	g_signal_connect_after (window, "delete-event", G_CALLBACK (gtk_main_quit), NULL);
+	gtk_container_add ((GtkContainer *) window, widget);
+	
+	if ((oob = deployment->GetOutOfBrowserSettings ())) {
+		load_window_icons ((GtkWindow *) window, deployment, oob->GetIcons ());
+		settings = oob->GetWindowSettings ();
+	} else
+		settings = NULL;
+	
+	if (settings != NULL) {
+		gtk_window_set_title ((GtkWindow *) window, settings->GetTitle ());
+		
+		if (geometry == NULL) {
+			gtk_window_resize ((GtkWindow *) window, settings->GetWidth (), settings->GetHeight ());
+			
+			if (settings->GetWindowStartupLocation () == WindowStartupLocationManual)
+				gtk_window_move ((GtkWindow *) window, settings->GetLeft (), settings->GetTop ());
+		}
+		
+		switch (settings->GetWindowStyle ()) {
+		case WindowStyleBorderlessRoundCornersWindow:
+		case WindowStyleNone:
+			gtk_window_set_decorated ((GtkWindow *) window, false);
+			break;
+		default:
+			/* by default, gtk enables window decorations */
+			break;
+		}
+	} else if (oob != NULL) {
+		gtk_window_set_title ((GtkWindow *) window, oob->GetShortName ());
+	} else {
+		gtk_window_set_title ((GtkWindow *) window, "Moonlight");
+	}
+	
+	if (geometry != NULL) {
+		/* override WindowSettings' dimensions */
+		gtk_window_parse_geometry ((GtkWindow *) window, geometry);
+	}
+	
+	/* resize the moonlight widget */
+	gtk_window_get_size ((GtkWindow *) window, &width, &height);
+	moon_window->Resize (width, height);
+	
+	return window;
+}
 
-	top_level_window = GTK_WINDOW (gtk_window_new (GTK_WINDOW_TOPLEVEL));
-	gtk_window_set_title (top_level_window, title);
+class FileDownloadState {
+	Downloader *downloader;
+	char *uri;
+	
+ public:
+	
+	FileDownloadState (Downloader *dl)
+	{
+		downloader = dl;
+		uri = NULL;
+	}
+	
+	~FileDownloadState ()
+	{
+		Close ();
+	}
+	
+	void Abort ()
+	{
+		Close ();
+	}
+	
+	char *GetResponseText (char *fname, char *PartName)
+	{
+		return NULL;
+	}
+	
+	void Open (const char *verb, const char *uri)
+	{
+		this->uri = g_strdup (uri);
+	}
+	
+	void Send ()
+	{
+		int n, offset = 0;
+		struct stat st;
+		char buf[4096];
+		FILE *fp;
+		
+		if (stat (uri, &st) == -1 || !S_ISREG (st.st_mode)
+		    || !(fp = fopen (uri, "rb"))) {
+			downloader->NotifyFailed ("File does not exist");
+			return;
+		}
+		
+		downloader->NotifySize (st.st_size);
+		
+		offset = 0;
+		while ((n = fread (buf, 1, sizeof (buf), fp)) > 0) {
+			downloader->Write (buf, offset, n);
+			offset += n;
+		}
+		
+		fclose (fp);
+		
+		downloader->NotifyFinished (uri);
+	}
+	
+	void Close ()
+	{
+		g_free (uri);
+		uri = NULL;
+	}
+};
 
-	static const GREVersionRange gre_version = {
-		"1.8", PR_TRUE,
-		"9.9", PR_TRUE
-	};
+static gpointer
+downloader_create_state (Downloader *dl)
+{
+	return new FileDownloadState (dl);
+}
 
-	char xpcom_lib_path [PATH_MAX];
-	char* xpcom_dir_path;
+static void
+downloader_destroy_state (gpointer data)
+{
+	delete ((FileDownloadState *) data);
+}
 
-	GRE_GetGREPathWithProperties (&gre_version, 1, nsnull, 0, xpcom_lib_path, sizeof (xpcom_lib_path));
-	xpcom_dir_path = g_path_get_dirname (xpcom_lib_path);
+static void
+downloader_open (gpointer state, const char *verb, const char *uri, bool streaming, bool disable_cache)
+{
+	((FileDownloadState *) state)->Open (verb, uri);
+}
 
-#if HAVE_GECKO_1_9
-	gtk_moz_embed_set_path (xpcom_dir_path);
-#else
-	gtk_moz_embed_set_comp_path (xpcom_dir_path);
-#endif
-	g_free (xpcom_dir_path);
+static void
+downloader_send (gpointer state)
+{
+	((FileDownloadState *) state)->Send ();
+}
 
-	moz_embed = gtk_moz_embed_new();
-	gtk_container_add (GTK_CONTAINER (top_level_window), moz_embed);
+static void
+downloader_abort (gpointer state)
+{
+	((FileDownloadState *) state)->Abort ();
+}
 
-	g_signal_connect (G_OBJECT (top_level_window), "delete-event",    G_CALLBACK (DeleteCallback),   top_level_window);
-	g_signal_connect (G_OBJECT (top_level_window), "key_press_event", G_CALLBACK (KeyPressCallback), top_level_window);
+static void
+downloader_header (gpointer state, const char *header, const char *value)
+{
+	//g_assert_not_reached ();
+}
 
-	gtk_widget_set_usize (moz_embed, width, height);
-	gtk_widget_show_all (moz_embed);
-	gtk_widget_show_all (GTK_WIDGET (top_level_window));
+static void
+downloader_body (gpointer state, void *body, guint32 length)
+{
+	//g_assert_not_reached ();
+}
 
-	gtk_moz_embed_load_url (GTK_MOZ_EMBED (moz_embed), application);
-	gtk_main ();
+static void *
+downloader_request (const char *method, const char *uri, gpointer context)
+{
+	g_assert_not_reached ();
+}
 
+static int
+display_help (GetOptsContext *ctx, GetOptsOption *opt, const char *arg, void *valuep)
+{
+	fputs ("Usage: lunar-launcher [OPTIONS...] [appid]\n\n", stdout);
+	getopts_print_help (ctx);
+	exit (EXIT_SUCCESS);
+	
 	return 0;
 }
 
-gboolean
-LunarLauncher::DeleteCallback (GtkWidget* widget, GdkEventKey* event, GtkWindow* window)
+static int
+display_version (GetOptsContext *ctx, GetOptsOption *opt, const char *arg, void *valuep)
 {
-	gtk_main_quit ();
-	return false;
-}
-
-gboolean
-LunarLauncher::KeyPressCallback (GtkWidget* widget, GdkEventKey* event, GtkWindow* window)
-{
-	LOG_LAUNCH ("[%i lunar launcher]: key press: keyval: 0x%x state: 0x%x\n", getpid (), event->keyval, event->state);
-	return false;
-}
-
-/*
- * Signal handling
- */
-
-/*
- * launcher_handle_native_sigsegv: 
- *   (this is a slightly modified version of mono_handle_native_sigsegv from mono/mono/mini/mini-exceptions.c)
- *
- */
-
-typedef void void_method ();
-
-static void
-print_stack_traces ()
-{
-	/* Try to get more meaningful information using gdb */
-#if !defined(PLATFORM_WIN32)
-	/* From g_spawn_command_line_sync () in eglib */
-	int res;
-	int stdout_pipe [2] = { -1, -1 };
-	pid_t pid;
-	const char *argv [16];
-	char buf1 [128];
-	int status;
-	char buffer [1024];
-
-	res = pipe (stdout_pipe);
-	g_assert (res != -1);
-		
-	/*
-	 * glibc fork acquires some locks, so if the crash happened inside malloc/free,
-	 * it will deadlock. Call the syscall directly instead.
-	 */
-	pid = syscall (SYS_fork);
-	if (pid == 0) {
-		close (stdout_pipe [0]);
-		dup2 (stdout_pipe [1], STDOUT_FILENO);
-
-		for (int i = getdtablesize () - 1; i >= 3; i--)
-			close (i);
-
-		argv [0] = g_find_program_in_path ("gdb");
-		if (argv [0] == NULL) {
-			close (STDOUT_FILENO);
-			exit (1);
-		}
-
-		argv [1] = "-ex";
-		sprintf (buf1, "attach %ld", (long)getpid ());
-		argv [2] = buf1;
-		argv [3] = "--ex";
-		argv [4] = "info threads";
-		argv [5] = "--ex";
-		argv [6] = "thread apply all bt";
-		argv [7] = "--batch";
-		argv [8] = 0;
-
-		execv (argv [0], (char**)argv);
-		exit (1);
-	}
-
-	close (stdout_pipe [1]);
-
-	fprintf (stderr, "\nDebug info from gdb:\n\n");
-
-	while (1) {
-		int nread = read (stdout_pipe [0], buffer, 1024);
-
-		if (nread <= 0)
-			break;
-		write (STDERR_FILENO, buffer, nread);
-	}		
-
-	waitpid (pid, &status, WNOHANG);
+	fputs ("lunar-launcher " VERSION "\n", stdout);
+	exit (EXIT_SUCCESS);
 	
-#endif
-
-	fprintf (stderr, "\nDebug info from libmoon:\n\n");
-	void *libmoon = dlopen ("libmoon.so", RTLD_LOCAL | RTLD_LAZY);
-	void_method *print_stack_trace = (libmoon == NULL) ? NULL : (void_method *) dlsym (libmoon, "print_stack_trace");
-	if (print_stack_trace)
-		print_stack_trace ();
+	return 0;
 }
 
-static bool handling_sigsegv = false;
+#define HELP_DESC      "Display help and quit"
+#define VERSION_DESC   "Display version and quit"
+#define GEOMETRY_DESC  "Override window geometry"
 
-void
-launcher_handle_native_sigsegv (int signal)
+static GetOptsOption options[] = {
+	{ "help",      'h',  GETOPTS_ARG_CUSTOM,          HELP_DESC,      NULL,     'h', display_help,    NULL       },
+	{ "version",   'v',  GETOPTS_ARG_CUSTOM,          VERSION_DESC,   NULL,     'v', display_version, NULL       },
+	{ "geometry",   0,   GETOPTS_REQUIRED_STRING_ARG, GEOMETRY_DESC,  NULL,     'g', NULL,            &geometry  },
+	GETOPTS_TABLE_END
+};
+
+int main (int argc, char **argv)
 {
-	const char *signal_str = (signal == SIGSEGV) ? "SIGSEGV" : "SIGABRT";
-	switch (signal) {
-	case SIGSEGV: signal_str = "SIGSEGV"; break;
-	case SIGABRT: signal_str = "SIGABRT"; break;
-	case SIGQUIT: signal_str = "SIGQUIT"; break;
-	default:
-		signal_str = "UNKNOWN"; break;
-	}
+	Deployment *deployment;
+	GetOptsContext *ctx;
+	const char **args;
+	GtkWidget *window;
+	int n;
 	
-	if (handling_sigsegv) {
-		/*
-		 * In our normal sigsegv handling we do signal-unsafe things to provide better 
-		 * output to what actually happened. If we get another one, do only signal-safe
-		 * things
-		 */
-		_exit (1);
-		return;
-	}
-
-	/* To prevent infinite loops when the stack walk causes a crash */
-	handling_sigsegv = true;
-
-	/*
-	 * A SIGSEGV indicates something went very wrong so we can no longer depend
-	 * on anything working. So try to print out lots of diagnostics, starting 
-	 * with ones which have a greater chance of working.
-	 */
-	fprintf (stderr,
-			 "\n"
-			 "=============================================================\n"
-			 "Moonlight (OOB) got a %s while executing native code.        \n"
-			 " We'll first ask gdb for a stack trace, then try our own     \n"
-			 " stack walking method (usually not as good as gdb, but it    \n"
-			 " can do managed and native stack traces together)            \n"
-			 "=============================================================\n"
-			 "\n", signal_str);
-
-	print_stack_traces ();
-
-	if (signal != SIGQUIT) {
-		abort ();
-	} else {
-		handling_sigsegv = false;
-	}
-}
-
-static void
-launcher_add_signal_handlers ()
-{
-	struct sigaction sa;
-
-	sa.sa_handler = launcher_handle_native_sigsegv;
-	sigemptyset (&sa.sa_mask);
-	sa.sa_flags = 0;
-
-	g_assert (sigaction (SIGSEGV, &sa, NULL) != -1);
-	g_assert (sigaction (SIGQUIT, &sa, NULL) != -1);
-}
-
-int
-main (int argc, char **argv)
-{
-	LunarLauncher *launcher;
-	int result;
-
-	launcher_add_signal_handlers ();
-
 	gtk_init (&argc, &argv);
-
-	launcher = new LunarLauncher ();
-	result = launcher->Launch (argc, argv);
-	delete launcher;
-
-	return result;
+	g_thread_init (NULL);
+	gdk_threads_init ();
+	
+	ctx = getopts_context_new (argc, argv, options, GETOPTS_FLAG_DEFAULT);
+	getopts_parse_args (ctx);
+	args = getopts_get_args (ctx, &n);
+	
+	if (n != 1)
+		display_help (ctx, NULL, NULL, NULL);
+	
+	// FIXME: need to get the plugin dir somehow
+	runtime_init_browser ("/home/fejj/.mozilla/firefox/3ocwu648.default/extensions/moonlight@novell.com/plugins/moonlight");
+	
+	Downloader::SetFunctions (downloader_create_state,
+				  downloader_destroy_state,
+				  downloader_open,
+				  downloader_send,
+				  downloader_abort,
+				  downloader_header,
+				  downloader_body,
+				  downloader_request,
+				  NULL,
+				  NULL);
+	
+	deployment = new Deployment ();
+	Deployment::SetCurrent (deployment);
+	
+	if (!deployment->InitializeAppDomain ()) {
+		g_warning ("Couldn't initialize the AppDomain");
+		return EXIT_FAILURE;
+	}
+	
+	if (!(window = create_window (deployment, geometry, args[0])))
+		return EXIT_FAILURE;
+	
+	gtk_widget_show (window);
+	
+	gtk_main ();
+	
+	getopts_context_free (ctx, true);
+	deployment->Shutdown ();
+	runtime_shutdown ();
+	
+	return EXIT_SUCCESS;
 }
