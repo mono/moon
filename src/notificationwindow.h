9d169f3218f5225cfa1d34a55f7d993dfc298445
Alan McGovern <alan.mcgovern@gmail.com>
2010-04-27 12:42:29 +0000

    2010-04-27  Alan McGovern  <amcgovern@novell.com>
    	* src/notificationwindow.h: Add back in the SL4 beta 'Visible'
    	  DP.
    
    	* class/tuning/SecurityAttributes/automatic/System.Windows.auto.ssc:
    	  regen.
    
    svn path=/trunk/moon/; revision=156178

--- src/notificationwindow.h
+++ src/notificationwindow.h
@@ -56,11 +56,16 @@ public:
 	const static int HeightProperty;
 	/* @PropertyType=Visibility,DefaultValue=VisibilityVisible,GenerateAccessors,ManagedSetterAccess=Private,ManagedFieldAccess=Internal*/
 	const static int VisibilityProperty;
+	/* @PropertyType=bool,DefaultValue=true,GenerateAccessors */
+	const static int VisibleProperty; // FIXME: This DP should be removed when we have the SL4 final drts
 	/* @PropertyType=double */
 	const static int WidthProperty;
 
 	/* @DelegateType=EventHandler */
 	const static int ClosedEvent;
+
+	void SetVisible (bool value);
+	bool GetVisible ();
 
 	void SetVisibility (Visibility value);
 	Visibility GetVisibility ();
