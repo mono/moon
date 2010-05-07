f26ebca9ef0908b29d5ac747f49190f1c161945b
Jeffrey Stedfast <fejj@novell.com>
2010-05-07 18:31:10 +0000

    aded GlyphTypefaces test program
    svn path=/trunk/moon/; revision=156910

---
+++ test/3.0/GlyphTypefaces/Page.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace GlyphTypefaces {
+	public partial class Page : Canvas {
+		public Page ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
