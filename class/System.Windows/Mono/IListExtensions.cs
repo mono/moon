afee8793663b02f05846dda0a42cbfdb85d44bd9
Alan McGovern <alan.mcgovern@gmail.com>
2010-05-18 14:01:00 +0000

    2010-05-18  Alan McGovern  <amcgovern@novell.com>
    	* class/System.Windows/Makefile.am:
    	* class/System.Windows/Mono/IListExtensions.cs: Add
    	  BinarySearch extension method to IList<T> so that I can
    	  binary search observable collections (and others).
    
    svn path=/trunk/moon/; revision=157499

---
+++ class/System.Windows/Mono/IListExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Mono
+{
+	static class IListExtensions
+	{
+		public static int BinarySearch<T> (this IList<T> list, T item, IComparer<T> comparer)
+		{
+			// cache this in case we need it
+			if (comparer == null)
+				comparer = Comparer<T>.Default;
+
+			int iMin = 0;
+			int iMax = list.Count - 1;
+			int iCmp = 0;
+			try {
+				while (iMin <= iMax) {
+					// Be careful with overflow
+					// http://googleresearch.blogspot.com/2006/06/extra-extra-read-all-about-it-nearly.html
+					int iMid = iMin + ((iMax - iMin) / 2);
+					T elt = list [iMid];
+
+					iCmp = comparer.Compare (elt, item);
+
+					if (iCmp == 0)
+						return iMid;
+					else if (iCmp > 0)
+						iMax = iMid - 1;
+					else
+						iMin = iMid + 1; // compensate for the rounding down
+				}
+			}
+			catch (Exception e) {
+				throw new InvalidOperationException ("Comparer threw an exception.", e);
+			}
+
+			return ~iMin;
+		}
+	}
+}
+
