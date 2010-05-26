b122289b513e2f46bb8abd7df183bb90e9cf9a00
Jeffrey Stedfast <fejj@novell.com>
2010-05-26 17:46:28 +0000

    silence some warnings for async events
    svn path=/trunk/moon/; revision=157973

--- src/dependencyobject.cpp
+++ src/dependencyobject.cpp
@@ -1042,7 +1042,9 @@ EventObject::Emit (int event_id, EventArgs *calldata, bool only_unemitted, int s
 	if (events->lists [event_id].event_list->IsEmpty () && events->lists [event_id].onevent == NULL) {
 		if (calldata) {
 #if DEBUG
-			printf ("EMIT OF EVENT %d ON OBJECT %s CALLED WITH NO LISTENERS AND NON-NULL CALLDATA\n", event_id, GetTypeName());
+			if (!(GetObjectType () == Type::TEXTBOX && (event_id == TextBox::SelectionChangedEvent || event_id == TextBox::TextChangedEvent))
+			    || !(GetObjectType () == Type::PASSWORDBOX && event_id == PasswordBox::PasswordChangedEvent))
+				printf ("EMIT OF EVENT %d ON OBJECT %s CALLED WITH NO LISTENERS AND NON-NULL CALLDATA\n", event_id, GetTypeName());
 #endif
 			calldata->unref ();
 		}
