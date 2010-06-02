a523b7b88c69e69599825ad6da157a333fbc33cd
Jackson Harper <jackson@novell.com>
2010-06-02 17:09:30 +0000

    * ImageSourceConverter.cs: remove the override/new stuff. I         * don't         think the new was ever needed, we were just getting a warning         because we stopped defining NET_2_1.  Without this fix the         overridden methods never get called.
    svn path=/trunk/moon/; revision=158352

--- class/System.Windows/System.Windows.Media/ImageSourceConverter.cs
+++ class/System.Windows/System.Windows.Media/ImageSourceConverter.cs
@@ -36,22 +36,13 @@ namespace System.Windows.Media {
 
 		}
 		
-#if NET_2_1
-		override
-#else
-		new
-#endif
-		public bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
+		public override bool CanConvertFrom (ITypeDescriptorContext context, Type sourceType)
 		{
 			return sourceType == typeof (string) || sourceType == typeof (Uri);
 		}
 		
-#if NET_2_1
-		override
-#else
-		new
-#endif
-		public object ConvertFrom (ITypeDescriptorContext context, CultureInfo culture, object value)
+
+		public override object ConvertFrom (ITypeDescriptorContext context, CultureInfo culture, object value)
 		{
 			if (value is string) {
 				return new BitmapImage {
