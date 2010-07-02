d2df4bb348a38120d2324e017be9411e8154f5bd
David Reveman <dreveman@novell.com>
2010-07-02 20:55:01 +0000

    2010-07-02  David Reveman  <davidr@novell.com>
    	* 3.0/PlaneProjections: Add perspective transformation test.
    
    svn path=/trunk/moon/; revision=159841

---
+++ test/3.0/PlaneProjections/Page.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace PlaneProjections {
+	public partial class Page : Canvas {
+		public Page ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
