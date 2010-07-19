45b08707410ef2037043afda76e5275fbf5f2705
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-07-19 11:10:51 +0000

    2010-07-19  Rolf Bjarne Kvinge  <RKvinge@novell.com>
    	* pal/gtk/im-gtk.cpp|h: Fix the random shutdown crash here by clearing the
    	client window when the parent widget is destroyed.
    
    svn path=/trunk/moon/; revision=160563

--- src/pal/gtk/im-gtk.cpp
+++ src/pal/gtk/im-gtk.cpp
@@ -1,4 +1,12 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * im-gtk.cpp
+ *
+ * Copyright 2010 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ *
+ */
 
 #include <config.h>
 #include "im-gtk.h"
@@ -10,14 +18,16 @@ using namespace Moonlight;
 MoonIMContextGtk::MoonIMContextGtk ()
 {
 	im = gtk_im_multicontext_new ();
+	im_widget = NULL;
 }
 
 MoonIMContextGtk::~MoonIMContextGtk ()
 {
 	g_object_unref (im);
 	im = NULL;
+	if (im_widget != NULL)
+		g_signal_handlers_disconnect_matched (im_widget, G_SIGNAL_MATCH_DATA, 0, NULL, NULL, NULL, this);
 }
-
 
 void
 MoonIMContextGtk::SetUsePreedit (bool flag)
@@ -26,8 +36,47 @@ MoonIMContextGtk::SetUsePreedit (bool flag)
 }
 
 void
+MoonIMContextGtk::ImWindowDestroyed (GtkWindow *window, gpointer data)
+{
+	((MoonIMContextGtk *) data)->SetClientWindow (NULL);
+}
+
+void
 MoonIMContextGtk::SetClientWindow (MoonWindow* window)
 {
+	GdkWindow *gdk_window = NULL;
+	GdkWindow *w;
+	gpointer user_data = NULL;
+	GtkWidget *im_w = NULL;
+
+	/*
+	 * We need to clear the client window when the GtkWindow the im context uses is destroyed, otherwise
+	 * we'll get random crashes upon shutdown when the im code tries to access a widget which has already
+	 * been destroyed. So we listen to the destroyed signal of the gtk window and clear the client window
+	 * when signalled.
+	 */
+	if (window != NULL) {
+		gdk_window = GDK_WINDOW (window->GetPlatformWindow());
+		/* Find the parent gtk widget of the gdk window. */
+		w = gdk_window;
+		while (w != NULL) {
+			gdk_window_get_user_data (w, &user_data);
+			if (user_data) {
+				im_w = (GtkWidget *) user_data;
+				break;
+			}
+			w = gdk_window_get_parent (w);
+		}
+	}
+	if (im_w != this->im_widget) {
+		if (this->im_widget != NULL)
+			g_signal_handlers_disconnect_matched (this->im_widget, G_SIGNAL_MATCH_DATA, 0, NULL, NULL, NULL, this);
+
+		if (im_w != NULL)
+			g_signal_connect (im_w, "destroy", G_CALLBACK (ImWindowDestroyed), this);
+	}
+	this->im_widget = im_w;
+
 	gtk_im_context_set_client_window (im, window ? GDK_WINDOW(window->GetPlatformWindow ()) : NULL);
 }
 
--- src/pal/gtk/im-gtk.h
+++ src/pal/gtk/im-gtk.h
@@ -1,4 +1,12 @@
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * im-gtk.h
+ *
+ * Copyright 2010 Novell, Inc. (http://www.novell.com)
+ *
+ * See the LICENSE file included with the distribution for details.
+ *
+ */
 
 #ifndef MOON_IM_GTK_H
 #define MOON_IM_GTK_H
@@ -35,6 +43,9 @@ public:
 
  private:
 	GtkIMContext *im;
+	GtkWidget *im_widget;
+
+	static void ImWindowDestroyed (GtkWindow *window, gpointer data);
 };
 
 };
