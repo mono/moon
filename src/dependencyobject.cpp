aed492cc4d660add8443fd318b01687b0004d236
Chris Toshok <toshok@ximian.com>
2010-08-11 22:07:14 -0700

    don't call eventclosure funcs when they're pending_removal.
    This is rather delicate, but the func might be a managed delegate,
    and when, e.g. the first handler we emit removes the third (which
    is managed) and then the GC finalizes the third, we end up calling
    through a collected delegate, and boom.

--- src/dependencyobject.cpp
+++ src/dependencyobject.cpp
@@ -1229,7 +1229,7 @@ EventObject::DoEmitCurrentContext (int event_id, EventArgs *calldata)
 
 		EventClosure *closure = ctx->closures[i];
 
-		if (closure && closure->func
+		if (closure && closure->func && !closure->pending_removal
 		    && (!ctx->only_unemitted || closure->emit_count == 0)
 		    && (ctx->starting_generation == -1 || closure->token < ctx->starting_generation)) {
 			closure->func (this, calldata, closure->data);
