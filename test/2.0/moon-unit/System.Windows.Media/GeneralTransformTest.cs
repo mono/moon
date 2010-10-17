d9f14359caa7f9778abe941b4428c689200bf4d0
Chris Toshok <toshok@gmail.com>
2010-10-17 14:02:23 -0700

    test both RV 2 and 3+ behavior for this test, and MoonlightBug the RV2 version

--- test/2.0/moon-unit/System.Windows.Media/GeneralTransformTest.cs
+++ test/2.0/moon-unit/System.Windows.Media/GeneralTransformTest.cs
@@ -21,25 +21,50 @@ namespace MoonTest.System.Windows.Media
 	public partial class GeneralTransformTest
 	{
 		[TestMethod]
-		public void Inverses ()
+		[MinRuntimeVersion (3)]
+		public void Inverses_sl3 ()
 		{
 			GeneralTransform inverse;
 			
-			RotateTransform rotate = new RotateTransform ();
+			RotateTransform rotate = new RotateTransform { Angle = 10.0 };
 			inverse = rotate.Inverse;
-			Assert.IsTrue (inverse is RotateTransform);
+			Assert.IsTrue (inverse is MatrixTransform, "#0");
 			
-			ScaleTransform scale = new ScaleTransform ();
-			inverse = rotate.Inverse;
-			Assert.IsTrue (inverse is ScaleTransform);
+			ScaleTransform scale = new ScaleTransform { ScaleX = 2.0, ScaleY = 2.0 };
+			inverse = scale.Inverse;
+			Assert.IsTrue (inverse is MatrixTransform, "#1");
 			
-			SkewTransform skew = new SkewTransform ();
-			inverse = rotate.Inverse;
-			Assert.IsTrue (inverse is SkewTransform);
+			SkewTransform skew = new SkewTransform { AngleX = 1.0, AngleY = 1.0 };
+			inverse = skew.Inverse;
+			Assert.IsTrue (inverse is MatrixTransform, "#2");
 			
-			TranslateTransform translate = new TranslateTransform ();
+			TranslateTransform translate = new TranslateTransform { X = 5.0, Y = 5.0 };
+			inverse = translate.Inverse;
+			Assert.IsTrue (inverse is MatrixTransform, "#3");
+		}
+
+		[TestMethod]
+		[MaxRuntimeVersion (2)]
+		[MoonlightBug ("we don't return null for .Inverse.  not sure why it was even present in sl2, it seems to return null unconditionally")]
+		public void Inverses_sl2 ()
+		{
+			GeneralTransform inverse;
+			
+			RotateTransform rotate = new RotateTransform { Angle = 10.0 };
 			inverse = rotate.Inverse;
-			Assert.IsTrue (inverse is TranslateTransform);
+			Assert.IsNull (inverse, "#0");
+			
+			ScaleTransform scale = new ScaleTransform { ScaleX = 2.0, ScaleY = 2.0 };
+			inverse = scale.Inverse;
+			Assert.IsNull (inverse, "#1");
+			
+			SkewTransform skew = new SkewTransform { AngleX = 1.0, AngleY = 1.0 };
+			inverse = skew.Inverse;
+			Assert.IsNull (inverse, "#2");
+			
+			TranslateTransform translate = new TranslateTransform { X = 5.0, Y = 5.0 };
+			inverse = translate.Inverse;
+			Assert.IsNull (inverse, "#3");
 		}
 	}
 }
