mono/moon

commit 99307a4382592ba77951889cab22e699facb832d

Plugin: don't set the NPP instance->pdata field until the old PluginInstance is completely shutdown
* src/plugin.cpp: When recreating a plugin instance, don't set the NPP
  instance->pdata field until the old PluginInstance has been shutdown,
  so that we have the correct plugin instance in the NPP instance->pdata field
  during shutdown of the old plugin instance. This also requires not setting
  instance->pdata in the PluginInstance ctor.

plugin/plugin.cpp
@@ -153,8 +153,6 @@ PluginInstance::PluginInstance (NPP instance, guint16 mode)
 
 	accessibility_bridge = new AccessibilityBridge ();
 	
-	/* back pointer to us */
-	instance->pdata = this;
 	download_dir = NULL;
 }
 
@@ -183,9 +181,6 @@ PluginInstance::Recreate (const char *source)
 		onSourceDownloadProgressChanged, onSourceDownloadComplete,
 		culture, uiCulture, NULL };
 
-		
-	instance->pdata = NULL;
-	
 	PluginInstance *result;
 	result = new PluginInstance (instance, mode);
 	
@@ -217,6 +212,8 @@ PluginInstance::Recreate (const char *source)
 	/* destroy the current plugin instance */
 	Deployment::SetCurrent (deployment);
 	Shutdown ();
+
+	instance->pdata = NULL;
 	unref (); /* the ref instance->pdata has */
 	
 	/* put in the new plugin instance */

commit 02d17eeec31fdcc2ba9f6185b244270085c426a1

Fix crash in #0 introduced in 99307a4. The NPP instance->pdata must always point to the current PluginInstance.

plugin/plugin.cpp
@@ -183,6 +183,7 @@ PluginInstance::Recreate (const char *source)
 
 	PluginInstance *result;
 	result = new PluginInstance (instance, mode);
+	instance->pdata = result;
 	
 	// printf ("PluginInstance::Recreate (%s), created %p\n", source, result);
 	
@@ -210,6 +211,7 @@ PluginInstance::Recreate (const char *source)
 	g_free (maxFramerate);
 	
 	/* destroy the current plugin instance */
+	instance->pdata = this;
 	Deployment::SetCurrent (deployment);
 	Shutdown ();
 