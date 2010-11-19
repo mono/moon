8005a7427d41467527deb6efb33cc8192be9035f
Rolf Bjarne Kvinge <RKvinge@novell.com>
2010-11-19 13:43:57 +0100

    SilverlightHost: use the correct HtmlPage typename when searching for it in both browser and desktop profile

--- class/System.Windows/System.Windows.Interop/SilverlightHost.cs
+++ class/System.Windows/System.Windows.Interop/SilverlightHost.cs
@@ -133,7 +133,13 @@ namespace System.Windows.Interop {
 			}
 		}
 
-		const string HtmlPage = "System.Windows.Browser.HtmlPage, System.Windows.Browser, PublicKey=00240000048000009400000006020000002400005253413100040000010001008D56C76F9E8649383049F383C44BE0EC204181822A6C31CF5EB7EF486944D032188EA1D3920763712CCB12D75FB77E9811149E6148E5D32FBAAB37611C1878DDC19E20EF135D0CB2CFF2BFEC3D115810C3D9069638FE4BE215DBF795861920E5AB6F7DB2E2CEEF136AC23D5DD2BF031700AEC232F6C6B1C785B4305C123B37AB";
+		const string HtmlPage =
+#if NET_2_1
+			"System.Windows.Browser.HtmlPage, System.Windows.Browser, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e"
+#else
+			"System.Windows.Browser.HtmlPage, System.Windows.Browser, Version=3.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756"
+#endif
+				;
 		const BindingFlags StaticNonPublic = BindingFlags.Static | BindingFlags.NonPublic;
 		private static Type htmlpage = Type.GetType (HtmlPage);
 		private static MethodInfo get_navigation_state;
