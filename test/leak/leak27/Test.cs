4198acdfe83dcf51992d48ec4743a03ae1d77356
Alan McGovern <alan.mcgovern@gmail.com>
2011-01-06 14:56:33 +0000

    Immortality testcase for UIElements with tooltips.

---
+++ test/leak/leak27/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace Leak
+{
+	public partial class Page
+	{
+		void RunTest ()
+		{
+			WeakControl = new ContentControl ();
+			ToolTipService.SetToolTip (WeakControl, "Tooltiped!");
+
+			GCAndInvoke (() => {
+				if (WeakControl != null)
+					Fail ("The control should be collected");
+				else
+					Succeed ();
+			});
+		}
+	}
+}
