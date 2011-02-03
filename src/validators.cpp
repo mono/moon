5f57d932e6e7e89124efff9757e98aa95f26b413
Jeffrey Stedfast <fejj@gnome.org>
2011-02-03 16:39:24 -0500

    Don't throw UnauthAccess in IsSetterSealedValidator if being parsed

--- src/validators.cpp
+++ src/validators.cpp
@@ -312,7 +312,7 @@ Validators::StoryboardTargetPropertyValidator (DependencyObject* instance, Depen
 bool
 Validators::IsSetterSealedValidator (DependencyObject* instance, DependencyProperty *property, Value *value, MoonError *error)
 {
-	if (instance->Is (Type::SETTERBASE)) {
+	if (instance->Is (Type::SETTERBASE) && !instance->IsBeingParsed ()) {
 		if (((SetterBase*) instance)->GetIsSealed ()) {
 			MoonError::FillIn (error, MoonError::UNAUTHORIZED_ACCESS, "Cannot modify a setter after it is used");
 			return false;
