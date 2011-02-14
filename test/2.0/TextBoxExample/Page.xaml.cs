a023d9231e5ada1d97a60ad77c16d4dd6bc7e00f
Chris Toshok <toshok@gmail.com>
2011-02-14 11:27:46 -0800

    change textboxexample to use a rtb, and print out lots of helpful info

--- test/2.0/TextBoxExample/Page.xaml.cs
+++ test/2.0/TextBoxExample/Page.xaml.cs
@@ -2,51 +2,72 @@ using System;
 using System.Text;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Documents;
 using System.Windows.Controls;
 
 namespace TextBoxExample {
-	public partial class Page : Canvas {
+	public partial class Page : Grid {
+		Run run1, run2, run3, run4, run5;
+
 		public Page ()
 		{
 			InitializeComponent ();
-			
-			txtTextBox.Text = "hello\rworld\nand\r\nstuff";
-			
+		
+			txtTextBox.ContentChanged += OnContentChanged;	
 			txtTextBox.SelectionChanged += OnSelectionChanged;
-			txtTextBox.TextChanged += OnTextChanged;
-			
-			//cbAcceptReturn.IsChecked = txtTextBox.AcceptsReturn;
-			//cbAcceptReturn.Unchecked += OnUnchecked;
-			//cbAcceptReturn.Checked += OnChecked;
+
+			boldButton.Click += delegate { txtTextBox.Selection.ApplyPropertyValue (TextElement.FontWeightProperty, FontWeights.Bold); };
+			italicsButton.Click += delegate { txtTextBox.Selection.ApplyPropertyValue (TextElement.FontStyleProperty, FontStyles.Italic); };
+			//selectionButton.Click += delegate { Console.WriteLine ("HI HI HI HI HI"); txtSelection.Text = txtTextBox.Selection.Xaml.Replace (">", ">\n"); };
+
+			Paragraph p1 = new Paragraph () { FontSize = 12.0 };
+			Paragraph p2 = new Paragraph () { FontSize = 15.0 };
+			Paragraph p3 = new Paragraph () { FontSize = 15.0 };
+
+			run1 = new Run () { Text = "thisisatest" };
+			run2 = new Run () { Text = "thisisonlyatest", FontSize = 16.0 };
+			run3 = new Run () { Text = "thisisatest" };
+
+			run4 = new Run () { Text = "thisisalsoatest" };
+			run5 = new Run () { Text = "andonemore" };
+
+			p1.Inlines.Add (run1);
+			p1.Inlines.Add (run2);
+			p1.Inlines.Add (run3);
+
+			p2.Inlines.Add (run4);
+
+			p3.Inlines.Add (run5);
+
+			txtTextBox.Blocks.Add (p1);
+			txtTextBox.Blocks.Add (p2);
+			txtTextBox.Blocks.Add (p3);
+
+			OnContentChanged (null, EventArgs.Empty);
 		}
 		
-		void OnSelectionChanged (object sender, EventArgs args)
+		void OnContentChanged (object sender, EventArgs args)
 		{
-			string text = txtTextBox.SelectedText;
-			
-			if (text != "") {
-				txtSelection.Foreground = new SolidColorBrush (Colors.White);
-				rectSelection.Fill = new SolidColorBrush (Colors.Blue);
-				txtSelection.Text = text;
-			} else {
-				txtSelection.Foreground = new SolidColorBrush (Colors.Black);
-				rectSelection.Fill = new SolidColorBrush (Colors.White);
-				txtSelection.Text = "No text selected";
-			}
-			
-			rectSelection.Height = txtSelection.ActualHeight;
-			rectSelection.Width = txtSelection.ActualWidth;
-			
-			txtSelectionState.Text = "OnSelectionChanged(): SelectionStart = " + txtTextBox.SelectionStart +
-				"; SelectionLength = " + txtTextBox.SelectionLength;
+			txtTextBlock.Text = txtTextBox.Xaml.Replace (">", ">\n");
+			if (run4.ReadLocalValue (TextElement.FontSizeProperty) == DependencyProperty.UnsetValue)
+				txtSelection.Text = "run4 has inherited FontSizeProperty";
+			else
+				txtSelection.Text = "run4 has local FontSizeProperty";
+
 		}
-		
-		void OnTextChanged (object sender, EventArgs args)
+
+		void OnSelectionChanged (object sender, RoutedEventArgs args)
 		{
-			txtTextBlock.Text = txtTextBox.Text.Replace ("\n", "\\n").Replace ("\r", "\\r");
-			
-			txtSelectionState.Text = "OnTextChanged(): SelectionStart = " + txtTextBox.SelectionStart +
-				"; SelectionLength = " + txtTextBox.SelectionLength;
+			if (run4.ReadLocalValue (TextElement.FontSizeProperty) == DependencyProperty.UnsetValue)
+				txtSelection.Text = "run4 has inherited FontSizeProperty\n";
+			else
+				txtSelection.Text = "run4 has local FontSizeProperty\n";
+
+			if (txtTextBox.Selection.Xaml == null)
+				txtSelection.Text += "selection changed, null xaml == null";
+			txtSelection.Text += txtTextBox.Selection.Xaml.Replace (">", ">\n");
+
+			//txtSelection.Text += "\n" + (txtTextBox.Selection.Start.CompareTo (txtTextBox.Selection.End)).ToString();
 		}
 		
 		//void OnUnchecked (object sender, EventArgs args)
