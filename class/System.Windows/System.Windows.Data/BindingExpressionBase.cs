mono/moon

commit 9ceb150719d2e7a30bbeb37fdbc15b34365042cb

[Binding] Use TargetNullValue if it's supplied
If the value for a binding can't be retrieved, use the TargetNull value if it's
available rather than instantly using the fallback value. Fixes the subtest
'TargetNullValueWithoutPathTest' in drt 233.

class/System.Windows/System.Windows.Data/BindingExpressionBase.cs
@@ -386,13 +386,13 @@ namespace System.Windows.Data {
 
 			cached = true;
 			if (DataSource == null || PropertyPathWalker.IsPathBroken) {
-				// If it the path is broken, don't call the converter unless we use the fallback.
-				// FIXME: Add an explicit test on this.
-				if (Binding.FallbackValue == null) {
+				if (Binding.TargetNullValue != null) {
+					cachedValue = Binding.TargetNullValue;
+				} else if (Binding.FallbackValue != null) {
+					cachedValue = Binding.FallbackValue;
+				} else {
 					cachedValue = dp.GetDefaultValue (Target);
-					return cachedValue;
 				}
-				cachedValue = Binding.FallbackValue;
 			}
 			else {
 				cachedValue = PropertyPathWalker.Value;

commit 1e22a49c3942274860a250a560a2289ef5299f91

[Binding] If we use the DP default value, don't invoke the converter.
Fixes a regression in moon-unit caused by 9ceb150719.

class/System.Windows/System.Windows.Data/BindingExpressionBase.cs
@@ -392,6 +392,7 @@ namespace System.Windows.Data {
 					cachedValue = Binding.FallbackValue;
 				} else {
 					cachedValue = dp.GetDefaultValue (Target);
+					return cachedValue;
 				}
 			}
 			else {