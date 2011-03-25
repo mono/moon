3c42861afe7cd5664841cb806097088c7cc31c04
Chris Toshok <toshok@gmail.com>
2011-03-25 10:35:36 -0700

    loads of fixups.  some value printing for non-do's, dp printing, and general stability foo

--- data/libmoonplugin.so-gdb.py
+++ data/libmoonplugin.so-gdb.py
@@ -4,7 +4,10 @@ def get_object_type (val):
     return int(val['object_type']);
 
 def get_type_name (val):
-    return str(val['deployment']['types']['types']['array'][get_object_type(val)].cast(gdb.lookup_type("Moonlight::Type").pointer())['name'])
+    try:
+      return str(val['deployment']['types']['types']['array'][get_object_type(val)].cast(gdb.lookup_type("Moonlight::Type").pointer())['name'])
+    except:
+      return "Unknown"
 
 
 def moonlight_is (deployment, dest, type):
@@ -68,6 +71,7 @@ def is_eventobject (val):
     if type.code != gdb.TYPE_CODE_PTR:
         return False
     type = type.target()
+    type = type.unqualified()
     return is_eventobject_helper (type)
 
 def find_type (name):
@@ -86,19 +90,136 @@ class ValuePrinter:
         self.val = val
         self.deployment = deployment
 
-    def to_string(self):
-        if self.val == None or (self.val['padding'] & 1 == 1):
-          return "Value (NULL)"
-        else:
-          if moonlight_is (self.deployment, find_type ("DEPENDENCY_OBJECT"), self.val['k']):
-            return "Value (" + str(self.val['u']['dependency_object']) + ")"
-          else:
-            return "value"
+#	bool		AsBool () const	            { checked_get_exact (Type::BOOL, false, (bool)u.i32); }
+#	gunichar	AsChar () const             { checked_get_exact (Type::CHAR, 0, u.c); }
+#	double 		AsDouble () const           { checked_get_exact (Type::DOUBLE, 0.0, u.d); }
+#	gint64		AsEnum (Types *types = NULL) const { checked_get_enum (); }
+#	float 		AsFloat () const	    { checked_get_exact (Type::FLOAT, 0.0, u.f); }
+#	guint64		AsUInt64 () const	    { checked_get_exact (Type::UINT64, 0, u.ui64); }
+#	gint64		AsInt64 () const	    { checked_get_exact (Type::INT64, 0, u.i64); }
+#	TimeSpan	AsTimeSpan () const	    { checked_get_exact (Type::TIMESPAN, 0, (TimeSpan)u.i64); }
+#	guint32		AsUInt32 () const	    { checked_get_exact (Type::UINT32, 0, u.ui32); }
+#	gint32		AsInt32 () const	    { checked_get_exact (Type::INT32, 0, u.i32); }
+#	char*		AsString () const	    { checked_get_exact (Type::STRING, NULL, u.s); }
 
-class CollectionPrinter:
+    def formatString (self):
+        return "String=%s" % (self.val['u']['s'])
+    def formatColor (self):
+        color = self.val['u']['color']
+        if long(color) == 0:
+          return "Color=NULL"
+        return "Color (r=%d,g=%d,b=%d,a=%g)" % (int(float(color.val['r']) * 255), int(float(color.val['g']) * 255), int(float(color.val['b']) * 255), float(color.val['a']))
+    def formatPoint (self):
+        point = self.val['u']['point']
+        if long(Point) == 0:
+          return "Point=NULL"
+        return "Point (x=%g,y=%g)" % (float(point.val['x']), float(point.val['y']))
+    def formatUri (self):
+        return "Uri" # TODO
+    def formatRect (self):
+        return "Rect" # TODO
+    def formatSize (self):
+        return "Size" # TODO
+    def formatFontFamily (self):
+        return "FontFamily" # TODO
+    def formatFontWeight (self):
+        return "FontWeight" # TODO
+    def formatFontStyle (self):
+        return "FontStyle" # TODO
+    def formatFontStretch (self):
+        return "FontStretch" # TODO
+    def formatFontSource (self):
+        return "FontSource" # TODO
+    def formatFontResouce (self):
+        return "FontResource" # TODO
+    def formatPropertyPath (self):
+        return "FontPropertyPath" # TODO
+    def formatXmlLanguage (self):
+        return "XmlLanguage" # TODO
+    def formatDependencyProperty (self):
+        return str (self.val['u']['dp'])
+    def formatDateTime (self):
+        return "DateTime" # TODO
+    def formatGCHandle (self):
+        return "GCHandle" # TODO
+    def formatNPObject (self):
+        return "NPObject" # TODO
+    def formatRepeatBehavior (self):
+        return "RepeatBehavior" # TODO
+    def formatDuration (self):
+        duration = self.val['u']['duration']
+        if long(duration) == 0:
+            return "Duration(NULL)"
+
+        if int(duration['k']) == 0:
+            return "Duration(Automatic)"
+        elif int(duration['k']) == 2:
+            return "Duration(Forever)"
+        else:
+            return "Duration(Timespan:%d)" % (long(duration['timespan']))
+    def formatKeyTime (self):
+        return "KeyTime" # TODO
+    def formatGridLength (self):
+        return "GridLength" # TODO
+    def formatThickness (self):
+        return "Thickness" # TODO
+    def formatCornerRadius (self):
+        return "CornerRadius" # TODO
+    def formatDefault (self):
+        return self.val['k']
+
+    def to_string(self):
+        formatters = {
+            "Moonlight::Type::DURATION": self.formatDuration,
+            "Moonlight::Type::STRING": self.formatString
+        }
+
+        if self.val == None or (self.val['padding'] & 1 == 1):
+          return "[NULL]"
+        else:
+          if self.deployment == None:
+            self.deployment = self.find_deployment_on_stack ()
+
+          if self.deployment == None:
+            return "[Value]"
+          elif moonlight_is (self.deployment, find_type ("DEPENDENCY_OBJECT"), self.val['k']):
+            return "[" + str(self.val['u']['dependency_object']) + "]"
+          else:
+#            print self.val['k']
+            return "[%s]" % (formatters.get(str(self.val['k']), self.formatDefault) ())
+
+    def find_deployment_on_stack (self):
+        frame = gdb.selected_frame()
+        # look back along the stack for DO*'s used as "this" in a c++ method
+        # FIXME we should also grovel along the parameter list in methods
+
+        while frame != None and frame.is_valid():
+          this_var = frame.read_var ("this")
+          if this_var != None and is_eventobject (this_var):
+              deployment = this_var.cast (gdb.lookup_type ("Moonlight::EventObject").pointer())['deployment']
+              return deployment
+          frame = frame.older()
+
+        return None
+
+class DependencyPropertyPrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        return  "[name=%s, owner=%s]" % (self.val['name'], str(self.val['owner_type']))
+
+class EventObjectPrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        return  "[%s]" % (get_type_name (self.val))
+
+class CollectionPrinter(EventObjectPrinter):
     class _iterator:
         def __init__(self, val):
-	    self.val = val;
+            self.val = val
             self.count = 0
 
         def __iter__(self):
@@ -113,42 +234,46 @@ class CollectionPrinter:
             return ('[%d]' % count, ValuePrinter (data, self.val['deployment']).to_string())
 
     def __init__ (self, val):
-        self.val = val
+        # not sure why this isn't working...
+        # super(CollectionPrinter, self).__init__(val)
+        eo_init = EventObjectPrinter.__init__;
+        self.eo_init (self, val)
 
     def children(self):
         return self._iterator(self.val)
 
-    def to_string (self):
-        return  "0x%x" % (long(self.val))
-
     def display_hint (self):
         return "array"
 
-
-class EventObjectPrinter:
-    def __init__(self, val):
-        self.val = val
-
-    def to_string(self):
-        return  "0x%x [%s]" % (long(self.val), get_type_name (self.val))
-
 def lookup_printer (val):
 
+    if val == None:
+       return None
+
+    type = val.type
+    if type.code == gdb.TYPE_CODE_PTR:
+      type = type.target()
+
+    type = type.unqualified()
+
+
     if is_eventobject (val):
-        try:
-            val_type = val['object_type']
-            if moonlight_is (val['deployment'], find_type ("COLLECTION"), val_type):
-	        print "collection!"
-	        return CollectionPrinter (val.cast(gdb.lookup_type("Moonlight::Collection").pointer()))
-        except:
+#        try:
+        val_type = val['object_type']
+        if moonlight_is (val['deployment'], find_type ("COLLECTION"), val_type):
+            return CollectionPrinter (val.cast(gdb.lookup_type("Moonlight::Collection").pointer()))
+#        except:
             # we hit this if val is null
-            return None
+#            return None
         str_type = str(val_type)
 	return EventObjectPrinter (val)
-      
-    str_type = str(val.type)
-    if str_type == "Moonlight::Value *":
-        return ValuePrinter (val)
+
+    str_type = str(type)
+    if str_type == "Moonlight::Value":
+        return ValuePrinter (val, None)
+
+    if str_type == "Moonlight::DependencyProperty":
+        return DependencyPropertyPrinter (val)
 
     lookup_tag = val.type.tag
 
