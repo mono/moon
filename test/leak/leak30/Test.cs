62eb577014cc215a584827d36ef2cbe8544320d0
Alan McGovern <alan.mcgovern@gmail.com>
2011-04-08 14:17:57 +0100

    Add leak test 30.

---
+++ test/leak/leak30/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Leak
+{
+	public partial class Page
+	{
+		struct Wrapper {
+			public object Value { get; set; }
+		}
+
+		void RunTest ()
+		{
+			// Store a CLR struct in a custom DP. Make that CLR struct hold
+			// a reference to the Control its stored in to try and create a
+			// cycle which prevents GC.
+			var dp = DependencyProperty.RegisterAttached ("CustomGCCheckDP", typeof (Wrapper), typeof (ContentControl), null);
+			WeakControl = new ContentControl ();
+			WeakControl.SetValue (dp, new Wrapper { Value = WeakControl });
+
+			GCAndInvoke (() => {
+				if (WeakControl == null)
+					Succeed ();
+				else
+					Fail ("Storing a struct in a custom DP made an immortal object");
+			});
+		}
+	}
+}
