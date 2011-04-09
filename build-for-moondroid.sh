6e1c2f5ca250432c98c5a41e54e194ecf2ae4b60
Chris Toshok <toshok@gmail.com>
2011-04-09 12:44:37 -0700

    use -j 4 and don't install

--- build-for-moondroid.sh
+++ build-for-moondroid.sh
@@ -4,5 +4,4 @@ if [ -d /usr/X11/share/aclocal ]; then
 else
     PKG_CONFIG_PATH=$MOONLIGHT_PREFIX/lib/pkgconfig ./autogen.sh --host=arm-linux-androideabi --prefix=$MOONLIGHT_PREFIX --with-ffmpeg=no --with-alsa=no --with-pulseaudio=no --with-pal=android --with-unwind=no --with-curl=no --with-manual-mono=yes --with-cairo=system --disable-browser-support --with-gallium-path=no --enable-sdk=no --with-egl=no --with-glx=no --with-testing=no CFLAGS="-DPLATFORM_ANDROID -I$MOONLIGHT_PREFIX/include $CFLAGS" LDFLAGS="-L$MOONLIGHT_PREFIX/lib $LDFLAGS" CXXFLAGS="-fno-rtti -DPLATFORM_ANDROID -I$MOONLIGHT_PREFIX/include $CFLAGS"
 fi
-make
-make install
+make -j 4
