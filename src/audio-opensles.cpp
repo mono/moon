c904d4b4e9f5c75f0b0d2b65e2943cfdebf9c984
Chris Toshok <toshok@gmail.com>
2011-04-17 14:10:35 -0700

    more opensles work

--- src/audio-opensles.cpp
+++ src/audio-opensles.cpp
@@ -32,7 +32,17 @@
 	} \
 } while (0)
 
-#define NUM_BUFFERS_IN_QUEUE 3
+#define BUFFER_SIZE 8192
+#define NUM_BUFFERS_IN_QUEUE 10
+
+#if TOSHOK_HONEYCOMB_HACK
+extern "C" {
+extern SLAPIENTRY const SLInterfaceID SL_IID_ANDROIDBUFFERQUEUE;
+extern SLAPIENTRY const SLInterfaceID XA_IID_ENGINE;
+extern SLAPIENTRY const SLInterfaceID XA_IID_PLAY;
+extern SLAPIENTRY const SLInterfaceID XA_IID_ANDROIDBUFFERQUEUE;
+};
+#endif
 
 namespace Moonlight {
 
@@ -116,19 +126,12 @@ OpenSLESSource::NextBuffer ()
 	CHECK_RESULT ("playerBufferQueue->GetState");
 
 	void *pBuffer = buffers[(bufferQueueState.index+1) % NUM_BUFFERS_IN_QUEUE];
-	int bufferSize = 4096;
+	int bufferSize = BUFFER_SIZE;
 
         Write (pBuffer, bufferSize / GetOutputBytesPerFrame ());
 
         result = (*playerBufferQueue)->Enqueue(playerBufferQueue, pBuffer, bufferSize);
 	CHECK_RESULT("playerBufferQueue->Enqueue");
-#if notyet
-        // enqueue another buffer
-        result = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, nextBuffer, nextSize);
-        // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
-        // which for this code example would indicate a programming error
-        assert(SL_RESULT_SUCCESS == result);
-#endif
 }
 
 bool
@@ -145,12 +148,12 @@ OpenSLESSource::InitializeOpenSLES ()
 
 	buffers = (gpointer*)g_malloc(sizeof (gpointer) * NUM_BUFFERS_IN_QUEUE);
 	for (int i = 0; i < NUM_BUFFERS_IN_QUEUE; i ++)
-	  buffers[i] = g_malloc (4096);
+	  buffers[i] = g_malloc (BUFFER_SIZE);
 
 	g_debug ("channel count = %d\n", GetAudioStream()->GetChannels());
 
 	// configure audio source
-	SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_BUFFERQUEUE, NUM_BUFFERS_IN_QUEUE};
+	SLDataLocator_AndroidSimpleBufferQueue loc_bufq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, NUM_BUFFERS_IN_QUEUE};
 
 	SLDataFormat_PCM format_pcm = {
 	  SL_DATAFORMAT_PCM, GetAudioStream()->GetChannels(),
@@ -167,12 +170,18 @@ OpenSLESSource::InitializeOpenSLES ()
 	SLDataSink audioSnk = {&loc_outmix, NULL};
 
 	// create audio player
-	const SLInterfaceID ids[1] = {SL_IID_BUFFERQUEUE};
+	const SLInterfaceID ids[1] = {
+#if TOSHOK_HONEYCOMB_HACK
+		SL_IID_ANDROIDBUFFERQUEUE
+#else
+		SL_IID_ANDROIDSIMPLEBUFFERQUEUE
+#endif
+	};
 	const SLboolean req[1] = {SL_BOOLEAN_TRUE};
 	result = (*player->engineEngine)->CreateAudioPlayer(player->engineEngine,
 							    &playerObject,
 							    &audioSrc, &audioSnk,
-							    0, ids, req);
+							    1, ids, req);
 	CHECK_RESULT_BOOL ("engineEngine->CreateAudioPlayer");
 
 	// realize the player
@@ -184,17 +193,27 @@ OpenSLESSource::InitializeOpenSLES ()
 	CHECK_RESULT_BOOL ("playerObject->GetInterface (SL_IID_PLAY)");
 
 	// get the buffer queue interface
-	result = (*playerObject)->GetInterface(playerObject, SL_IID_BUFFERQUEUE, &playerBufferQueue);
-	CHECK_RESULT_BOOL ("playerObject->GetInterface(SL_IID_BUFFERQUEUE)");
+	result = (*playerObject)->GetInterface(playerObject,
+#if TOSHOK_HONEYCOMB_HACK
+					       SL_IID_ANDROIDBUFFERQUEUE,
+#else
+					       SL_IID_ANDROIDSIMPLEBUFFERQUEUE,
+#endif
+					       &playerBufferQueue);
+	CHECK_RESULT_BOOL ("playerObject->GetInterface(SL_IID_ANDROIDBUFFERQUEUE)");
 
+	g_debug ("1");
 	// register callback on the buffer queue
 	result = (*playerBufferQueue)->RegisterCallback(playerBufferQueue, OpenSLESSource::NextBufferCallback, this);
+	g_debug ("2");
 	CHECK_RESULT_BOOL ("playerBufferQueue->RegisterCallback()");
 
 	// set the player's state to playing
 	// FIXME do we do this here?
 	result = (*playerPlay)->SetPlayState(playerPlay, SL_PLAYSTATE_PLAYING);
 	CHECK_RESULT_BOOL ("playerPlay->SetPlayState (SL_PLAYSTATE_PLAYING)");
+
+	initialized = true;
 
 	return true;
 }
@@ -249,8 +268,9 @@ OpenSLESSource::Stopped ()
 guint64
 OpenSLESSource::GetDelayInternal ()
 {
-	LOG_OSL ("OpenSLESSource::GetDelayInternal (): not implemented\n");
-	return G_MAXUINT64;
+	return 0; // let's be optimistic
+// 	LOG_OSL ("OpenSLESSource::GetDelayInternal (): not implemented\n");
+// 	return G_MAXUINT64;
 }
 
 /*
@@ -296,6 +316,88 @@ OpenSLESPlayer::FinishShutdownInternal ()
 {
 }
 
+static char *
+unknownInterfaceToString (SLInterfaceID interfaceId)
+{
+	static char buf[1024];
+	snprintf (buf, sizeof (buf), "%08X-%04X-%04X-%04X-%02X-%02X-%02X-%02X-%02X-%02X",
+		  (unsigned int)interfaceId->time_low,
+		  interfaceId->time_mid,
+		  interfaceId->time_hi_and_version,
+		  interfaceId->clock_seq,
+		  interfaceId->node[0],
+		  interfaceId->node[1],
+		  interfaceId->node[2],
+		  interfaceId->node[3],
+		  interfaceId->node[4],
+		  interfaceId->node[5]);
+	return buf;
+}
+
+static const char*
+interfaceIdToString (SLInterfaceID interfaceId)
+{
+#define CHECK(x) interfaceId == (x) ? #x :
+#define DEFAULT(x) x
+
+	return CHECK(SL_IID_EQUALIZER)
+		CHECK(SL_IID_BASSBOOST)
+		CHECK(SL_IID_VIRTUALIZER)
+		CHECK(SL_IID_PRESETREVERB)
+		CHECK(SL_IID_ENVIRONMENTALREVERB)
+		CHECK(SL_IID_3DCOMMIT)
+		CHECK(SL_IID_3DDOPPLER)
+		CHECK(SL_IID_3DGROUPING)
+		CHECK(SL_IID_3DLOCATION)
+		CHECK(SL_IID_3DMACROSCOPIC)
+		CHECK(SL_IID_3DSOURCE)
+		CHECK(SL_IID_AUDIODECODERCAPABILITIES)
+		CHECK(SL_IID_AUDIOENCODER)
+		CHECK(SL_IID_AUDIOENCODERCAPABILITIES)
+		CHECK(SL_IID_AUDIOIODEVICECAPABILITIES)
+		CHECK(SL_IID_BUFFERQUEUE)
+		CHECK(SL_IID_DEVICEVOLUME)
+		CHECK(SL_IID_DYNAMICINTERFACEMANAGEMENT)
+		CHECK(SL_IID_DYNAMICSOURCE)
+		CHECK(SL_IID_EFFECTSEND)
+		CHECK(SL_IID_ENGINE)
+		CHECK(SL_IID_ENGINECAPABILITIES)
+		CHECK(SL_IID_LED)
+		CHECK(SL_IID_METADATAEXTRACTION)
+		CHECK(SL_IID_METADATATRAVERSAL)
+		CHECK(SL_IID_MIDIMESSAGE)
+		CHECK(SL_IID_MIDIMUTESOLO)
+		CHECK(SL_IID_MIDITEMPO)
+		CHECK(SL_IID_MIDITIME)
+		CHECK(SL_IID_MUTESOLO)
+		CHECK(SL_IID_NULL)
+		CHECK(SL_IID_OBJECT)
+		CHECK(SL_IID_OUTPUTMIX)
+		CHECK(SL_IID_PITCH)
+		CHECK(SL_IID_PLAY)
+		CHECK(SL_IID_PLAYBACKRATE)
+		CHECK(SL_IID_PREFETCHSTATUS)
+		CHECK(SL_IID_RATEPITCH)
+		CHECK(SL_IID_RECORD)
+		CHECK(SL_IID_SEEK)
+		CHECK(SL_IID_THREADSYNC)
+		CHECK(SL_IID_VIBRA)
+		CHECK(SL_IID_VISUALIZATION)
+		CHECK(SL_IID_VOLUME)
+		CHECK(SL_IID_ANDROIDEFFECT)
+		CHECK(SL_IID_ANDROIDEFFECTCAPABILITIES)
+		CHECK(SL_IID_ANDROIDEFFECTSEND)
+		CHECK(SL_IID_ANDROIDCONFIGURATION)
+		CHECK(SL_IID_ANDROIDSIMPLEBUFFERQUEUE)
+#if TOSHOK_HONEYCOMB_HACK
+		CHECK(SL_IID_ANDROIDBUFFERQUEUE)
+		CHECK(XA_IID_ENGINE)
+		CHECK(XA_IID_PLAY)
+		CHECK(XA_IID_ANDROIDBUFFERQUEUE)
+#endif
+		DEFAULT(unknownInterfaceToString (interfaceId));
+}
+
 bool
 OpenSLESPlayer::Initialize ()
 {
@@ -320,36 +422,26 @@ OpenSLESPlayer::Initialize ()
 	for (SLuint32 i = 0; i < numSupportedInterfaces; i ++) {
 		SLInterfaceID interfaceId;
 		slQuerySupportedEngineInterfaces (i, &interfaceId);
-		LOG_OSL ("      interface = %s: ", 
-#define CHECK(x) interfaceId == (x) ? #x :
-#define DEFAULT(x) x
-			 CHECK(SL_IID_NULL)
-			 CHECK(SL_IID_OBJECT)
-			 CHECK(SL_IID_AUDIOIODEVICECAPABILITIES)
-			 CHECK(SL_IID_LED)
-			 CHECK(SL_IID_VIBRA)
-			 CHECK(SL_IID_METADATAEXTRACTION)
-			 CHECK(SL_IID_METADATATRAVERSAL)
-			 CHECK(SL_IID_DYNAMICSOURCE)
-			 CHECK(SL_IID_OUTPUTMIX)
-			 CHECK(SL_IID_PLAY)
-			 CHECK(SL_IID_PREFETCHSTATUS)
-			 CHECK(SL_IID_PLAYBACKRATE)
-			 CHECK(SL_IID_SEEK)
-			 CHECK(SL_IID_RECORD)
-			 CHECK(SL_IID_EQUALIZER)
-			 CHECK(SL_IID_VOLUME)
-			 CHECK(SL_IID_DEVICEVOLUME)
-			 CHECK(SL_IID_BUFFERQUEUE)
-			 CHECK(SL_IID_PRESETREVERB)
-			 CHECK(SL_IID_ENVIRONMENTALREVERB)
-			 CHECK(SL_IID_EFFECTSEND)
-			 CHECK(SL_IID_3DGROUPING)
-			 CHECK(SL_IID_3DCOMMIT)
-			 DEFAULT("Unknown"));
+		LOG_OSL ("      interface = %s: ", interfaceIdToString (interfaceId));
 	}
 
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+	LOG_OSL ("********************************");
+
+#if 0
+	const SLInterfaceID ids[1] = {SL_IID_ENGINECAPABILITIES};
+	const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+	result = slCreateEngine(&engineObject, 0, NULL, 1, ids, req);
+#else
 	result = slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
+#endif
 	CHECK_RESULT_BOOL ("slCreateEngine");
 
 	result = (*engineObject)->Realize (engineObject, SL_BOOLEAN_FALSE);
@@ -358,12 +450,13 @@ OpenSLESPlayer::Initialize ()
 	result = (*engineObject)->GetInterface (engineObject, SL_IID_ENGINE, &engineEngine);
 	CHECK_RESULT_BOOL ("engine->GetInterface (SL_IID_ENGINE)");
 
+#if 0
 	{
 		SLEngineCapabilitiesItf capabilities;
 		SLuint16 profilesSupported;
 
 		result = (*engineObject)->GetInterface (engineObject, SL_IID_ENGINECAPABILITIES, &capabilities);
-		CHECK_RESULT_BOOL ("engineEngine->GetInterface (SL_IID_ENGINECAPABILITIES)");
+		CHECK_RESULT_BOOL ("engineObject->GetInterface (SL_IID_ENGINECAPABILITIES)");
 
 		result = (*capabilities)->QuerySupportedProfiles (capabilities, &profilesSupported);
 		CHECK_RESULT_BOOL ("capabilities->QuerySupportedProfiles");
@@ -375,6 +468,7 @@ OpenSLESPlayer::Initialize ()
 		if (profilesSupported & SL_PROFILES_GAME)
 			g_debug ("   GAME");
 	}
+#endif
 
 	result = (*engineEngine)->CreateOutputMix (engineEngine, &outputMixObject, 0, NULL, NULL);
 	CHECK_RESULT_BOOL ("engine->CreateOutputMix");
