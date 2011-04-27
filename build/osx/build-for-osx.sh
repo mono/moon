cb045ef6cc6d4ff9300d26e1a724db453df25baa
Chris Toshok <toshok@gmail.com>
2011-04-27 11:49:16 -0700

    clean up the osx stuff further.

--- build/osx/build-for-osx.sh
+++ build/osx/build-for-osx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./autogen.sh --prefix=/opt/moonlight-osx --with-pal=cocoa --with-cgl=yes --with-vda=yes --with-curl=system CFLAGS="-arch i386" \
-	--enable-xlib=no --enable-trace=no --enable-svg=no --enable-png=yes --enable-ft=no --enable-fc=no --enable-gobject=no --enable-xlib-xrender=no
+./autogen.sh --prefix=/opt/moonlight-osx --with-pal=cocoa --with-cgl=yes --with-vda=yes --with-curl=system \
+	--enable-xlib=no --enable-trace=no --enable-svg=no --enable-png=yes --enable-ft=no --enable-fc=no --enable-gobject=no --enable-xlib-xrender=no $DARWOON_CONFIGURE_HOST
